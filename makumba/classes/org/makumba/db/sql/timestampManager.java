e19176f655de6982e9431cbd7ed1ce3ef2ac2681
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2003-03-26 14:55:53 +0000

    some timestamps are returned as bigDecimals, so getTimestamp() is better

--- makumba/classes/org/makumba/db/sql/timestampManager.java
+++ makumba/classes/org/makumba/db/sql/timestampManager.java
@@ -1,3 +1,26 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
 package org.makumba.db.sql;
 import java.sql.*;
 
@@ -34,6 +57,19 @@ public class timestampManager extends FieldManager
 	Object o=super.checkValueImpl(value);	
 	if(o instanceof java.util.Date && !(o instanceof Timestamp))
 	   o= new Timestamp(((java.util.Date)o).getTime());
+	return o;
+    }
+
+    public Object getValue(ResultSet rs, int i)
+       throws SQLException
+    {
+	Object o= rs.getTimestamp(i);
+	if(rs.wasNull())
+	    return null;
+	//  return getDefaultValue();
+	//        if(o instanceof java.lang.BigDecimal)
+
+	// System.out.println(o.getClass());
 	return o;
     }
 
