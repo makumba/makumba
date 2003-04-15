89cd12e9b8ff3bb6a221294c2926ae48991d6bc1
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2003-04-15 14:02:26 +0000

    a more sophisticated version

--- makumba/classes/org/makumba/view/jsptaglib/NewProjectionException.java
+++ makumba/classes/org/makumba/view/jsptaglib/NewProjectionException.java
@@ -1,3 +1,11 @@
 package org.makumba.view.jsptaglib;
 
-class NewProjectionException extends RuntimeException{}
+public class NewProjectionException 
+    extends org.makumba.controller.http.AllowedException
+{
+    public NewProjectionException(String vname, String s)
+    { super("dummy exception due to self-reload of "+s); }
+
+    // we avoid log pollution
+    public void printStackTrace(java.io.PrintWriter s){ s.print(getMessage());}
+}
