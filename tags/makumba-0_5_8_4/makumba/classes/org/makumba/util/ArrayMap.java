13b12139d8e09d2f923f354d0bd9e43f729fb776
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2003-06-04 10:43:19 +0000

    This commit was manufactured by cvs2svn to create tag 'makumba-0_5_8_4'.

---
+++ tags/makumba-0_5_8_4/makumba/classes/org/makumba/util/ArrayMap.java
@@ -0,0 +1,104 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.util;
+import java.util.*;
+
+/** this class exports an Object[] array as a dictionary */
+public class ArrayMap extends Dictionary
+{
+  public Object[] data;
+  Dictionary keyIndex;
+
+  public ArrayMap(Dictionary d, Object[]o){ data=o; keyIndex=d; }
+  public ArrayMap(){keyIndex=new Hashtable(); }
+  Integer index(Object key){ return (Integer)keyIndex.get(key); }
+  public Object get(Object key)
+  { 
+    Integer i= index(key);
+    if(i==null)
+      return null;
+    return data[i.intValue()];
+  }
+  public Object put(Object key, Object value)
+  {
+    Integer i= index(key);
+    if(i!=null)
+      {
+	Object ret=data[i.intValue()];
+	data[i.intValue()]=value;
+	return ret;
+      }
+    else
+      throw new RuntimeException("invalid key: "+key);
+  }
+
+  public Object remove(Object key){ return put(key, null); }
+  public Enumeration keys()
+  {
+    return new Enumeration(){
+      Object nxt;
+      Object next;
+      Enumeration e;
+      {e=keyIndex.keys();findNext();}
+      void findNext()
+	{ 
+	  next=null;
+	  while(e.hasMoreElements() && (next=get(nxt=e.nextElement()))==null); 
+	}
+      public boolean hasMoreElements(){ return next!=null; }
+      public Object nextElement(){  Object o=nxt; findNext(); return o ;}
+    }; 
+  }
+  public Enumeration elements() 
+  { 
+    return new Enumeration(){
+      int i;
+      {i=0; findNext();}
+      void findNext(){ while(i<data.length && data[i]==null)i++; }
+      public boolean hasMoreElements(){ return i<data.length; }
+      public Object nextElement(){  Object o=data[i++]; findNext(); return o ;}
+    }; 
+  }
+  public int size() { int n=0; for(int i=0; i<data.length; i++) if(data[i]!=null) n++; return n;}
+  public boolean isEmpty(){ return size()==0; }
+  public String toString() 
+  {
+    StringBuffer ret= new StringBuffer();
+    ret.append("{");
+    String sep="";
+    Object o;
+    for(Enumeration e=keys(); e.hasMoreElements(); )
+      {
+	ret.append(sep); sep=",";
+	ret.append(o=e.nextElement()).append("=").append(get(o));
+      }
+    ret.append("}");
+    return ret.toString();
+  }
+  abstract class Enumerator implements Enumeration
+  {
+    
+  }
+}
+
---
+++ tags/makumba-0_5_8_4/makumba/classes/org/makumba/view/jsptaglib/TagStrategySupport.java
@@ -0,0 +1,62 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.view.jsptaglib;
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+
+/** a simple implementation of TagStrategy */
+public class TagStrategySupport implements TagStrategy
+{
+  MakumbaTag tag;
+  RootTagStrategy root;
+  BodyContent bodyContent;
+  PageContext pageContext;
+  Object key;
+  
+  /** initialize */
+  public void init(MakumbaTag root, MakumbaTag tag, Object key)
+  {
+    this.tag=tag;
+    this.root=(RootTagStrategy)root.strategy;
+    this.key=key;
+ }
+
+  public void loop(){}
+  public void setBody(BodyContent bc){ bodyContent=bc; }
+  public void setPage(PageContext pc){ pageContext=pc; }
+  public BodyContent getBody(){ return bodyContent; }
+  public int doStart() throws JspException { return Tag.SKIP_BODY; }
+  public void doAnalyze() { }
+  public int doAfter()throws JspException { return Tag.SKIP_BODY; }
+  public int doEnd() throws JspException 
+  {
+    if(tag.wasException())
+      return BodyTag.SKIP_PAGE;
+    return BodyTag.EVAL_PAGE;
+  }
+
+  public void doRelease() {bodyContent=null;}
+  public void rootClose() {}
+  public Object getKey(){ return key; }
+}
