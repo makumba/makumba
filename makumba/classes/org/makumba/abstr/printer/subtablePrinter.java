3e9059283465f4ac3953cbd4b7a302903bc0214c
Manuel Bernhardt <manuel@bernhardt.io>
2003-09-06 00:29:11 +0000

    This commit was manufactured by cvs2svn to create tag 'makumba-0_5_9_6'.

--- makumba/classes/org/makumba/abstr/printer/subtablePrinter.java
+++ makumba/classes/org/makumba/abstr/printer/subtablePrinter.java
@@ -27,6 +27,6 @@ public class subtablePrinter extends FieldPrinter
 {
    public String valueToString()
    { 
-       return super.valueToString()+(hasDescription()?"\t;"+getDescription():"")+"\n"+new RecordPrinter(getSubtable()); 
+       return super.valueToString()+" "+new RecordPrinter(getSubtable()); 
    }
 }
--- makumba/classes/org/makumba/view/html/charViewer.java
+++ makumba/classes/org/makumba/view/html/charViewer.java
@@ -28,22 +28,19 @@ import org.makumba.util.HtmlUtils;
 
 public class charViewer extends FieldViewer
 {
-  static String[] params= { "urlEncode", "html", "maxLength", "ellipsis", "ellipsisLength", "addTitle" };
-  static String[][] paramValues= { {"true", "false"}, { "true", "false", "auto" }, null, null, null, { "true", "false", "auto" }};
+  static String[] params= { "urlEncode", "html" };
+  static String[][] paramValues= { {"true", "false"}, { "true", "false", "auto" }};
 
   public String[] getAcceptedParams(){ return params; }
   public String[][] getAcceptedValue(){ return paramValues; }
-
 
   public String formatNotNull(Object o, Dictionary formatParams) 
   {
     String txt=o.toString();
     String ht= (String)formatParams.get("html");
-
-
     if(ht!=null && (ht.equals("true") || ht.equals("auto") && HtmlUtils.detectHtml(txt)))
       return txt;
+    return HtmlUtils.string2html(txt);
 
-    return formatMaxLengthEllipsis(txt, formatParams);
   }
 }
