a897f20a59f0378bef4749394be3d784da6d50f2
Frederik Habils <38843752+frederikhabils@users.noreply.github.com>
2003-09-07 20:37:05 +0000

    fix bug 555, iterationGroup does not depend on doAfterBody

--- makumba/classes/org/makumba/view/jsptaglib/QueryExecution.java
+++ makumba/classes/org/makumba/view/jsptaglib/QueryExecution.java
@@ -62,7 +62,7 @@ public class QueryExecution
 
   static final private String EXECUTIONS= "org.makumba.taglibQueryExecutions";
   static final private String CURRENT_DATA_SET="org.makumba.currentDataSet";  
-  static final private Dictionary nothing= new ArrayMap();
+  static final private Dictionary NOTHING= new ArrayMap();
 
   /** Allocate a currentDataSet and a container for the QueryExecutions of the listGroup.
    * Executed by the rootList
@@ -72,7 +72,7 @@ public class QueryExecution
     pageContext.setAttribute(EXECUTIONS, new HashMap());
     Stack currentDataSet= new Stack();
     // org.makumba.view.Grouper requires the stack not be empty
-    currentDataSet.push(nothing);
+    currentDataSet.push(NOTHING);
     pageContext.setAttribute(CURRENT_DATA_SET, currentDataSet);
   }
 
@@ -129,6 +129,7 @@ public class QueryExecution
   }
 
   /** A new parent iteration is made. Compute the iterationGroupData based on listData and currentDataSet. Push the first currentListData to the currentDataSet.
+   * Only if the dataSize is 0, nothing is pushed to the stack, and at endIterationGroup, nothing is popped.
    * @return the number of iterations in the iterationGroup, possibly 0.
    */
   public int onParentIteration()
@@ -141,7 +142,7 @@ public class QueryExecution
   }
 
   /** Pop the previous currentListData from the currentDataSet and check if there are more iterations to be made within the iterationGroup. 
-   * If there are, add the currentListData to the currentDataSet
+   * If there are, add the currentListData to the currentDataSet; else push a DUMMY data (cfr bug 555).
    * @return the current iteration position, relative to the start of the interationGroup or -1 if there are no more results
    */
   public int nextGroupIteration()
@@ -149,11 +150,24 @@ public class QueryExecution
     valueQueryData.clear();
     currentDataSet.pop();
     iteration++;
-    if(iteration==iterationGroupData.size())
+    if(iteration==iterationGroupData.size()){
+      currentDataSet.push(NOTHING); // push a dummy Data; will be removed by endIterationGroup
       return -1;
+    }
     currentDataSet.push(currentListData());
     return iteration;
   }
+
+
+  /** Pops the the last data of an iterationGroup. 
+   *  In case of a empty-body/simple QueryTag, this will pop the first element; otherwise it pops the dummy data. cfr Bug 555.  
+   */
+  public void endIterationGroup(){
+    if (dataSize() > 0) {
+        currentDataSet.pop() ;
+    }
+  }
+
   
   /** The data of the current iteration; i.e. the currentListData */
   public ArrayMap currentListData(){ return (ArrayMap)iterationGroupData.elementAt(iteration); }
