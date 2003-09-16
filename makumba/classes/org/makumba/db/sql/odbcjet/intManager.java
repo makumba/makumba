da50ee357538f34884c5f746d30a12b218d64165
Manuel Bernhardt <manuel@bernhardt.io>
2003-09-16 22:20:18 +0000

    This commit was manufactured by cvs2svn to create tag 'makumba-0_5_9_12'.

--- makumba/classes/org/makumba/db/sql/odbcjet/intManager.java
+++ makumba/classes/org/makumba/db/sql/odbcjet/intManager.java
@@ -26,11 +26,10 @@ package org.makumba.db.sql.odbcjet;
 /** odbcjet might represent int as double */
 public class intManager extends org.makumba.db.sql.intManager
 {
-  protected boolean unmodified(int type, int size, java.util.Vector columns, 
-			       int index)
+  protected boolean unmodified(java.sql.ResultSetMetaData rsm, int index)
        throws java.sql.SQLException
   {
-    return super.unmodified(type, size, columns, index) || 
-      type==java.sql.Types.DOUBLE;
+    return super.unmodified(rsm, index) ||
+      rsm.getColumnType(index)==java.sql.Types.DOUBLE;
   }
 }
