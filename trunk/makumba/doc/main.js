857617d22ca17bdc2ce290712d5a6bf7f8c50910
stefanba <stefanba@1c21897b-c138-0410-a5df-bc8783e84eff>
2003-09-24 00:29:39 +0000

    common footer

--- trunk/makumba/doc/main.js
+++ trunk/makumba/doc/main.js
@@ -27,3 +27,32 @@ function makeNavBar(pathToBase)
 
 	document.write('</div>');
 }
+
+
+/* Takes cvs "$Id$" */
+/*   or "$Header$" */
+/*   tag and breaks it down to basic parts */
+function getPageInfo(cvsID)
+{
+	//trim it
+	cvsID=cvsID.substring(cvsID.indexOf(": ")+2,cvsID.lastIndexOf(" $"));
+	//split it at spaces
+	var cvsIds=cvsID.split(" ");
+	//take needed parts
+	var filename=cvsIds[0].substring(0,cvsIds[0].indexOf(","));
+	var revision=cvsIds[1];
+	var date=cvsIds[2].replace("/","-").replace("/","-");
+	var time=cvsIds[3];
+	var author=cvsIds[4];
+
+	return(''+filename+', revision '+revision+', last modified on '+date+' at '+time+' by '+author);
+
+}
+
+function makeFooter(cvsID)
+{
+	document.write('<div class="pagefooter">');
+	document.write(getPageInfo(cvsID));
+	document.write('</div>');
+
+}
