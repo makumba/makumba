52d9ca8d3699ec9057a9de2d1528ff2af2a8e245
stefanba <stefanba@1c21897b-c138-0410-a5df-bc8783e84eff>
2003-10-09 09:39:05 +0000

    rearanged menu

--- trunk/makumba/doc/main.js
+++ trunk/makumba/doc/main.js
@@ -11,18 +11,20 @@ function makeNavBar(pathToBase)
 	document.write('<a href="'+bDir+'whitepaper.html">Whitepaper</a><br>');
 	document.write('<a href="'+bDir+'makumba-spec.html">Specification</a><br>');
 	document.write('<a href="'+bDir+'makumba-example.html">Example</a><br>');
-	document.write('<a href="'+bDir+'Makumba-reference.pdf">Taglib Reference(pdf)</a><br>');
+	document.write('<a href="'+bDir+'Makumba-reference.pdf">Reference PDF</a><br>');
 	document.write('<a href="'+bDir+'api/org/makumba/package-summary.html">API docs</a><a href="api/org/makumba/package-summary.html"></a><br>');
 	document.write('<a href="'+bDir+'download.html">Download</a><br>');
 	document.write('<a href="'+bDir+'faq.html">FAQ</a><br>');
-	document.write('<a href="'+bDir+'SQL-drivers.html">Database configuration</a><br>');
+	document.write('<a href="'+bDir+'SQL-drivers.html">DB config</a><br>');
 
 	document.write('<br>&nbsp;<br>');
 
-	document.write('<a href="'+bDir+'hacking/index.html">Developer</a><br>');
-	document.write('<a href="'+bDir+'makumba.html">Design Issues</a><br>');
+	//document.write('<img src="'+bDir+'gears.gif" align="right">');
+	document.write('<a href="http://bugzilla.makumba.org/query.cgi?product=Makumba">Bugzilla</a><br>');
 	document.write('<a href="'+bDir+'tasks.html">Tasks</a><br>');
-	document.write('<a href="http://cvs.makumba.org/">CVS source tree</a><br>');
+	document.write('<a href="'+bDir+'makumba.html">Design Issues</a><br>');
+	document.write('<a href="'+bDir+'hacking/index.html">Developer doc</a><br>');
+	document.write('<a href="http://cvs.makumba.org/">CVS repository</a><br>');
 
 
 	document.write('</div>');
