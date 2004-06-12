147d531781fbb18f454a927d5ae6641809b0e715
adlerscout <adlerscout@1c21897b-c138-0410-a5df-bc8783e84eff>
2004-06-12 20:32:40 +0000

    New version of Multipart that doesn't create a byte[] with the size of the file. It should solve the problem of big uploads.
    I run a couple of tests and it seems to work but more test is needed.
    There are some println in the code for testing purposes.

--- trunk/makumba/classes/org/makumba/controller/http/MultipartHttpParameters.java
+++ trunk/makumba/classes/org/makumba/controller/http/MultipartHttpParameters.java
@@ -1,4 +1,4 @@
-///////////////////////////////
+//////////////////////////////////
 //  Makumba, Makumba tag library
 //  Copyright (C) 2000-2003  http://www.makumba.org
 //
@@ -22,18 +22,37 @@
 /////////////////////////////////////
 
 package org.makumba.controller.http;
-import javax.servlet.http.*;
-import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Hashtable;
+import java.util.Vector;
+import java.util.Enumeration;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.PushbackInputStream;
+import org.makumba.Text;
+import org.makumba.util.BoundaryInputStream;
+
+/* to delete after testing */
+/* 
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.util.Date;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+*/
+/* - end to delete - */
 
 /** Parse the input stream of a http request as a multipart/form-data. 
- * Store uploaded files as org.makumba.Text. Normal http parameters are stored as Strings (simple) or Vectors (multiple) 
+ *  Store uploaded files as org.makumba.Text.
+ *  Normal http parameters are stored as Text.toString (simple) or Vectors (multiple) 
  */
 public class MultipartHttpParameters extends HttpParameters
 {
   Hashtable parameters=new Hashtable();
 
   void computeAtStart(){}
-
+    
   public boolean knownAtStart(String s)
   {
     return parameters.get(s)!=null;  
@@ -42,113 +61,172 @@ public class MultipartHttpParameters extends HttpParameters
   public MultipartHttpParameters(HttpServletRequest req)
   {
     super(req);
-
-    byte [] bytes;
-
-    // the index needed to go thru the request
-    int index=0; 
-
-    // first we copy the request in an array
-    bytes= new byte[request.getContentLength()];
-
-    int bytesRead=0;
-    for(int totalRead=0; totalRead < bytes.length; totalRead+=bytesRead )
-      try{
-      bytesRead=request.getInputStream().read(bytes, totalRead, bytes.length - totalRead);
-      if(bytesRead==-1)
-	throw new org.makumba.MakumbaError("attempt to read the multipart stream twice during one request");
-    }catch(Exception e){throw new org.makumba.MakumbaError(e); }
     
-    // now we find out the multipart boundary
-    for(; index < bytes.length && bytes[index]!=13; index++);
-    
-    String boundary= new String(bytes, 0, index-1);
-    
-    index++;
-    
-    // now we go thru each section (file or http parameter)
-    while(index<bytes.length)
-      {
-	// determine a header
-	int h=index;
-	while(bytes[index] != 13 ||  bytes[index + 2] != 13)
-	  index++;
+    System.out.println("\n\n---- testing new code with Boundary ------\n");
+    try{  // testing the BIS
+
+	//System.out.println("DEBUG-mul: contentType: ["+request.getContentType()+"]");
+	int index_b = request.getContentType().indexOf("boundary=");
+	String boundaryWithoutMark = request.getContentType().substring(index_b+"boundary=".length());
+	//System.out.println("DEBUG-mul: boundary without -- ["+boundaryWithoutMark+"]");
+	//as specified in the RFCs the boundary in the parts has '--' in the beginning
+	String boundary = "--".concat(boundaryWithoutMark);
 	
-	String header = new String(bytes, h, index-h);
-	index+=4;
+	PushbackInputStream pis = new PushbackInputStream(request.getInputStream(), org.makumba.Text.FILE_LIMIT);
 
-	// determine the http parameter name
-	String name="";
-	int n= header.indexOf(" name"+"=\"");
-	if(n!=-1)
-	  name=header.substring(n+7, header.indexOf("\"", n+7));
+	//read the first line that countains just the boundary
+	new Text(new BoundaryInputStream(pis, boundary));
+	int var1 = pis.read();
+	int var2 = pis.read();
+	if (var1 != 13 || var2 != 10)
+	    System.out.println("*DEBUG-mul: ERROR reading CRLF after the 1st boundary ->["+var1+"]["+var2+"]");
 
-	// read the rest of the section until the boundary
-	int j=0;
-	int start=index;
-	int end=0;
-        while(index<bytes.length)
-        {
-	  if(bytes[index] == (byte)boundary.charAt(j))
-            {
-	      if(j==boundary.length()-1)
-                {
-		  end = index - boundary.length() - 1;
-		  break;
-                }
-	      index++;
-	      j++;
-            } 
-	  else
-            {
-	      index++;
-	      j=0;
-            }
-        } 
-	index++;
-	
-       
-	// if it's  a file, we store it as Text
-	n=header.indexOf("filename");
-	if(n !=-1)
-	  {
-	    String fn=header.substring(n+10, header.indexOf("\"", n+10));
-	    if(fn.indexOf("\\")!=-1)
-	      fn=fn.substring(fn.lastIndexOf("\\")+1);
-	    else
-	      if(fn.indexOf("/")!=-1)
-		fn=fn.substring(fn.lastIndexOf("/")+1);
-	    parameters.put(name+"_filename", fn);
-			   
-	    n= header.indexOf("Content-Type:");
-	    if(n!=-1)
-	      {
-		String type= header.substring(n+13).trim();
+	//go through all the request
+	while(pis.available()>0) {
+    
+	    StringBuffer header = new StringBuffer();
+	    int NrBytesRead = 0;
+	    boolean emptyLineFound = false;
+	    int byteRead; int x;
+	    boolean endOfRequest = false;
+
+	    while ( (byteRead = pis.read()) != -1) {
+		//---- reading the header -------
+		header.append((char)byteRead);
+		NrBytesRead++;
+		
+		//empty line: when we find 13 10 13 10 (CR LF CR LF)
+		if (NrBytesRead > 2) {
+
+		    if (byteRead == 13 && (header.charAt(NrBytesRead-2)) == 10 && (header.charAt(NrBytesRead-3)) == 13 ) {
+			if ( (x = pis.read()) != 10)
+			    throw new org.makumba.MakumbaError("Multipart Not recognized"); //read the ´LF' that comes after the 'CR' (13)
+			//System.out.println("DEBUG-mul: ending of the header+empty Char["+
+			//(int)header.charAt(NrBytesRead-3)+"]["+(int)header.charAt(NrBytesRead-2)+"] and ["+(int)header.charAt(NrBytesRead-1)+"]["+x+"]");
+			
+			header.deleteCharAt(NrBytesRead-1); //remove last CR
+			header.deleteCharAt(NrBytesRead-2); //remove LF
+			header.deleteCharAt(NrBytesRead-3); //remove first CR
+			//System.out.println("DEBUG-mul: removed CRLF from the ending of the header");
+			System.out.println("DEBUG-mul: header ["+header+"]");
+			emptyLineFound = true;
+		    }
+		}
+		if (emptyLineFound) break;
+		
+		//test if it's the end of the request
+		if (NrBytesRead == 2)
+		    if (byteRead == '-' && (header.charAt(NrBytesRead-1)) == '-' ) {
+			//read the last CR LF after the ending '--'
+			pis.read(); pis.read();
+			endOfRequest = true;
+		    }
+	    }//---- end of reading the header
+
+	    if (!endOfRequest) {
+		//---- processing the header -----
+		String name="";
+		int index_h = header.indexOf(" name=\"");
+		if (index_h!=-1) {
+		    int beginIndex = index_h + " name=\"".length();
+		    name = header.substring(beginIndex, header.indexOf("\"", beginIndex));
+		} else System.out.println("DEBUG-mul: ------ no \"name\" in the header was found \n\n");
+		
+		//----------- check headers and save ---------------
+		// if it's  a file, we store it as Text
+
+		index_h = header.indexOf("filename=\"");
+		if (index_h != -1) {//fileName
+		    int beginIndex = index_h + "filename=\"".length();
+		    String fileName = header.substring(beginIndex, header.indexOf("\"", beginIndex));
+
+		    // -- remove / and \ that appear in some Operative Systems
+		    if (fileName.indexOf("\\") != -1) {
+			fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
+		    }
+		    else if(fileName.indexOf("/") != -1)
+			fileName=fileName.substring(fileName.lastIndexOf("/") + 1);
+
+		    parameters.put(name+"_filename", fileName);		    
+		    
+		    index_h = header.indexOf("Content-Type:");
+		    // some browsers Safari (Mac), Netscape in SUNs don't set Content-Type
+		    // for some files e.g.: .cab (in both) and .class (in the SUN)
+		    if(index_h != -1) {
+			beginIndex = index_h + "Content-Type:".length();
+			String type= header.substring(beginIndex).trim();
+			//System.out.println("DEBUG-mul: Content-Type ["+type+"]");
+
+			parameters.put(name+"_contentType", type);
+			
+			if(type.indexOf("application/x-macbinary") != -1) {
+			    //throw new org.makumba.MakumbaError("upload is not supported for Apple Mac");
+			    System.out.println("\n\nDEBUG-mul: -#######- Mac upload -######- \n");
+			    // adler: this code was copied from the previous version
+			    // it seems to work in the tests I did
+			    byte[] temp_buff = new byte[128];
+			    pis.read(temp_buff, 0, 128);
+			}
+			Text contentToSave = new Text(new BoundaryInputStream(pis, boundary));
+			int contentSize = contentToSave.length();
+// ----------
+/*
+  Format formatter = new SimpleDateFormat("mm.ss");
+  String check_time = formatter.format(new Date());
+  System.out.println("DEBUG-mulX: contentSize = "+contentSize);
+  OutputStream fileout = new FileOutputStream("public_html/testfiles/n1-"+check_time+"-"+fileName);
+  contentToSave.writeTo(fileout);
+*/
+// ----------
+
+                        parameters.put(name+"_contentLength", new Integer(contentSize));
+			parameters.put(name, contentToSave);
+		    } else // there is no Content-Type 
+			if ( fileName.length() > 0 ) {
+			    System.out.println("DEBUG-mul: no Content-Type found but there might be some content");
+			    parameters.put(name+"_contentType", "text/plain"); //not sure which type should I put
+			    Text contentToSave = new Text(new BoundaryInputStream(pis, boundary));
+			    int contentSize = contentToSave.length();
+
+// ----------
+/*
+  Format formatter = new SimpleDateFormat("mm.ss");
+  String check_time = formatter.format(new Date());
+  System.out.println("DEBUG-mulX: contentSize = "+contentSize);
+  OutputStream fileout = new FileOutputStream("public_html/testfiles/n2-"+check_time+"-"+fileName);
+  contentToSave.writeTo(fileout);
+*/
+// ----------
+
+			    parameters.put(name+"_contentLength", new Integer(contentSize));
+			    System.out.println("DEBUG-mul: no content type found but file with size = "+contentSize);
+			    parameters.put(name, contentToSave);
+			}
+			else {
+			    // no content type & no filename in the header
+			    // not sure when this happens...
+			    System.out.println("* * DEBUG-mul: no Content-Type found and no filename * *");
+			    parameters.put(name+"_contentType", "");
+			    parameters.put(name+"_contentLength", new Integer(0));
+			    parameters.put(name, org.makumba.Pointer.NullText);
+			}
+		    
+		} // end if "filename"
+		// if it's a string parameter we store it as String
+		else addParameter(name, new Text(new BoundaryInputStream(pis, boundary)).toString());
+		
+	    } // end if (!endOfRequest)
 	    
-		parameters.put(name+"_contentType", type);
-		if(type.indexOf("application/x-macbinary") !=-1)
-		  start+=128;
-		parameters.put(name+"_contentLength", new Integer(end-start));
-		parameters.put(name, new org.makumba.Text(bytes, start, end-start));
-	      }
-	    else
-	      {
-		// no content type -> no content
-		parameters.put(name+"_contentType", "");
-		parameters.put(name+"_contentLength", new Integer(0));
-		parameters.put(name, org.makumba.Pointer.NullText);
-	      }
-	  }
-	else
-	  // if it's a string parameter we store it as String
-	  addParameter(name, new String(bytes, start, (end- start)));
+	}//end of while(pis.available())
 	
-	// are we at the end?
-	if((char)bytes[index + 1] == '-')
-	  break;
-	index+=2;
-      }
+    }
+    catch (IllegalArgumentException e) { System.out.println("DEBUG-mul: -- end: nothing to push back");}
+    catch(Throwable t){t.printStackTrace();}
+
+    //System.out.println("\n\n\n---- END new code with Boundary ------\n");
+
   }
+
 
   void addParameter(String name, String value)
   {
@@ -166,6 +244,7 @@ public class MultipartHttpParameters extends HttpParameters
     else
       parameters.put(name, value);
   }
+
   
   /** we compose what we read from the multipart with what we have in the query string.
    * the assumption is that the multipart cannot change during execution, while the query string may change due to e.g. forwards
@@ -209,4 +288,18 @@ public class MultipartHttpParameters extends HttpParameters
 	  return v;
 	}
   }
+  
 }
+
+/* data inside the request
+ * - 1st line: boundary CR+LF
+ * - header & values CR+LF (e.g. Content-Type: application/octec-stream)
+ * - CR+LF
+ * - content (related to the header just read)
+ * - CR+LF
+ * - boundary CR+LF
+ * - header... and so forth
+ * ...
+ * 
+ * and after the last boundary you will have '--' with CR+LF
+ */
