841f7e086361034489cd1dd3ee817031cd1588e9
adlerscout <adlerscout@users.noreply.github.com>
2004-06-13 23:05:22 +0000

    missing one import

--- makumba/classes/org/makumba/Text.java
+++ makumba/classes/org/makumba/Text.java
@@ -26,6 +26,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import org.makumba.util.LongData;
 import org.makumba.util.RuntimeWrappedException;
 
