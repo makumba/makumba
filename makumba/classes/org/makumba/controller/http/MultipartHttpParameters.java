18fb52bf3ae951146da3295a23254c325305e083
adlerscout <adlerscout@users.noreply.github.com>
2004-06-16 11:25:40 +0000

    fixed the problem with net congestion + (some) tests done

--- makumba/classes/org/makumba/controller/http/MultipartHttpParameters.java
+++ makumba/classes/org/makumba/controller/http/MultipartHttpParameters.java
@@ -22,17 +22,14 @@
 /////////////////////////////////////
 
 package org.makumba.controller.http;
-import javax.servlet.http.HttpServletRequest;
+import java.io.PushbackInputStream;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
-import java.util.Enumeration;
-import java.io.InputStream;
-import java.io.IOException;
-import java.io.ByteArrayInputStream;
-import java.io.PushbackInputStream;
+import javax.servlet.http.HttpServletRequest;
+import org.makumba.MakumbaSystem;
 import org.makumba.Text;
 import org.makumba.util.BoundaryInputStream;
-import org.makumba.MakumbaSystem;
 
 /** Parse the input stream of a http request as a multipart/form-data. 
  *  Store uploaded files as org.makumba.Text.
@@ -72,18 +69,17 @@ public class MultipartHttpParameters extends HttpParameters
 	    MakumbaSystem.getMakumbaLogger("fileUpload").severe("Mul: ERROR reading CRLF after the 1st boundary ["+var1+"]["+var2+"]");
 
 	//go through all the request
-	//TODO: Cristi: "if there is a net congestion, available() returns 0, while read()
-	// blocks until more bytes come. so with available() there is the risk that,
-	// on net congestion, you finish too early. i'm not sure. didn't use available()
-	// so much, but i think it's made to provide a non-blocking alternative to read()"
-	while(pis.available()>0) {
+	endOfRequest:
+	while(true) {
 	    
 	    StringBuffer headers = new StringBuffer();
 	    int NrBytesRead = 0;
 	    int byteRead; int x;
-	    boolean endOfRequest = false;
 	    
-	    while ( (byteRead = pis.read()) != -1) {
+	    while(true) {
+		if ((byteRead = pis.read()) == -1)
+		    break endOfRequest;
+
 		//---- reading the headers -------
 		headers.append((char)byteRead);
 		NrBytesRead++;
@@ -91,10 +87,14 @@ public class MultipartHttpParameters extends HttpParameters
 		//empty line: when we find 13 10 13 10 (CR LF CR LF)
 		if (NrBytesRead > 2) {
 		    
-		    if (byteRead == 13 && (headers.charAt(NrBytesRead-2)) == 10 && (headers.charAt(NrBytesRead-3)) == 13 ) {
-			if ( (x = pis.read()) != 10)
-			    throw new org.makumba.MakumbaError("Multipart Not recognized"); //read the ³LF' that comes after the 'CR' (13)
-			
+		    if (byteRead == 13 && (headers.charAt(NrBytesRead-3)) == 13 ) {
+			x = pis.read(); //if  != 10
+			// some browsers send CR LF CR CR so no -> throw new org.makumba.MakumbaError("Multipart Not recognized");
+			MakumbaSystem.getMakumbaLogger("fileUpload").fine("MUL-endOfHeaders: ["+
+									  (int)headers.charAt(NrBytesRead-3)+"|"+
+									  (int)headers.charAt(NrBytesRead-2)+"] ["+
+									  byteRead+"|"+
+									  x+"]");
 			headers.deleteCharAt(NrBytesRead-1); //remove last CR
 			headers.deleteCharAt(NrBytesRead-2); //remove LF
 			headers.deleteCharAt(NrBytesRead-3); //remove first CR
@@ -109,86 +109,84 @@ public class MultipartHttpParameters extends HttpParameters
 		    if (byteRead == '-' && (headers.charAt(NrBytesRead-1)) == '-' ) {
 			//read the last CR LF after the ending '--'
 			pis.read(); pis.read();
-			endOfRequest = true;
+			break endOfRequest;
 		    }
-	    }//---- end of reading the headers
+	    } //---- end of reading the headers
+
+	    //---- processing the headers -----
+	    String name="";
+	    int index_h = headers.indexOf(" name=\"");
+	    if (index_h!=-1) {
+		int beginIndex = index_h + " name=\"".length();
+		name = headers.substring(beginIndex, headers.indexOf("\"", beginIndex));
+	    } else MakumbaSystem.getMakumbaLogger("fileUpload").warning("DEBUG-mul: ------ no \"name\" in the headers was found \n\n");
 	    
-	    if (!endOfRequest) {
-		//---- processing the headers -----
-		String name="";
-		int index_h = headers.indexOf(" name=\"");
-		if (index_h!=-1) {
-		    int beginIndex = index_h + " name=\"".length();
-		    name = headers.substring(beginIndex, headers.indexOf("\"", beginIndex));
-		} else MakumbaSystem.getMakumbaLogger("fileUpload").warning("DEBUG-mul: ------ no \"name\" in the headers was found \n\n");
+	    //----------- check headers and save ---------------
+	    // if it's  a file, we store it as Text
+	    
+	    index_h = headers.indexOf("filename=\"");
+	    if (index_h != -1) {//fileName
+		int beginIndex = index_h + "filename=\"".length();
+		String fileName = headers.substring(beginIndex, headers.indexOf("\"", beginIndex));
 		
-		//----------- check headers and save ---------------
-		// if it's  a file, we store it as Text
+		// -- remove / and \ that appear in some Operative Systems
+		if (fileName.indexOf("\\") != -1) {
+		    fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
+		}
+		else if(fileName.indexOf("/") != -1)
+		    fileName=fileName.substring(fileName.lastIndexOf("/") + 1);
 		
-		index_h = headers.indexOf("filename=\"");
-		if (index_h != -1) {//fileName
-		    int beginIndex = index_h + "filename=\"".length();
-		    String fileName = headers.substring(beginIndex, headers.indexOf("\"", beginIndex));
+		if (fileName.length() > 0) {
+		    String type;
+		    Text contentToSave;
+		    int contentSize;
 		    
-		    // -- remove / and \ that appear in some Operative Systems
-		    if (fileName.indexOf("\\") != -1) {
-			fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
-		    }
-		    else if(fileName.indexOf("/") != -1)
-			fileName=fileName.substring(fileName.lastIndexOf("/") + 1);
-		    
-		    if (fileName.length() > 0) {
-			String type;
-			Text contentToSave;
-			int contentSize;
-
-			// --- check Content-Type ---
-			index_h = headers.indexOf("Content-Type:");
-			// some browsers Safari (Mac), Netscape in SUNs don't set Content-Type
-			// for some files e.g.: .cab (in both) and .class (in the SUN)
-			if(index_h != -1) {
-			    beginIndex = index_h + "Content-Type:".length();
-			    type = headers.substring(beginIndex).trim();
-			    
-			    if(type.indexOf("application/x-macbinary") != -1) {
-				MakumbaSystem.getMakumbaLogger("fileUpload").fine("Mac upload: application/x-macbinary");
-				// adler: this code was copied from the previous version
-				// it seems to work in the tests I did
-				byte[] temp_buff = new byte[128];
-				pis.read(temp_buff, 0, 128);
-			    }
-
-			} else {
-			    type = "application/octet-stream";
-			    MakumbaSystem.getMakumbaLogger("fileUpload").warning("no Content-Type found but there might be some content");
-			}
-
-			// ---- read the content and set parameters
-			contentToSave = new Text(new BoundaryInputStream(pis, boundary));
-			contentSize = contentToSave.length();
+		    // --- check Content-Type ---
+		    index_h = headers.indexOf("Content-Type:");
+		    // some browsers Safari (Mac), Netscape in SUNs don't set Content-Type
+		    // for some files e.g.: .cab (in both) and .class (in the SUN)
+		    if(index_h != -1) {
+			beginIndex = index_h + "Content-Type:".length();
+			type = headers.substring(beginIndex).trim();
 			
-			parameters.put(name+"_contentType", type);
-			parameters.put(name+"_filename", fileName);
-			parameters.put(name+"_contentLength", new Integer(contentSize));
-			parameters.put(name, contentToSave);
-
-			MakumbaSystem.getMakumbaLogger("fileUpload").fine("Parameters set: contentType="+type
-									  +", fileName="+fileName+", contentSize="+contentSize);
-		    } else { //no file -> nothing to upload
-			Text checkContent = new Text(new BoundaryInputStream(pis, boundary)); //to read the boundary
-			if (checkContent.length() > 0)
-			    MakumbaSystem.getMakumbaLogger("fileUpload").warning("no filename in the headers but there was some content (size="+checkContent.length()+", that was not put in the parameters");
-			parameters.put(name+"_contentType", "");
-			parameters.put(name+"_filename", "");
-			parameters.put(name+"_contentLength", new Integer(0));
-			parameters.put(name, org.makumba.Pointer.NullText);
+			if(type.indexOf("application/x-macbinary") != -1) {
+			    MakumbaSystem.getMakumbaLogger("fileUpload").fine("Mac upload: application/x-macbinary");
+			    // adler: this code was copied from the previous version
+			    // it seems to work in the tests I did
+			    byte[] temp_buff = new byte[128];
+			    pis.read(temp_buff, 0, 128);
+			}
+			
+		    } else {
+			type = "application/octet-stream";
+			MakumbaSystem.getMakumbaLogger("fileUpload").warning("no Content-Type found but there might be some content");
 		    }
-		} else // no filename so it is a string parameter. We store it as String
-		    addParameter(name, new Text(new BoundaryInputStream(pis, boundary)).toString());
+		    
+		    // ---- read the content and set parameters
+		    contentToSave = new Text(new BoundaryInputStream(pis, boundary));
+		    contentSize = contentToSave.length();
+		    
+		    parameters.put(name+"_contentType", type);
+		    parameters.put(name+"_filename", fileName);
+		    parameters.put(name+"_contentLength", new Integer(contentSize));
+		    parameters.put(name, contentToSave);
+		    
+		    MakumbaSystem.getMakumbaLogger("fileUpload").fine("Parameters set: contentType="+type
+								      +", fileName="+fileName+", contentSize="+contentSize);
+		} else { //no file -> nothing to upload
+		    Text checkContent = new Text(new BoundaryInputStream(pis, boundary)); //to read the boundary
+		    if (checkContent.length() > 0)
+			MakumbaSystem.getMakumbaLogger("fileUpload").warning("no filename in the headers but there was some content (size="+checkContent.length()+", that was not put in the parameters");
+		    parameters.put(name+"_contentType", "");
+		    parameters.put(name+"_filename", "");
+		    parameters.put(name+"_contentLength", new Integer(0));
+		    parameters.put(name, org.makumba.Pointer.NullText);
+		}
+	    } else // no filename so it is a string parameter. We store it as String
+		addParameter(name, new Text(new BoundaryInputStream(pis, boundary)).toString());
 	    
-	    } // end if (!endOfRequest)
-	
-	}//end of while(pis.available())
+	}//end of while(true)
+
     } catch(Exception e){throw new org.makumba.MakumbaError(e); }
   }//end of the method MultipartHttpParameters
 
@@ -259,7 +257,7 @@ public class MultipartHttpParameters extends HttpParameters
 /* data inside the request
  * - 1st line: boundary + CR+LF
  * - headers & values + CR+LF (e.g. filename="file.doc" Content-Type: application/octec-stream)
- * - CR+LF
+ * - CR+LF (Konqueror 3.2.1 sends CR CR)
  * - content (related to the headers just read)
  * - CR+LF
  * - boundary CR+LF
