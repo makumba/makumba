cb38b1a5b257845426aa65a809dc15cb2eae0139
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2004-08-15 16:46:57 +0000

    This commit was manufactured by cvs2svn to create tag 'makumba-0_5_11_2'.

---
+++ tags/makumba-0_5_11_2/makumba/classes/org/makumba/db/sql/cloudscape/textManager.java
@@ -0,0 +1,32 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.db.sql.cloudscape;
+
+/** this deals with SQL uncontrolled-length fields */
+public class textManager extends org.makumba.db.sql.textManager
+{
+  /** ask this field to write its contribution in a SQL CREATE statement */
+  public String inCreate(Database d){ return getDBName()+" "+getDBType(d)+"(1024000)";}
+
+}
---
+++ tags/makumba-0_5_11_2/makumba/classes/org/makumba/db/sql/realManager.java
@@ -0,0 +1,42 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.db.sql;
+
+/** this deals with SQL double precision numeric fields */
+public class realManager extends FieldManager
+{
+  
+  protected int getSQLType()
+  {
+    return java.sql.Types.DOUBLE;
+  }
+
+  /** Use standard SQL name, unless defined otherwise in sqlEngines.properties. */
+  protected String getDBType()
+  {
+      return "DOUBLE PRECISION"; //standard name
+  }
+
+
+}
