ea8e4e30be42da40d94a0300fdd06bb384736317
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2005-01-29 23:21:05 +0000

    Added search for classes the error occurs in, link is provided when successful.

--- makumba/classes/org/makumba/devel/errorViewer.java
+++ makumba/classes/org/makumba/devel/errorViewer.java
@@ -22,28 +22,107 @@
 /////////////////////////////////////
 
 package org.makumba.devel;
-import java.io.*;
-import javax.servlet.http.*;
 
-/** the error viewer. To be used from TagExceptionServlet.*/
-public class errorViewer extends LineViewer
-{
-  String hBody;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.util.StringTokenizer;
 
-  public errorViewer(HttpServletRequest req, HttpServlet sv, String title, String body, String hiddenBody) throws IOException
-  {
-    super(false);
-    contextPath=req.getContextPath();
-    super.title=title;
-    hBody=hiddenBody;
-    reader=new StringReader(body);
-  }
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
-  void footer(PrintWriter pw) throws IOException
-  {
-      if (hBody!=null)
-	pw.println("<!--\n"+hBody+"\n-->");
-      super.footer(pw);
-  }
+import org.makumba.view.jsptaglib.TomcatJsp;
+
+/**
+ * the error viewer. To be used from TagExceptionServlet.
+ * 
+ * @version $ID $
+ * @author Stefan Baebler
+ * @author Rudolf Mayer
+ *  
+ */
+public class errorViewer extends LineViewer {
+    private String hiddenBody;
+
+    public errorViewer(HttpServletRequest request, HttpServlet servlet, String title, String body, String hiddenBody)
+            throws IOException {
+        super(false, request, servlet);
+        realPath = servlet.getServletConfig().getServletContext().getRealPath("/");
+
+        jspClasspath = TomcatJsp.getContextCompiledJSPDir(servlet.getServletContext());
+        super.title = title;
+        this.hiddenBody = hiddenBody;
+        reader = new StringReader(body);
+    }
+
+    public String parseLine(String s) {
+        Class javaClass;
+        String jspPage;
+        String jspClass;
+        java.net.URL mdd;
+        java.net.URL idd;
+        String sunClass;
+
+        StringBuffer source = new StringBuffer(s);
+        StringBuffer result = new StringBuffer();
+
+        StringTokenizer tokenizer = getLineTokenizer(s);
+        while (tokenizer.hasMoreElements()) {
+            String token = tokenizer.nextToken();
+
+            int indexOf = source.indexOf(token);
+            int indexAfter = indexOf + token.length();
+
+            result.append(source.substring(0, indexOf));
+
+            if (token.indexOf(".") != -1) {
+
+                if (searchMDD && (mdd = org.makumba.abstr.RecordParser.findDataDefinition(token, "mdd")) != null
+                        || (idd = org.makumba.abstr.RecordParser.findDataDefinition(token, "idd")) != null) {
+                    result.append(formatMDDLink(token));
+                } else if (searchJavaClasses && (javaClass = findClass(token)) != null) {
+                    int indexNumberBegin = source.substring(indexAfter).indexOf(":") + 1;
+                    Integer lineNumber = null;
+                    if (indexNumberBegin != -1) {
+                        int indexEnd = source.substring(indexAfter).indexOf(")");
+                        String lineNumberText = source.substring(indexAfter).substring(indexNumberBegin, indexEnd);
+                        lineNumber = Integer.valueOf(lineNumberText);
+                    }
+                    result.append(formatClassLink(javaClass.getName(), token, lineNumber));
+                } else if ((sunClass = findJDKClass(token)) != null) {
+                    result.append(sunClass);
+                } else if (searchJSPPages && (jspPage = findPage(token)) != null) {
+                    result.append(formatJSPLink(jspPage, token, null));
+                } else if (searchCompiledJSPClasses && (jspClass = findCompiledJSP(token)) != null) {
+                    result.append(formatClassLink(jspClass, token, null));
+                } else {
+                    result.append(token);
+                }
+            } else {
+                result.append(token);
+            }
+            source.delete(0, indexOf + token.length());
+        }
+        return result.append(source).toString();
+    }
+
+    /**
+     * @param token
+     * @return
+     */
+    public Class findClass(String token) {
+        Class javaClass;
+        int index = token.lastIndexOf('.');
+        String className = token.substring(0, index);
+        javaClass = super.findClass(className);
+        return javaClass;
+    }
+
+    public void footer(PrintWriter pw) throws IOException {
+        if (hiddenBody != null)
+            pw.println("<!--\n" + hiddenBody + "\n-->");
+        super.footer(pw);
+    }
 
 }
+
