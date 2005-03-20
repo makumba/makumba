657c6c947ed547495b9970617659209cd8938e33
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2005-03-20 18:10:52 +0000

    added more methods, so dependence on RecordInfo could be removed

--- makumba/classes/org/makumba/DataDefinition.java
+++ makumba/classes/org/makumba/DataDefinition.java
@@ -43,21 +43,27 @@ public interface DataDefinition
   /** tells whether this data definition was generated temporarily to depict a query result as opposed to being read from an MDD file */
   public boolean isTemporary();
 
-  /** the title field indicated, or the default one */
+  /** The title field indicated, or the default one */
   public String getTitleFieldName();
 
-  /** which is the name of the index field (primary key), if any? */
+  /** The name of the index field (primary key), if any? */
   public String getIndexPointerFieldName();
 
-  /** which is the name of the creation timestamp field, if any? */
+  /** The name of the creation timestamp field, if any? */
   public String getCreationDateFieldName();
 
-  /** which is the name of the modification timestamp field, if any? */
+  /** The name of the modification timestamp field, if any? */
   public String getLastModificationDateFieldName();
 
   /** If this type is the data pointed to by a 1-1 pointer or subset, return the field definition in the main record, otherwise return null */
   public FieldDefinition getParentField();
 
-  /** which is the name of the set member (Pointer, Character or Integer), for set subtypes */
+  /** The name of the set member (Pointer, Character or Integer), for set subtypes */
   public String getSetMemberFieldName() ;
+
+  /** The name of the pointer to the main table, for set and internal set subtypes*/
+  public String getSetOwnerFieldName();
+
+  /** Add a new field definition. Works only for temporary data definitions */
+  public void addField(FieldDefinition fd);
 }
