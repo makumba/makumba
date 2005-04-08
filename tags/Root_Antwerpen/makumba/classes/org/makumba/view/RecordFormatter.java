e08fa0cc7a215bb42a91bfc7b62a62784c099fa7
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2005-04-08 10:51:56 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_Antwerpen'.

---
+++ tags/Root_Antwerpen/makumba/classes/org/makumba/view/RecordFormatter.java
@@ -0,0 +1,63 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.view;
+import java.util.Dictionary;
+
+import org.makumba.DataDefinition;
+import org.makumba.abstr.RecordHandler;
+
+public class RecordFormatter extends RecordHandler
+{
+  public RecordFormatter(DataDefinition ri) 
+  {
+    super(ri); 
+  }
+  
+  public RecordFormatter(ComposedQuery q) 
+  {
+    super((DataDefinition)q.getResultType()); 
+    for(int i=0; i<handlerOrder.size(); i++)
+      ((FieldFormatter)handlerOrder.elementAt(i)).initExpr(q.getProjectionAt(i));
+  }
+
+  public RecordFormatter(DataDefinition ri, java.util.Hashtable names) 
+  {
+    super(ri);
+    for(int i=0; i<handlerOrder.size(); i++)
+      {
+	FieldFormatter ff= (FieldFormatter)handlerOrder.elementAt(i);
+	ff.initExpr((String)names.get(ff.getName()));
+      }
+  }
+
+  protected String applyParameters(FieldFormatter ff, Dictionary formatParams, String s)
+  { return s; }
+
+  public String format(int i, Object value, Dictionary formatParams)
+  {
+    FieldFormatter ff= (FieldFormatter)handlerOrder.elementAt(i); 
+    ff.checkParams(formatParams);
+    return applyParameters(ff, formatParams, ff.format(value, formatParams));
+  }
+}
