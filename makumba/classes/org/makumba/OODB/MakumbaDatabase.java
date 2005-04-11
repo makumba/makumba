3086d375eecf5ef520b96b0f0a5204ad6a9ceea8
bvdvelde <bvdvelde@users.sourceforge.net>
2005-04-11 14:42:58 +0000

    step 1 bridge pattern db.sql
    moved the abstraction class and the interface for the implementation to .OODB

---
+++ makumba/classes/org/makumba/OODB/MakumbaDatabase.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 11-apr-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.makumba.OODB;
+
+/**
+ * @author Bart
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class MakumbaDatabase {
+
+}
--- makumba/classes/org/makumba/db/sql/informix/dateTimeManager.java
+++
@@ -1,9 +0,0 @@
-package org.makumba.db.sql.informix;
-
-public class dateTimeManager extends org.makumba.db.sql.dateTimeManager
-{
-  protected String getDBType()
-  {
-    return "DATETIME YEAR TO FRACTION";
-  }
-}
