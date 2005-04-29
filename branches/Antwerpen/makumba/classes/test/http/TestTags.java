b45f342972da68450bb39e46685a9f478c2c3cab
jpeeters <jpeeters@1c21897b-c138-0410-a5df-bc8783e84eff>
2005-04-29 20:46:23 +0000

    - Tests for Char and Date

--- branches/Antwerpen/makumba/classes/test/http/TestTags.java
+++ branches/Antwerpen/makumba/classes/test/http/TestTags.java
@@ -1,12 +1,13 @@
 /*
  * Created on Apr 29, 2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package test.http;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Dictionary;
@@ -28,8 +29,10 @@ import org.apache.cactus.WebResponse;
 import org.makumba.Database;
 import org.makumba.MakumbaSystem;
 import org.makumba.Pointer;
+import org.makumba.Text;
 import org.makumba.view.jsptaglib.MakumbaVersionTag;
 import org.makumba.view.jsptaglib.QueryTag;
+
 
 /**
  * @author jpeeters
@@ -40,16 +43,78 @@ public class TestTags extends JspTestCase {
 	static Pointer ptr;
 	static Dictionary pc;
 	static Vector v;
+	static String readPerson = "SELECT p.indiv.name AS name, p.indiv.surname AS surname, p.uniqChar AS uniqChar, p.birthdate AS birthdate, p.weight AS weight, p.TS_modify AS TS_modify, p.TS_create AS TS_create, p.comment AS comment FROM test.Person p WHERE p= $1";
 
+	static Object[][] languageData = { { "English", "en" }, { "French", "fr" },
+			{ "German", "de" }, { "Italian", "it" }, { "Spanish", "sp" } };
+	
 	public static Test suite() {
 		TestSetup setup = new TestSetup(new TestSuite(TestTags.class)) {
+			ArrayList ptrs = new ArrayList();
 			protected void setUp() {
 				// do your one-time setup here!
 				db = MakumbaSystem.getConnectionTo(MakumbaSystem.getDefaultDatabaseName("test/testDatabase.properties"));
+				insertPerson();
+				insertLanguages();				
 			}
+			
+			protected void insertPerson() {
+				Properties p = new Properties();
 
+				p.put("indiv.name", "john");
+				p.put("indiv.surname", "doe");
+				
+				Calendar c = Calendar.getInstance();
+				c.clear();
+				c.set(1977, 2, 5);
+				Date birthdate = c.getTime();
+				p.put("birthdate", birthdate);
+						
+				
+				p.put("gender", new Integer(1));
+				p.put("uniqChar", new String("testing \" character field"));
+				
+				p.put("weight", new Double(85.7d));
+				
+				p.put("comment", new Text("This is a text field. It's a comment about this person."));
+
+				p.put("uniqInt", new Integer(255));
+				
+				Vector intSet = new Vector();
+				intSet.addElement(new Integer(1));
+				intSet.addElement(new Integer(0));
+				p.put("intSet", intSet);
+				
+				Vector charSet = new Vector();
+				charSet.addElement("f");
+				charSet.addElement("e");		
+				p.put("charSet", charSet);
+
+				ptr = db.insert("test.Person", p);
+				db.commit();
+			}
+			protected void deletePerson() {
+				db.delete(ptr);
+			}
+			
+			protected void insertLanguages() {
+				Dictionary p = new Hashtable();				
+				for (int i = 0; i < languageData.length; i++) {
+					p.put("name", languageData[i][0]);
+					p.put("isoCode", languageData[i][1]);
+					ptrs.add(db.insert("test.Language", p));
+				}
+				db.commit();
+			}
+			protected void deleteLanguages() {
+				for (int i = 0; i<ptrs.size(); i++)
+					db.delete((Pointer)ptrs.get(i));
+			}
+			
 			protected void tearDown() {
 				// do your one-time tear down here!
+				deletePerson();
+				deleteLanguages();
 				db.close();
 			}
 		};
@@ -70,51 +135,19 @@ public class TestTags extends JspTestCase {
 		pageContext.popBody();
 	}
 
-	public void testMakObjectTag() throws ServletException, IOException {
-		Properties p = new Properties();
-		Calendar c = Calendar.getInstance();
-		c.clear();
-		c.set(1977, 2, 5);
-		Date birth = c.getTime();
-		String myComment = "this is the comment";
-
-		p.put("birthdate", birth);
-		p.put("comment", myComment);
-
-		p.put("weight", new Double(85.7d));
-
-		p.put("indiv.name", "john");
-		p.put("indiv.surname", "doe");
-		p.put("extraData.something", "else");
-
-		Vector setintElem = new Vector();
-		setintElem.addElement(new Integer(1));
-		setintElem.addElement(new Integer(0));
-
-		Vector setcharElem = new Vector();
-		setcharElem.addElement("f");
-		setcharElem.addElement("e");
-
-		p.put("intSet", setintElem);
-		p.put("charSet", setcharElem);
-
-		ptr = db.insert("test.Person", p);
-		db.commit();
+	public void testDataInput() throws JspException, JspException {
 		assertNotNull(ptr);
 		assertEquals(ptr.getType(), "test.Person");
-
-		QueryTag qt = new QueryTag();
-
-		pageContext.include("testMakObjectTag.jsp");
-
-		String readPerson = "SELECT p.indiv.name AS name, p.indiv.surname AS surname, p.birthdate AS birthdate, p.weight as weight, p.TS_modify as TS_modify, p.TS_create as TS_create, p.extraData.something as something, p.extraData as extraData, p.comment as comment FROM test.Person p WHERE p= $1";
-		v = db.executeQuery(readPerson, ptr);
-		db.delete(ptr);
-		db.commit();
 	}
-
+	
+	public void testMakObjectTag() throws ServletException, IOException {			
+		QueryTag makobject = new QueryTag();
+		pageContext.include("testMakObjectTag.jsp");		
+	}
 	public void endMakObjectTag(WebResponse response) {
 		String output = response.getText();
+		
+		v = db.executeQuery(readPerson, ptr);
 
 		assertEquals(1, v.size());
 		pc = (Dictionary) v.elementAt(0);
@@ -126,31 +159,10 @@ public class TestTags extends JspTestCase {
 				.indexOf("\r")));
 	}
 
-	static Object[][] languageData = { { "English", "en" }, { "French", "fr" },
-			{ "German", "de" }, { "Italian", "it" }, { "Spanish", "sp" } };
-
 	public void testMakListTag() throws ServletException, IOException {
-		Dictionary p = new Hashtable();
-		if (db.executeQuery("SELECT l FROM test.Language l", null).size() == 0)
-			for (int i = 0; i < languageData.length; i++) {
-				p.put("name", languageData[i][0]);
-				p.put("isoCode", languageData[i][1]);
-				db.insert("test.Language", p);
-			}
-		db.commit();
-		p = new Hashtable();
-
 		QueryTag maklist = new QueryTag();
-
 		pageContext.include("testMakListTag.jsp");
-
-		for (int i = 0; i < languageData.length; i++) {
-			db.delete("test.Language l", "l.name = '" + languageData[i][0]
-					+ "'", null);
-		}
-		db.commit();
 	}
-
 	public void endMakListTag(WebResponse response) {
 		String output = response.getText();
 		int i = 0, begin, end;
@@ -168,4 +180,40 @@ public class TestTags extends JspTestCase {
 		return;
 	}
 
+	public void testMakChar() throws ServletException, IOException {
+		QueryTag makobject = new QueryTag();
+		pageContext.include("testMakFieldTypes.jsp");		
+	}	
+	public void endMakChar(WebResponse response) {
+		String output = response.getText();
+		v = db.executeQuery(readPerson, ptr);
+		
+		pc = (Dictionary) v.elementAt(0);
+		
+		assertEquals(pc.get("name"), output.substring(output.indexOf("testName!")+9, output.indexOf("!endName")));
+		assertEquals(pc.get("surname"), output.substring(output.indexOf("testSurname!")+12, output.indexOf("!endSurname")));
+		assertEquals(((String)pc.get("uniqChar")).replaceAll("\"","&quot;"), output.substring(output.indexOf("testAllAuto!")+12, output.indexOf("!endAllAuto")));
+		assertEquals(pc.get("uniqChar"), output.substring(output.indexOf("testAllHtml!")+12, output.indexOf("!endAllHtml")));
+		assertEquals(((String)pc.get("uniqChar")).replaceAll("\"","&quot;"), output.substring(output.indexOf("testAllNoHtml!")+14, output.indexOf("!endAllNoHtml")));
+	}
+	
+	public void testMakDate() throws ServletException, IOException {
+		QueryTag makobject = new QueryTag();
+		pageContext.include("testMakFieldTypes.jsp");		
+	}	
+	public void endMakDate(WebResponse response) {
+		String output = response.getText();
+		String value;
+		v = db.executeQuery(readPerson, ptr);
+		
+		pc = (Dictionary) v.elementAt(0);
+		
+		DateFormat df = new SimpleDateFormat("dd MMMM yyyy");
+		assertEquals(df.format(pc.get("birthdate")), output.substring(output.indexOf("testBirthdate!")+14, output.indexOf("!endBirthdate")));
+		
+		df = new SimpleDateFormat("dd-mm-yy");
+		assertEquals(df.format(pc.get("birthdate")), output.substring(output.indexOf("testBirthdateFormat!")+20, output.indexOf("!testBirthdateFormat")));		
+	}
+		
+	
 }
