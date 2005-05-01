b8940189cc58bb2a2b0007a86880d67fa5bacda6
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2005-05-01 22:45:24 +0000

    This commit was manufactured by cvs2svn to create tag 'makumba-0_5_14_1'.

---
+++ tags/makumba-0_5_14_1/makumba/classes/org/makumba/abstr/FieldParser.java
@@ -0,0 +1,121 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.abstr;
+
+import org.makumba.DataDefinition;
+
+public class FieldParser extends FieldHandler
+{
+  FieldParser(){}
+  
+  FieldParser(FieldInfo fi)
+  {
+    this.fi= fi;
+  }
+ 
+  FieldParser parse(FieldCursor fc) throws org.makumba.DataDefinitionParseError
+  { 
+    while(true)
+    { 
+      if(fc.lookup("not"))
+      {
+        if(fi.notNull)
+          throw fc.fail("too many not null");
+        fc.expect("null");
+        fc.expectWhitespace();
+        fi.notNull= true;
+        continue;
+      }
+      
+      if(fc.lookup("fixed"))
+      {
+        fc.expectWhitespace();
+        if(fi.fixed)
+          throw fc.fail("too many fixed");
+        fi.fixed= true;
+        continue;
+      }
+
+      if(fc.lookup("unique"))
+      {
+        fc.expectWhitespace();
+        if(fi.unique)
+          throw fc.fail("already unique");
+        fi.unique= true;
+        continue;
+      }
+      
+      break;
+    }
+    
+    
+    FieldParser ret= setType(fc.expectTypeLiteral(), fc);
+    
+    if(ret == null) 
+      {
+        String s=fc.rp.definedTypes.getProperty(fi.type);
+        if(s==null)
+          throw fc.fail("unknown type: "+fi.type);
+        fc.substitute(fi.type.length(), s);
+        
+        ret= setType(fc.expectTypeLiteral(), fc);
+
+        if(ret== null)
+          throw fc.fail("unknown type: "+fi.type);
+      }
+    fi.description= fi.description==null?fi.name:fi.description;
+    return ret;
+  }
+
+  FieldParser setType(String s, FieldCursor fc) throws org.makumba.DataDefinitionParseError
+  {
+     fi.type=s;
+     FieldParser ret;
+     
+     ret= (FieldParser)fc.rp.makeHandler(fi.type);
+     if(ret==null)
+        return null;
+     ret.fi= fi;        
+     return ret.parse(fc);        
+  }
+
+  String addText(String nm, String origNm, String val)  
+  { return "subfield not allowed"; }  
+  
+  String acceptTitle(String nm, String origNm, String val, Object o) 
+  { 
+    val= val.trim();
+    if(nm.equals("!title"))
+    {
+        DataDefinition ri=(DataDefinition)o;
+        if(ri.getFieldDefinition(val)==null)
+	  return ri.getName()+ " has no field called "+ val;
+        fi.extra2=val;
+        return null;
+    }
+    return addText(nm, origNm, val);
+  }  
+  
+  public void parseSubfields() { }
+}
---
+++ tags/makumba-0_5_14_1/makumba/classes/org/makumba/util/ReadableFormatter.java
@@ -0,0 +1,144 @@
+package org.makumba.util;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * Formats argument in a readable (meaningful) way
+ * 
+ * @author Stefan Baebler
+ * @version $Id$
+ */
+
+public class ReadableFormatter {
+
+	/**
+	 * Formats byte size in nice format - a wrapper method
+	 * 
+	 * @param byteSize
+	 *            <code>String</code> representing the byte size
+	 * @see #readableBytes(long)
+	 * @return Formatted number of bytes (eg: empty, 15 B, 12kB, 821 kB, 3
+	 *         MB...) <br>
+	 *         "N/A" if <code>byteSize</code> is not a number <br>"" if
+	 *         <code>byteSize</code> is null
+	 */
+	public static String readableBytes(String byteSize) {
+		try {
+			return readableBytes((new Long(byteSize.toString())).longValue());
+		} catch (NumberFormatException nfe) {
+			return "N/A";
+		} catch (NullPointerException npe) {
+			return "";
+		}
+	}
+
+	/**
+	 * Formats byte size in nice format
+	 * 
+	 * @param byteSize
+	 *            the size in bytes to format
+	 * @see #readableBytes(String)
+	 * @return Formatted number of bytes (eg: empty, 15 B, 12kB, 821 kB, 3
+	 *         MB...)
+	 */
+	public static String readableBytes(long byteSize) {
+		if (byteSize < 0l)
+			return ("invalid");
+		if (byteSize < 1l)
+			return ("empty");
+
+		float byteSizeF = (new java.lang.Float(byteSize)).floatValue();
+		String unit = "bytes";
+		float factor = 1f;
+		String[] desc = { "B", "kB", "MB", "GB", "TB" };
+
+		java.text.DecimalFormat nf = new java.text.DecimalFormat();
+		nf.setMaximumFractionDigits(1);
+		nf.setGroupingUsed(true);
+
+		String value = nf.format(byteSizeF);
+
+		int i = 0;
+		while (i + 1 < desc.length
+				&& (value.length() > 4 || (value.length() > 3 && value
+						.indexOf('.') < 0))) {
+			i++;
+			factor = factor * 1024l;
+			value = nf.format(byteSizeF / factor);
+		}
+		if (value.charAt(0) == '0' && i > 0) { //go one back if a too-big scale
+											   // is used
+			value = nf.format(java.lang.Math.round(1024 * byteSizeF / factor));
+			i--;
+		}
+
+		if (value.length() > 3 && value.indexOf('.') > 0) // sut decimals on
+														  // large numbers
+			value = value.substring(0, value.indexOf('.'));
+
+		unit = desc[i];
+		return (value + " " + unit);
+	}
+
+	/**
+	 * Formats lentgh of time periods in a nice format
+	 * 
+	 * @param milis
+	 *            a time difference in milli-seconds to format
+	 * @return formatted time (eg: "1 second", "3 hours", "2 weeks" "41
+	 *         years"...)
+	 */
+	public static String readableAge(long milis) {
+		//simplest implementation:
+		//return((new Long(secs)).toString())+" seconds";
+		long secs = milis / 1000l;
+
+		if (secs < 2l)
+			return ("1 second");
+		if (secs == 2l)
+			return ("2 seconds");
+
+		//default:
+		long value = secs; //new Long(secs);
+		String unit = "seconds";
+
+		//now try to give it a meaning:
+		Long s = new Long(secs);
+
+		long[] breaks = { 31536000, 2628000, 604800, 86400, 3600, 60, 1 };
+		String[] desc = { "year", "month", "week", "day", "hour", "minute",
+				"second" };
+
+		int i = 0;
+		while (i <= breaks.length && secs <= (2 * breaks[i])) {
+			i++;
+		}
+		//i=i-1;
+		//long break=breaks[i];
+		value = secs / breaks[i];
+		unit = desc[i];
+		if (value >= 2)
+			unit = unit + "s";
+
+		String retval = value + " " + unit;
+
+		//if...
+
+		return (retval);
+	}
+
+	/**
+	 * prints date in nice format - only relevant parts Skips year if current,
+	 * shows time if today...
+	 * 
+	 * @todo: Make it actually work :)
+	 */
+	public static String readableDate(java.util.Date date) {
+		java.text.DateFormat df = java.text.DateFormat.getInstance();
+		df.setTimeZone(TimeZone.getTimeZone("CET"));
+
+		return (df.format(date));
+	}
+
+}
