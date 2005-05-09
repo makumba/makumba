c1bd3385db0095ff73a2abe84047fa28d580622a
jpeeters <jpeeters@1c21897b-c138-0410-a5df-bc8783e84eff>
2005-05-09 15:18:39 +0000

    - we now check if tomcat is running, before doing the tests. If it's not running, we print an error and clean the database.

--- trunk/makumba/classes/test/http/TestTags.java
+++ trunk/makumba/classes/test/http/TestTags.java
@@ -5,6 +5,7 @@
 package test.http;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -25,13 +26,18 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.apache.cactus.JspTestCase;
+import org.apache.cactus.Request;
 import org.makumba.Database;
 import org.makumba.MakumbaSystem;
 import org.makumba.Pointer;
 import org.makumba.Text;
 import org.makumba.view.jsptaglib.MakumbaVersionTag;
 import org.makumba.view.jsptaglib.QueryTag;
+import org.xml.sax.SAXException;
 
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
 
 
@@ -44,98 +50,122 @@ public class TestTags extends JspTestCase {
 	static Pointer brother;
 	static Pointer address;
 	static Dictionary pc;
+	static Suite setup;
 	static Vector v;
 	static String readPerson = "SELECT p.indiv.name AS name, p.indiv.surname AS surname, p.gender AS gender, p.uniqChar AS uniqChar, p.uniqInt AS uniqInt, p.birthdate AS birthdate, p.weight AS weight, p.TS_modify AS TS_modify, p.TS_create AS TS_create, p.comment AS comment, a.description AS description, a.email AS email, a.usagestart AS usagestart FROM test.Person p, p.address a WHERE p= $1";
 	private String output;
 	private String line;
+
+	private static final class Suite extends TestSetup {
+		private Suite(Test arg0) {
+			super(arg0);
+		}
+
+		protected void setUp() {
+			Database db = getDB();
+			insertPerson(db);
+			insertLanguages(db);
+			/* Just a dummy select, so the test_Person__extraData_ is mentioned in the client side part of the tests.
+			 * If this is not done, the server side and the client side will attempt to insert the same primary key
+			 * in the catalog table (because they use the same DBSV, because they use the same database connection file). */
+			db.executeQuery("SELECT p.extraData.something FROM test.Person p WHERE 1=0", null);
+			db.close();
+		}
+
+		protected void insertPerson(Database db) {
+			Properties p = new Properties();
+			
+			p.put("indiv.name", "bart");
+			brother=db.insert("test.Person", p);
+
+			p.clear();
+			p.put("indiv.name", "john");
+			
+			Calendar c = Calendar.getInstance();
+			c.clear();
+			c.set(1977, 2, 5);
+			Date birthdate = c.getTime();
+			p.put("birthdate", birthdate);
+					
+			
+			p.put("gender", new Integer(1));
+			p.put("uniqChar", new String("testing \" character field"));
+			
+			p.put("weight", new Double(85.7d));
+			
+			p.put("comment", new Text("This is a text field. It's a comment about this person."));
+
+			p.put("uniqInt", new Integer(255));				
+			
+			Vector intSet = new Vector();
+			intSet.addElement(new Integer(1));
+			intSet.addElement(new Integer(0));
+			p.put("intSet", intSet);
+
+			p.put("brother", brother);
+			
+			person = db.insert("test.Person", p);
+			
+			p.clear();
+			p.put("description", "");
+			p.put("usagestart", birthdate);
+			address=db.insert(person, "address", p);
+
+		}
+
+		protected void deletePerson(Database db) {
+			db.delete(address);
+			db.delete(brother);
+			db.delete(person);
+		}
+
+		protected void insertLanguages(Database db) {
+			languages.clear();
+			Dictionary p = new Hashtable();
+			for (int i = 0; i < languageData.length; i++) {
+				p.put("name", languageData[i][0]);
+				p.put("isoCode", languageData[i][1]);
+				languages.add(db.insert("test.Language", p));
+			}				
+		}
+
+		protected void deleteLanguages(Database db) {
+			for (int i = 0; i<languages.size(); i++)
+				db.delete((Pointer)languages.get(i));
+		}
+
+		public void tearDown() {
+			// do your one-time tear down here!
+			Database db = getDB();
+			deletePerson(db);
+			deleteLanguages(db);
+			db.close();
+		}
+	}
 
 	static ArrayList languages = new ArrayList();
 	static Object[][] languageData = { { "English", "en" }, { "French", "fr" },
 			{ "German", "de" }, { "Italian", "it" }, { "Spanish", "sp" } };
 	
 	public static Test suite() {
-		TestSetup setup = new TestSetup(new TestSuite(TestTags.class)) {
-			protected void setUp() {
-				Database db = getDB();
-				insertPerson(db);
-				insertLanguages(db);
-				/* Just a dummy select, so the test_Person__extraData_ is mentioned in the server side part of the tests.
-				 * If this is not done, the server side and the client side will attempt to insert the same primary key
-				 * in the catalog table (because they use the same DBSV, because they use the same database connection file). */
-				db.executeQuery("SELECT p.extraData.something FROM test.Person p WHERE 1=0", null);
-				db.close();
-			}
-			
-			protected void insertPerson(Database db) {
-				Properties p = new Properties();
-				
-				p.put("indiv.name", "bart");
-				brother=db.insert("test.Person", p);
-
-				p.clear();
-				p.put("indiv.name", "john");
-				
-				Calendar c = Calendar.getInstance();
-				c.clear();
-				c.set(1977, 2, 5);
-				Date birthdate = c.getTime();
-				p.put("birthdate", birthdate);
-						
-				
-				p.put("gender", new Integer(1));
-				p.put("uniqChar", new String("testing \" character field"));
-				
-				p.put("weight", new Double(85.7d));
-				
-				p.put("comment", new Text("This is a text field. It's a comment about this person."));
-
-				p.put("uniqInt", new Integer(255));				
-				
-				Vector intSet = new Vector();
-				intSet.addElement(new Integer(1));
-				intSet.addElement(new Integer(0));
-				p.put("intSet", intSet);
-
-				p.put("brother", brother);
-				
-				person = db.insert("test.Person", p);
-				
-				p.clear();
-				p.put("description", "");
-				p.put("usagestart", birthdate);
-				address=db.insert(person, "address", p);
-
-			}
-			protected void deletePerson(Database db) {
-				db.delete(address);
-				db.delete(brother);
-				db.delete(person);
-			}
-			
-			protected void insertLanguages(Database db) {
-				languages.clear();
-				Dictionary p = new Hashtable();
-				for (int i = 0; i < languageData.length; i++) {
-					p.put("name", languageData[i][0]);
-					p.put("isoCode", languageData[i][1]);
-					languages.add(db.insert("test.Language", p));
-				}				
-			}
-			protected void deleteLanguages(Database db) {
-				for (int i = 0; i<languages.size(); i++)
-					db.delete((Pointer)languages.get(i));
-			}
-			
-			protected void tearDown() {
-				// do your one-time tear down here!
-				Database db = getDB();
-				deletePerson(db);
-				deleteLanguages(db);
-				db.close();
-			}
-		};
+		setup = new Suite(new TestSuite(TestTags.class));
 		return setup;
 	}
+	
+	public void beginTomcat(Request request) {
+		WebConversation wc = new WebConversation();
+	    WebRequest     req = new GetMethodWebRequest(System.getProperty("cactus.contextURL"));
+	    try {
+			WebResponse   resp = wc.getResponse( req );
+		} catch (MalformedURLException e) {
+		} catch (IOException e) {
+			setup.tearDown();
+			System.err.println("\n\n\n\n\nYou should run tomcat first! Use mak-tomcat to do that.\n\n");			
+			System.exit(1);
+		} catch (SAXException e) {
+		}		
+	}
+	public void testTomcat(){}
 	
 	public void testVersionTag() throws JspException, IOException {
 		MakumbaVersionTag versionTag = new MakumbaVersionTag();
