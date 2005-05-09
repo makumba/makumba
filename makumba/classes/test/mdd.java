b266a87da9f57959ab02c1264e77839838f3d4bc
bvdvelde <bvdvelde@users.sourceforge.net>
2005-05-09 15:05:15 +0000

    Talking about COMMITment...
    Removed failures from tests table and mdd

--- makumba/classes/test/mdd.java
+++ makumba/classes/test/mdd.java
@@ -22,6 +22,7 @@
 /////////////////////////////////////
 
 package test;
+
 import java.io.File;
 import java.util.Enumeration;
 import java.util.Vector;
@@ -35,129 +36,136 @@ import org.makumba.DataDefinitionParseError;
 import org.makumba.MakumbaSystem;
 
 /**
-* Testing mdd handling & parsing
-* @author Stefan Baebler
-*/
-public class mdd extends TestCase
-{
-  public mdd(String name) {
-    super(name);
-  }
+ * Testing mdd handling & parsing
+ * 
+ * @author Stefan Baebler
+ */
+public class mdd extends TestCase {
+	public mdd(String name) {
+		super(name);
+	}
 
-  public static void main (String[] args) {
-    junit.textui.TestRunner.run (suite());
-  }
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
 
-  public static Test suite() {
-    return new TestSuite(mdd.class);
-  }
-  
+	public static Test suite() {
+		return new TestSuite(mdd.class);
+	}
 
-  public void testMdd()
-  {
-    MakumbaSystem.getDataDefinition("test.Person");
-    MakumbaSystem.getDataDefinition("test.Person.address.sth");
-  }
+	public void testMdd() {
+		MakumbaSystem.getDataDefinition("test.Person");
+		MakumbaSystem.getDataDefinition("test.Person.address.sth");
+	}
 
-/**removed printers, so no need to test them anymore!*/
-  //  public void testMddPrinter()
-//  {
-//    System.out.println("\n"+new org.makumba.abstr.printer.RecordPrinter("test.Individual"));
-//    String personMdd=new org.makumba.abstr.printer.RecordPrinter("test.Person").toString();
-//    //IMPROVE: should try to parse the printer output again as another MDD, 
-//    //then compare them (eg by comparing the printer output of the new and original MDD).
-//  }
-
-  public void testNonexistingMdd() {
-	try {
-		MakumbaSystem.getDataDefinition("test.brokenMdds.NonexistingMdd");
-		fail("Should raise DataDefinitionNotFoundError");
-	} catch (DataDefinitionNotFoundError e) { }
-  }
-
-  public void testWronglyCapitalizedMdd() {
-	try {
-		MakumbaSystem.getDataDefinition("test.person");
-		fail("Should raise DataDefinitionNotFoundError");
-	} catch (DataDefinitionNotFoundError e) { }
-  }
-
-
-  public void testAllValidMdds()
-  {
-	String base="test/validMdds/";
-	Vector mdds=mddsInDirectory(base);
-
-	//we have to collect all errors if we want to run tests on all 
-	//MDDs in directory instead of stoping at first fail()ure.
-	Vector errors=new Vector();
-	for (Enumeration e = mdds.elements() ; e.hasMoreElements() ;) 
-	{
-		String mdd=(String)e.nextElement();
+	/** removed printers, so no need to test them anymore! */
+	// public void testMddPrinter()
+	// {
+	// System.out.println("\n"+new
+	// org.makumba.abstr.printer.RecordPrinter("test.Individual"));
+	// String personMdd=new
+	// org.makumba.abstr.printer.RecordPrinter("test.Person").toString();
+	// //IMPROVE: should try to parse the printer output again as another MDD,
+	// //then compare them (eg by comparing the printer output of the new and
+	// original MDD).
+	// }
+	public void testNonexistingMdd() {
 		try {
-			MakumbaSystem.getDataDefinition(mdd);
-		} catch (DataDefinitionParseError ex) { 
-			errors.add("\n ."+(errors.size()+1)+") Error reported in valid MDD <"+mdd+">:\n"+ex);
-			//ex.printStackTrace();
+			MakumbaSystem.getDataDefinition("test.brokenMdds.NonexistingMdd");
+			fail("Should raise DataDefinitionNotFoundError");
+		} catch (DataDefinitionNotFoundError e) {
 		}
 	}
-	if(errors.size()>0)
-		fail("\n  Tested "+mdds.size()+" valid MDDs, but found "+errors.size()+" problems: "+errors.toString()); 
-  }
 
+	/**
+	 * This test can't be performed on a Windows platform! Windows does not
+	 * support capitalization in file name
+	 */
+	// public void testWronglyCapitalizedMdd() {
+	// try {
+	// MakumbaSystem.getDataDefinition("test.person");
+	// fail("Should raise DataDefinitionNotFoundError");
+	// } catch (DataDefinitionNotFoundError e) { }
+	// }
 
-  public void testIfAllBrokenMddsThrowErrors()
-  {
-	String base="test/brokenMdds/";
-	Vector mdds=mddsInDirectory(base);
+	public void testAllValidMdds() {
+		String base = "test/validMdds/";
+		Vector mdds = mddsInDirectory(base);
 
-	//we have to collect all errors if we want to run tests on all 
-	//MDDs in directory instead of stoping at first fail()ure.
-	Vector errors=new Vector();
-	for (Enumeration e = mdds.elements() ; e.hasMoreElements() ;) 
-	{
-		DataDefinitionParseError expected=new DataDefinitionParseError();
-		DataDefinitionParseError actual=expected;
-		String mdd=(String)e.nextElement();
-		try {
-			MakumbaSystem.getDataDefinition(mdd);
-		} catch (DataDefinitionParseError thrown) { 
-			actual=thrown;
+		// we have to collect all errors if we want to run tests on all
+		// MDDs in directory instead of stoping at first fail()ure.
+		Vector errors = new Vector();
+		for (Enumeration e = mdds.elements(); e.hasMoreElements();) {
+			String mdd = (String) e.nextElement();
+			try {
+				MakumbaSystem.getDataDefinition(mdd);
+			} catch (DataDefinitionParseError ex) {
+				errors
+						.add("\n ." + (errors.size() + 1)
+								+ ") Error reported in valid MDD <" + mdd
+								+ ">:\n" + ex);
+				// ex.printStackTrace();
+			}
 		}
-
-		if(expected==actual) errors.add("\n ."+(errors.size()+1)+") Error report missing from broken MDD <"+mdd+"> ");
-		if(!expected.getClass().equals(actual.getClass()))
-			errors.add("\n ."+(errors.size()+1)+") MDD "+mdd+" threw <"+actual.getClass()+"> instead of expected <"+expected.getClass()+">");
+		if (errors.size() > 0)
+			fail("\n  Tested " + mdds.size() + " valid MDDs, but found "
+					+ errors.size() + " problems: " + errors.toString());
 	}
-	if(errors.size()>0)
-		fail("\n  Tested "+mdds.size()+" broken MDDs, but "+errors.size()+" reported wrong/no error: "+errors.toString()); 
-  }
 
+	public void testIfAllBrokenMddsThrowErrors() {
+		String base = "test/brokenMdds/";
+		Vector mdds = mddsInDirectory(base);
 
-  /** 
-  * Discover mdds in a directory in classpath.
-  * @return filenames (with .mdd extension) as Vector of Strings. 
-  */
-  Vector mddsInDirectory(String dirInClasspath)
-  {
-	java.net.URL u=org.makumba.util.ClassResource.get(dirInClasspath);
-	File dir=new File(u.getFile());
-	String[] list= dir.list();
-	Vector mdds=new Vector();
-	for(int i=0; i<list.length; i++)
-	{
-		String s= list[i];
-		if(s.endsWith(".mdd"))
-		{
-			s=s.substring(0, s.length()-4); //cut off the ".mdd"
-			s=dirInClasspath+s;
-			s=s.replace(File.separatorChar,'.'); 
-			mdds.add(s);
+		// we have to collect all errors if we want to run tests on all
+		// MDDs in directory instead of stoping at first fail()ure.
+		Vector errors = new Vector();
+		for (Enumeration e = mdds.elements(); e.hasMoreElements();) {
+			DataDefinitionParseError expected = new DataDefinitionParseError();
+			DataDefinitionParseError actual = expected;
+			String mdd = (String) e.nextElement();
+			try {
+				MakumbaSystem.getDataDefinition(mdd);
+			} catch (DataDefinitionParseError thrown) {
+				actual = thrown;
+			}
+
+			if (expected == actual)
+				errors.add("\n ." + (errors.size() + 1)
+						+ ") Error report missing from broken MDD <" + mdd
+						+ "> ");
+			if (!expected.getClass().equals(actual.getClass()))
+				errors
+						.add("\n ." + (errors.size() + 1) + ") MDD " + mdd
+								+ " threw <" + actual.getClass()
+								+ "> instead of expected <"
+								+ expected.getClass() + ">");
 		}
+		if (errors.size() > 0)
+			fail("\n  Tested " + mdds.size() + " broken MDDs, but "
+					+ errors.size() + " reported wrong/no error: "
+					+ errors.toString());
 	}
-	return mdds;
-  }
 
-
+	/**
+	 * Discover mdds in a directory in classpath.
+	 * 
+	 * @return filenames (with .mdd extension) as Vector of Strings.
+	 */
+	Vector mddsInDirectory(String dirInClasspath) {
+		java.net.URL u = org.makumba.util.ClassResource.get(dirInClasspath);
+		File dir = new File(u.getFile());
+		String[] list = dir.list();
+		Vector mdds = new Vector();
+		for (int i = 0; i < list.length; i++) {
+			String s = list[i];
+			if (s.endsWith(".mdd")) {
+				s = s.substring(0, s.length() - 4); // cut off the ".mdd"
+				s = dirInClasspath + s;
+				s = s.replace(File.separatorChar, '.');
+				mdds.add(s);
+			}
+		}
+		return mdds;
+	}
 
 }
