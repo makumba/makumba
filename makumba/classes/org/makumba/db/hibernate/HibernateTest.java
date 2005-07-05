83c9cc68a76c28fbf9aaa5068ecbda65337197a8
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2005-07-05 14:47:12 +0000

    removed unneeded imports

--- makumba/classes/org/makumba/db/hibernate/HibernateTest.java
+++ makumba/classes/org/makumba/db/hibernate/HibernateTest.java
@@ -2,10 +2,7 @@ package org.makumba.db.hibernate;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Vector;
-
 import javassist.CannotCompileException;
 import javassist.NotFoundException;
 
