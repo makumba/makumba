ee18cd0ccd271cce58d9ab47b059f4060def583f
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2006-01-15 00:03:53 +0000

    Fixing a bug with the form responder - the tomcat working directory was not set on the first time trying to write a form responder (worked only on load).

--- makumba/classes/org/makumba/view/jsptaglib/FormTagBase.java
+++ makumba/classes/org/makumba/view/jsptaglib/FormTagBase.java
@@ -230,7 +230,7 @@ public class FormTagBase extends MakumbaTag implements BodyTag
       bodyContent.writeOut(bodyContent.getEnclosingWriter());
 
       sb= new StringBuffer();
-      responder.writeFormPostamble(sb, basePointer, pageContext.getSession().getId());
+      responder.writeFormPostamble(sb, basePointer, pageContext.getSession());
    
       bodyContent.getEnclosingWriter().print(sb.toString()); 
       if(findParentForm()!=null)
