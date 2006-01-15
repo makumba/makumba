0e004cddcd5f06f9c594660ee224f310f497757f
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2006-01-15 20:52:12 +0000

    Fixes to query runner for double & set types. problems still exist with selecting pointers

--- trunk/makumba/classes/org/makumba/view/HibernateQueryRunner.java
+++ trunk/makumba/classes/org/makumba/view/HibernateQueryRunner.java
@@ -38,7 +38,9 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.makumba.DataDefinition;
 import org.makumba.HibernateSFManager;
+import org.makumba.Pointer;
 import org.makumba.db.hibernate.HibernateOqlAnalyzer;
+import org.makumba.db.hibernate.HibernatePointer;
 import org.makumba.db.sql.SQLPointer;
 
 public class HibernateQueryRunner extends AbstractQueryRunner {
@@ -60,14 +62,13 @@ public class HibernateQueryRunner extends AbstractQueryRunner {
                 String param = "p" + (i+1);
                 if (args[i] instanceof Vector) {
                     q.setParameterList(param, (Collection) args[i]);
-                    System.out.println("set COLLECTION param " + (i+1) + " ==> " + args[i]);
                 } else if (args[i] instanceof Date) {
                     q.setParameter(param, args[i], Hibernate.DATE);
                 } else if (args[i] instanceof Integer) {
                     q.setParameter(param, args[i], Hibernate.INTEGER );
                 } else { // we have any param type (most likely String) 
                     q.setParameter("p" + (+1), args[i]);
-                }                
+                }
             }
         }
         List list = q.list();
@@ -75,20 +76,29 @@ public class HibernateQueryRunner extends AbstractQueryRunner {
         HibernateOqlAnalyzer analyzer = HibernateOqlAnalyzer.getOqlAnalyzer(
                 query, HibernateSFManager.getSF());
         DataDefinition dataDef = analyzer.getProjectionType();
-        Object[] labels = dataDef.getFieldNames().toArray();
+        Object[] projections = dataDef.getFieldNames().toArray();
         int i = 1;
         for (Iterator iter = list.iterator(); iter.hasNext(); i++) {
             Object resultRow = iter.next();
             Dictionary dic = new Hashtable();
-            if (resultRow instanceof Object[]) { // our query result has more than one field
-                Object[] elements = (Object[]) resultRow;
-                for (int j = 0; j < elements.length; j++) {
-                    if (elements[j] != null) { // we add to the dictionary only fields with values in the DB
-                        dic.put(labels[j], elements[j]);
+            Object[] elements;
+            if (!(resultRow instanceof Object[])) { // our query result has only one field
+                elements = new Object[] {resultRow};
+            } else {
+                elements = (Object[]) resultRow;
+            }
+            for (int j = 0; j < elements.length; j++) {
+                if (elements[j] != null) { // we add to the dictionary only fields with values in the DB
+                    if (analyzer.getProjectionLeftSides()[j].endsWith(".id")) { // we have a pointer
+                        String labelName = analyzer.getProjectionLeftSides()[j];
+                        labelName = labelName.substring(0, labelName.length() -3);
+                        DataDefinition dd = analyzer.getLabelType(labelName);
+                        Pointer pointer = new HibernatePointer(dd.getName(), ((Integer) elements[j]).intValue());
+                        dic.put(projections[j], pointer);                        
+                    } else {
+                        dic.put(projections[j], elements[j]);
                     }
                 }
-            } else { // the query result had just one field
-                dic.put(labels[0], resultRow);
             }
             results.add(dic);
         }
@@ -109,9 +119,26 @@ public class HibernateQueryRunner extends AbstractQueryRunner {
         v.add(new Integer(1));
         v.add(new Integer(2));
         Object[] params1 = new Object[] { "Cristian", new Timestamp(cal.getTimeInMillis()), v, new Integer(1), new SQLPointer("general.Person", 151022406)  };
-        qr.execute("SELECT p.id as ID, p.name as name, p.surname as surname, p.birthdate as date, p.T_shirt as shirtSize FROM general.Person p where p.name = $1 AND p.birthdate is not null AND p.birthdate > :p2 and p.T_shirt in :p3",
-                        params1, 0, 20);
-        Object[] params2 = new Object[] { new Double(2.0) };
-        qr.execute("SELECT e.subject from general.archive.Email e WHERE e.spamLevel = :p1", params2, 0, -1);
+        Object[] params2 = new Object[] { "Cristian", new Timestamp(cal.getTimeInMillis()), new Integer(1), new Integer(2)};
+        Object[] params3 = new Object[] { new Double(2.0) };
+
+        String query1 = "SELECT p.id as ID, p.name as name, p.surname as surname, p.birthdate as date, p.T_shirt as shirtSize FROM general.Person p where p.name = $1 AND p.birthdate is not null AND p.birthdate > :p2 and p.T_shirt in (:p3)";
+        String query2 = "SELECT p.id as ID, p.name as name, p.surname as surname, p.birthdate as date, p.T_shirt as shirtSize FROM general.Person p where p.name = $1 AND p.birthdate is not null AND p.birthdate > :p2 and p.T_shirt in (:p3, :p4)";
+        String query3 = "SELECT e.subject, e.spamLevel AS spamLevel from general.archive.Email e WHERE e.spamLevel = :p1";
+
+        String[] queries = new String[] {query1, query2, query3};
+        Object[] params = new Object[] {params1, params2, params3};
+        for (int i=0; i<Math.min(queries.length, params.length); i++) {
+            System.out.println("Query" + queries[i] + " ==> \n" + printQueryResults(qr.execute(queries[i], (Object[]) params[i], 0, 100)) + "\n\n");
+        }
+        //printQueryResults(qr.execute(, params2, 0, -1));
+    }
+    
+    public static String printQueryResults(Vector v) {
+        String result = "";
+        for (int i = 0; i < v.size(); i++) {
+            result += "Row " + i + ":" + v.elementAt(i) + "\n";
+        }
+        return result;
     }
 }
