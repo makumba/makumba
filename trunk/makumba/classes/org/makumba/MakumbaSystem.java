226aed17eec75db3408a92a111048138dc198428
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2006-01-17 22:42:18 +0000

    using named resource cache for hql analyzer

--- trunk/makumba/classes/org/makumba/MakumbaSystem.java
+++ trunk/makumba/classes/org/makumba/MakumbaSystem.java
@@ -24,6 +24,7 @@
 package org.makumba;
 import java.io.StringReader;
 
+import org.makumba.db.hibernate.hql.HqlAnalyzer;
 import org.makumba.util.NamedResourceFactory;
 import org.makumba.util.NamedResources;
 import org.makumba.util.RuntimeWrappedException;
@@ -408,6 +409,22 @@ The programmer could just as well decide that all makumba logging at or over the
     MakumbaSystem.getMakumbaLogger("db.query.compilation").fine("OQL to SQL: "+ diff +" ms: "+oqlQuery);
     return t;
   }
+  
+    /**
+     * Get the Hibernate HQL analyzer for the indicated query
+     */
+    static public HqlAnalyzer getHqlAnalyzer(String hqlQuery) {
+        return (HqlAnalyzer) NamedResources.getStaticCache(parsedHqlQueries).getResource(hqlQuery);
+    }
+
+    static int parsedHqlQueries = NamedResources.makeStaticCache("Hibernate HQL parsed queries",
+        new NamedResourceFactory() {
+            private static final long serialVersionUID = 1L;
+
+            protected Object makeResource(Object nm, Object hashName) throws Exception {
+                return new HqlAnalyzer((String) nm);
+            }
+        }, true);
 
   /** 
   * Discover mdds in a directory in classpath.
