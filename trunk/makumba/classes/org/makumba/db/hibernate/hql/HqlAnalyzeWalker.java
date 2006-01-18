f4184f4f6914bc6aa7a3fb8ffd7c4ff1d7c66de8
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2006-01-18 00:51:14 +0000

    Added type support for parameters, and added according tests

--- trunk/makumba/classes/org/makumba/db/hibernate/hql/HqlAnalyzeWalker.java
+++ trunk/makumba/classes/org/makumba/db/hibernate/hql/HqlAnalyzeWalker.java
@@ -1,6 +1,3 @@
-/*
- * Created on 24-Jul-2005
- */
 package org.makumba.db.hibernate.hql;
 
 import java.util.ArrayList;
@@ -10,6 +7,11 @@ import java.util.Map;
 import antlr.RecognitionException;
 import antlr.SemanticException;
 import antlr.collections.AST;
+/**
+ * @author Christian Bogdan
+ * @author Manuel Gay
+ * @version $id
+ */
 
 public class HqlAnalyzeWalker extends HqlAnalyzeBaseWalker {
 
@@ -19,38 +21,43 @@ public class HqlAnalyzeWalker extends HqlAnalyzeBaseWalker {
         if (aliasTypes.get(alias.getText()) != null)
             throw new antlr.SemanticException("alias " + alias.getText() + " defined twice");
 
-        if(typeComputer.determineType(type, null)!=null)
+        if (typeComputer.determineType(type, null) != null)
             aliasTypes.put(alias.getText(), type);
-        else
-        {
-         // we have a from section in the form a.b.c that is not a type!
-         // a must be a recognizable label
-            int dot= type.indexOf('.');
-            if(dot==-1)
-                throw new SemanticException("unknown OQL type: "+type);
-            String label= type.substring(0, dot);
-            String labelType= (String)aliasTypes.get(label);
-            if(labelType==null)
-                throw new SemanticException("unknown OQL label/alias: "+label);
-            while(true){
-                int dot1= type.indexOf('.', dot+1);
+        else {
+            // we have a from section in the form a.b.c that is not a type!
+            // a must be a recognizable label
+            int dot = type.indexOf('.');
+            if (dot == -1)
+                throw new SemanticException("unknown OQL type: " + type);
+            String label = type.substring(0, dot);
+            String labelType = (String) aliasTypes.get(label);
+            if (labelType == null)
+                throw new SemanticException("unknown OQL label/alias: " + label);
+            while (true) {
+                int dot1 = type.indexOf('.', dot + 1);
                 String field;
-                if(dot1==-1)
-                    field= type.substring(dot+1);
+                if (dot1 == -1)
+                    field = type.substring(dot + 1);
                 else
-                    field= type.substring(dot+1, dot1);
-//                System.out.println(field);
-                Object tp= typeComputer.determineType(labelType, field);
-                if(!(tp instanceof String))
-                    throw new SemanticException("composite type expected in FROM expression "+type+". "+field +" is a non-composite field of type "+labelType);
-                labelType=(String)tp;
-                dot=dot1;
-                if(dot1==-1)
+                    field = type.substring(dot + 1, dot1);
+                // System.out.println(field);
+                Object tp = typeComputer.determineType(labelType, field);
+                if (!(tp instanceof String))
+                    throw new SemanticException("composite type expected in FROM expression " + type + ". " + field
+                            + " is a non-composite field of type " + labelType);
+                labelType = (String) tp;
+                dot = dot1;
+                if (dot1 == -1)
                     break;
             }
-            aliasTypes.put(alias.getText(), labelType);            
+            aliasTypes.put(alias.getText(), labelType);
         }
-  //      System.out.println(alias.getText()+" "+aliasTypes.get(alias.getText()));
+        // System.out.println(alias.getText()+" "+aliasTypes.get(alias.getText()));
+    }
+
+    AST deriveParamExpr(AST pe) throws SemanticException {
+
+        return new ParamTypeAST(ExprTypeAST.PARAMETER);
     }
 
     AST deriveArithmethicExpr(AST ae) throws SemanticException {
@@ -61,16 +68,21 @@ public class HqlAnalyzeWalker extends HqlAnalyzeBaseWalker {
             firstValue = (ExprTypeAST) ae.getFirstChild();
             secondValue = (ExprTypeAST) firstValue.getNextSibling();
         } catch (ClassCastException e) {
-            //this may occur if the expression is neither of type ExprTypeAST nor ObjectTypeAST (ie, it is a CommonAST)
-            throw new SemanticException("Cannot perform operation "+operator+" on elements "+ae.getFirstChild().getType()+" and "+ae.getFirstChild().getNextSibling().getType());
+            // this may occur if the expression is neither of type ExprTypeAST nor ObjectTypeAST (ie, it is a CommonAST)
+            throw new SemanticException("Cannot perform operation " + operator + " on elements "
+                    + ae.getFirstChild().getType() + " and " + ae.getFirstChild().getNextSibling().getType());
         }
-        
+
         // if the expr are not litterals
-        if(firstValue.getObjectType() != null || secondValue.getObjectType() != null) {
-            throw new SemanticException("Cannot perform operation "+operator+" on elements "+firstValue.getText()+" and "+secondValue.getText());
+        if (firstValue.getObjectType() != null || secondValue.getObjectType() != null) {
+            throw new SemanticException("Cannot perform operation " + operator + " on elements " + firstValue.getText()
+                    + " and " + secondValue.getText());
         }
-        
-        return new ExprTypeAST(firstValue.getDataType()>secondValue.getDataType()?firstValue.getDataType():secondValue.getDataType());
+        if (firstValue.getDataType() == ExprTypeAST.PARAMETER || secondValue.getDataType() == ExprTypeAST.PARAMETER)
+            return new ExprTypeAST(ExprTypeAST.PARAMETER);
+
+        return new ExprTypeAST(firstValue.getDataType() > secondValue.getDataType() ? firstValue.getDataType()
+                : secondValue.getDataType());
     }
 
     protected void setAlias(AST se, AST i) {
