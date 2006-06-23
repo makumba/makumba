d2985aeb4ba215a2a9e00124408cdabaecf7963a
Manuel Bernhardt <manuel@bernhardt.io>
2006-06-23 12:14:19 +0000

    This commit was manufactured by cvs2svn to create tag 'makumba-0_5_15'.

--- makumba/classes/org/makumba/db/hibernate/hql/ObjectTypeAST.java
+++ makumba/classes/org/makumba/db/hibernate/hql/ObjectTypeAST.java
@@ -8,58 +8,30 @@ package org.makumba.db.hibernate.hql;
 
 import java.util.Map;
 
-import org.makumba.DataDefinition;
-import org.makumba.DataDefinitionNotFoundError;
-import org.makumba.FieldDefinition;
-import org.makumba.MakumbaSystem;
-
-import antlr.RecognitionException;
+import antlr.CommonAST;
 import antlr.SemanticException;
 import antlr.collections.AST;
 
-public class ObjectTypeAST extends ExprTypeAST {
+public class ObjectTypeAST extends CommonAST {
 
-    private String objectType;
-    
-    public ObjectTypeAST(AST lhs, AST rhs, Map aliasTypes, ObjectType typeComputer) throws RecognitionException {
-        super(-2);
-        String type = null;
-        if (lhs instanceof ObjectTypeAST) {
-            type = ((ObjectTypeAST)lhs).getObjectType();
-        } else {
-            type = (String) aliasTypes.get(lhs.getText());
-            if (type == null) {
-                throw new SemanticException("unknown alias: " + lhs.getText() + " in property reference: "
-                        + lhs.getText() + "." + rhs.getText());
+    public ObjectTypeAST(AST lhs, AST rhs, Map aliasTypes) throws SemanticException{
+        String type= null;
+        if(lhs instanceof ObjectTypeAST){
+            type= lhs.getText();
+        }
+        else{
+            type= (String)aliasTypes.get(lhs.getText());
+            if(type==null){
+                throw new SemanticException("unknown alias: "+lhs.getText()+ " in property reference: "+lhs.getText()+"."+rhs.getText());
             }
         }
 
-        Object computedType = "";
-        setDescription(rhs.getText());
-        computedType = typeComputer.determineType(type, rhs.getText());
-        
-        //System.out.println("GOT TYPE: " + computedType);
-
-        if (computedType instanceof String) {
-            setObjectType(computedType.toString());
-        }else{
-            setExtraTypeInfo(computedType);
-            setDataType(typeComputer.getTypeOf(computedType));   
-        }
-    }
-    
-    public ObjectTypeAST(AST pointer, Map aliasTypes) throws SemanticException {
-        String type = (String) aliasTypes.get(pointer.getText());
-        setObjectType(type);
-        setDescription(pointer.getText());
-    }
-
-    public String getObjectType() {
-        return objectType;
-    }
-
-    public void setObjectType(String objectType) {
-        this.objectType = objectType;
+        /* analysis can look like
+        Type t= SessionFactory.getClassMetadata(type).getPropertyType(rhs.getText());
+        then check if t is a ptr or a set, and to what type it points, and do setText() with that type
+        */ 
+        setText(type+"->"+rhs.getText());
+        System.out.println(getText());
     }
 
 }
