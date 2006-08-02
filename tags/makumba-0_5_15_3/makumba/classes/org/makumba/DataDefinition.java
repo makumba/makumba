db302144861c478f6a0f7e63ede443af624ab54e
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2006-08-02 22:49:29 +0000

    This commit was manufactured by cvs2svn to create tag 'makumba-0_5_15_3'.

---
+++ tags/makumba-0_5_15_3/makumba/classes/org/makumba/DataDefinition.java
@@ -0,0 +1,82 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+//TODO extra comments about changes from refactoring
+
+package org.makumba;
+
+import java.util.Dictionary;
+
+/** Information about a makumba data definition as obtained from an MDD file or the structure of an OQL query result.
+ * This class is provided for makumba programs to be able to introspect makumba data structures. Such introspection is not needed usually, as the application programmer knows the makumba data structure.
+ */
+public interface DataDefinition
+{
+  /** name of this data definition */
+  public String getName();
+
+  /** the names of the fields declared in this data definition, in the order of declaration */
+  public java.util.Vector getFieldNames();
+  
+  /** the field with the respective name, null if such a field doesn't exist */
+  public FieldDefinition getFieldDefinition(String name);
+
+  /** the field with the respective index, null if such a field doesn't exist */
+  public FieldDefinition getFieldDefinition(int n);
+  
+  /** tells whether this data definition was generated temporarily to depict a query result as opposed to being read from an MDD file */
+  public boolean isTemporary();
+
+  /** The title field indicated, or the default one */
+  public String getTitleFieldName();
+
+  /** The name of the index field (primary key), if any? */
+  public String getIndexPointerFieldName();
+
+  /** The name of the creation timestamp field, if any? */
+  public String getCreationDateFieldName();
+
+  /** The name of the modification timestamp field, if any? */
+  public String getLastModificationDateFieldName();
+
+  /** If this type is the data pointed to by a 1-1 pointer or subset, return the field definition in the main record, otherwise return null */
+  public FieldDefinition getParentField();
+
+  /** The name of the set member (Pointer, Character or Integer), for set subtypes */
+  public String getSetMemberFieldName() ;
+
+  /** The name of the pointer to the main table, for set and internal set subtypes*/
+  public String getSetOwnerFieldName();
+
+  /** Add a new field definition. Works only for temporary data definitions */
+  public void addField(FieldDefinition fd);
+  
+  /**Checks whether all fieldnames exist in the database */
+  public void checkFieldNames(Dictionary d);
+  
+  /**Checks whether all fields to be inserted exist in the database */
+  //public void checkInsert(Dictionary d, Dictionary except);
+  
+  /**Checks whether all fields to be updated exist in the database */
+  //public void checkUpdate(Dictionary d, Dictionary except);
+}
---
+++ tags/makumba-0_5_15_3/makumba/classes/org/makumba/devel/TagExceptionServlet.java
@@ -0,0 +1,246 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.devel;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.makumba.LogicException;
+import org.makumba.LogicInvocationError;
+import org.makumba.MakumbaError;
+import org.makumba.MakumbaSystem;
+import org.makumba.OQLParseError;
+import org.makumba.util.JspParseData;
+import org.makumba.util.RuntimeWrappedException;
+import org.makumba.view.jsptaglib.MakumbaTag;
+
+/** 
+ * the servlet that receives errors in any makumba page and treats them 
+ * meant to be friendly for developer, so he'll see helpful stuff during development and not 
+ * stupid stakctraces (which are shown only in case of unknown exceptions)
+ * also indented for intercepting lack of authorization and show login pages
+ */
+public class TagExceptionServlet extends HttpServlet
+{
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+static Object errors [][]=
+  {
+    { org.makumba.OQLParseError.class, "query" },
+    { org.makumba.DataDefinitionNotFoundError.class, "data definition not found" },
+    { org.makumba.DataDefinitionParseError.class, "data definition parse" },
+    { org.makumba.DBError.class, "database" },
+    { org.makumba.ConfigFileError.class, "configuration" },
+    { org.makumba.ProgrammerError.class, "programmer" },
+    { org.makumba.view.jsptaglib.MakumbaJspException.class, "page" },
+    { org.makumba.AttributeNotFoundException.class, "attribute not set" },
+    { org.makumba.UnauthorizedException.class, "authorization" },
+    { org.makumba.InvalidValueException.class, "invalid value" },
+    { org.makumba.InvalidFieldTypeException.class, "invalid field type" },
+    { org.makumba.NoSuchFieldException.class, "no such field" },
+    { org.makumba.LogicException.class, "business logic" }
+  };
+
+  public void service(HttpServletRequest req, HttpServletResponse resp) 
+       throws IOException, ServletException
+  {
+    resp.setContentType("text/html");
+    PrintWriter wr=resp.getWriter();
+    Throwable t= (Throwable)req.getAttribute(javax.servlet.jsp.PageContext.EXCEPTION);
+    Throwable t1=null;    
+
+    Throwable original=t;
+    while(true)
+      {
+	if(t instanceof LogicException)
+	  t1=((LogicException)t).getReason();
+	else if(t instanceof MakumbaError && !(t instanceof OQLParseError))
+	  t1=((MakumbaError)t).getReason();
+	else if(t instanceof LogicInvocationError)
+	  t1=((LogicInvocationError)t).getReason();
+	else if(t instanceof RuntimeWrappedException)
+	  t1=((RuntimeWrappedException)t).getReason();
+	else 
+	  break;
+	if(t1==null)
+	  break;
+	t=t1;
+      }
+
+    if(t.getClass().getName().startsWith(org.makumba.view.jsptaglib.TomcatJsp.getJspCompilerPackage())){
+      knownError("JSP compilation error", t, original, req, wr);
+      return;
+    }
+
+    for(int i=0; i<errors.length; i++)
+      if((((Class)errors[i][0])).isInstance(t) || t1!=null && 
+	 (((Class)errors[i][0])).isInstance(t=t1))
+	{
+	  knownError("Makumba "+errors[i][1] +" error", t, original, req, wr);
+	  return;
+	}
+    unknownError(original, t, wr,req);
+    wr.flush();
+  }
+    
+  void knownError(String title, Throwable t, Throwable original, HttpServletRequest req, PrintWriter wr){
+    String trcOrig=trace(t);
+    String trc= shortTrace(trcOrig);
+    String body=t.getMessage();
+    String hiddenBody=null;
+    
+    body=formatTagData()+ body;
+    if(original instanceof LogicInvocationError || trcOrig.indexOf("at org.makumba.abstr.Logic")!=-1)
+      {
+	body=body+"\n\n"+trc;
+      }
+    else
+      {
+	hiddenBody=trc;
+      }
+      try{
+	SourceViewer sw=new errorViewer(req,this,title,body,hiddenBody);
+	sw.parseText(wr);
+      } catch (IOException e) {e.printStackTrace(); throw new org.makumba.util.RuntimeWrappedException(e);}
+  }
+    
+  String formatTagData(){
+    String tagExpl="During analysis of the following tag (and possibly tags inside it):";
+    JspParseData.TagData tagData=MakumbaTag.getAnalyzedTag();
+    if(tagData==null){
+      tagExpl="During running of:";
+      tagData= MakumbaTag.getRunningTag();
+    }
+    if(tagData==null){
+      tagExpl="While executing inside this body tag, but most probably _not_ due to the tag:";
+      tagData= MakumbaTag.getCurrentBodyTag();
+    }
+    if(tagData==null) return "";
+    StringBuffer sb= new StringBuffer();
+    try{
+      JspParseData.tagDataLine(tagData, sb);
+    }catch(Throwable t){ 
+      // ignore source code retrieving bugs
+      t.printStackTrace();
+    }
+    try{
+      return tagExpl+"\n"+tagData.getStart().getFile().getCanonicalPath()+":"+
+	tagData.getStart().getLine()+":"+tagData.getStart().getColumn()+":"+
+	tagData.getEnd().getLine()+":"+tagData.getEnd().getColumn()+"\n"+
+	sb.toString()+"\n\n";
+    }catch(java.io.IOException e) { throw new MakumbaError(e.toString()); }
+  }
+
+  String trace(Throwable t) 
+  {
+    StringWriter sw= new StringWriter();
+    t.printStackTrace(new PrintWriter(sw));
+    return sw.toString();
+  }
+  
+
+  String shortTrace(String s)
+  {
+    int i=s.indexOf("at org.makumba.controller.Logic");
+    if(i!=-1)
+      {
+	s=s.substring(0, i);
+	i=s.indexOf("at sun.reflect");
+	if(i!=-1)
+	  s=s.substring(0, i);
+      }
+    else
+      {
+	i=s.indexOf("at javax.servlet.http.HttpServlet.service(HttpServlet.java");
+	if(i!=-1)
+	  s=s.substring(0,i);
+	else
+	    {
+		i=s.indexOf("at org.makumba.controller.http.ControllerFilter.doFilter(ControllerFilter.java");
+		if(i!=-1)
+		    s=s.substring(0,i);
+		else
+		    MakumbaSystem.getMakumbaLogger("devel").severe ("servlet or filter call not found in stacktrace");
+	    }
+
+      }
+    return s;
+  }
+
+
+  void unknownError(Throwable original, Throwable t, PrintWriter wr, HttpServletRequest req)
+       throws IOException, ServletException
+  {
+    Throwable traced=t;
+    String title="";
+    String body="";
+    if(original instanceof LogicInvocationError)
+      {
+	title="Error in business logic code";
+      }
+    else if(trace(traced).indexOf("org.makumba")!=-1)
+      {
+	title="Internal Makumba error";
+	body="Please report to the developers.\n\n";
+	if(t instanceof ServletException){
+	  traced=((ServletException)t).getRootCause();
+	  // maybe there's no root cause...
+	  if(traced==null)
+	    traced=t;
+	}
+      }
+    else
+      {
+	title="Error in JSP Java scriplet or servlet container";
+      }
+
+    if(traced instanceof java.sql.SQLException)
+      {
+	title="SQL "+title;
+	body="The problem is related to SQL:\n"
+		+"	 SQLstate: "+((java.sql.SQLException)traced).getSQLState()+"\n"
+		+"	ErrorCode: "+((java.sql.SQLException)traced).getErrorCode()+"\n"
+		+"	  Message: "+traced.getMessage()+"\n\n"
+		+"Refer to your SQL server\'s documentation for error explanation.\n"
+		+"Please check the configuration of your webapp and SQL server.\n"
+		+body;
+      }
+
+    body=formatTagData()+body+shortTrace(trace(traced));
+    try{
+       SourceViewer sw=new errorViewer(req,this,title,body,null);
+       sw.parseText(wr);
+    } catch (IOException e) {e.printStackTrace(); throw new org.makumba.util.RuntimeWrappedException(e);}
+
+  }
+  
+
+}
