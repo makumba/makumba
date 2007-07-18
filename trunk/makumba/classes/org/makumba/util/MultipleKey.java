51c6226160f33127ae01897192c3e8aa26ced9f6
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2007-07-18 09:42:33 +0000

    Writing more javadoc, formatting source

--- trunk/makumba/classes/org/makumba/util/MultipleKey.java
+++ trunk/makumba/classes/org/makumba/util/MultipleKey.java
@@ -22,94 +22,147 @@
 /////////////////////////////////////
 
 package org.makumba.util;
+
 import java.util.Dictionary;
 import java.util.Vector;
 
-/** the values for a group of fields */
-public class MultipleKey extends Vector
-{
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-static private final Object theNull=new Object(){
-    public String toString(){ return "null key member"; }
-  };
+/**
+ * Holds the values for a group of fields. Every element of a MultipleKey holds an Object. In case of a null value being
+ * passed to one of the constructors, the element is replaced by a placeholder.
+ * 
+ * @author Cristian Bogdan
+ * @version $Id$
+ */
+public class MultipleKey extends Vector {
 
-  public MultipleKey(int size){ super(size); }
+    private static final long serialVersionUID = 1L;
 
-  public MultipleKey(Vector v){ this(v, v.size()); }
-  public MultipleKey(Vector v, int size)
-  {
-    super(size);
-    for(; elementCount<v.size(); elementCount++)
-      elementData[elementCount]= checkNull(v.elementAt(elementCount));
-  }
+    /**
+     * Dummy object holding the place for a null element
+     */
+    static private final Object theNull = new Object() {
+        public String toString() {
+            return "null key member";
+        }
+    };
 
-  public MultipleKey(Vector v, Object o)
-  {
-    this(v, v.size()+1);
-    setAt(o, v.size());
-  }
+    public MultipleKey(int size) {
+        super(size);
+    }
 
-  public MultipleKey(Object o[]) { this(o, o.length); }
-  public MultipleKey(Object o[], int size)
-  {
-    super(size);
-    for(; elementCount<o.length; elementCount++)
-      elementData[elementCount]= checkNull(o[elementCount]);
-  }
+    public MultipleKey(Vector v) {
+        this(v, v.size());
+    }
 
-  /** keep together the values associated with the give keys */
-  public MultipleKey(Vector keys, Dictionary data){ this(keys, data, keys.size()); }
-  /** keep together the values associated with the give keys */
-  public MultipleKey(Vector keys, Dictionary data, int size)
-  {
-    super(size);
-    for(; elementCount<keys.size(); elementCount++)
-      elementData[elementCount]= checkNull(data.get(keys.elementAt(elementCount)));
-  }
+    public MultipleKey(Vector v, int size) {
+        super(size);
+        for (; elementCount < v.size(); elementCount++)
+            elementData[elementCount] = checkNull(v.elementAt(elementCount));
+    }
 
-  /** keep together the values associated with the give keys */
-  public MultipleKey(Vector indexes, Object[]data){ this(indexes, data, indexes.size()); }
-  /** keep together the values associated with the give keys */
-  public MultipleKey(Vector indexes, Object[] data, int size)
-  {
-    super(size);
-    for(; elementCount<indexes.size(); elementCount++)
-      elementData[elementCount]= 
-	checkNull(data[((Integer)indexes.elementAt(elementCount)).intValue()]);
-  }
-  
-  public void setAt(Object o, int n)
-  {
-    if(elementCount<=n)
-      elementCount=n+1;
-    elementData[n]=checkNull(o);
-  }
+    public MultipleKey(Vector v, Object o) {
+        this(v, v.size() + 1);
+        setAt(o, v.size());
+    }
 
-  protected Object checkNull(Object o)
-  {
-    return o==null?theNull:o; 
-  }
+    public MultipleKey(Object o[]) {
+        this(o, o.length);
+    }
 
-  /** see if this bunch is equal with another one */
-  public boolean equals(Object o)
-  {
-    for(int i= 0; i< elementCount; i++)
-      if(!((MultipleKey)o).elementAt(i).equals(elementData[i]))
-	return false;
-    return true;
-  }
-  
-  /** compute a hash code of this bunch by adding the individual hashcodes */
-  public int hashCode()
-  {
-    int ret= 0;
-    for(int i= 0; i< elementCount; i++)
-      ret+=elementData[i].hashCode();
-    return ret;
-  }
+    public MultipleKey(Object o[], int size) {
+        super(size);
+        for (; elementCount < o.length; elementCount++)
+            elementData[elementCount] = checkNull(o[elementCount]);
+    }
+
+    /**
+     * Keeps together the values associated with the give keys
+     * 
+     * @param keys
+     *            A vector of keys
+     * @param data
+     *            The dictionary containing the values of the keys
+     */
+    public MultipleKey(Vector keys, Dictionary data) {
+        this(keys, data, keys.size());
+    }
+
+    /**
+     * Keeps together the values associated with the given keys
+     * 
+     * @param keys
+     *            A vector of keys
+     * @param data
+     *            The dictionary containing the values of the keys
+     * @param size
+     *            The size of the MultipleKey object to be constructed
+     */
+    public MultipleKey(Vector keys, Dictionary data, int size) {
+        super(size);
+        for (; elementCount < keys.size(); elementCount++)
+            elementData[elementCount] = checkNull(data.get(keys.elementAt(elementCount)));
+    }
+
+    /**
+     * Keeps together the values associated with the given keys
+     * 
+     * @param indexes
+     *            A vector of keys
+     * @param data
+     *            The object array containing the values of the keys
+     */
+    public MultipleKey(Vector indexes, Object[] data) {
+        this(indexes, data, indexes.size());
+    }
+
+    /**
+     * Keep together the values associated with the given keys
+     * 
+     * @param indexes
+     *            A vector of keys
+     * @param data
+     *            The object array containing the values of the keys
+     * @param size
+     *            The size of the MultipleKey object to be constructed
+     */
+    public MultipleKey(Vector indexes, Object[] data, int size) {
+        super(size);
+        for (; elementCount < indexes.size(); elementCount++)
+            elementData[elementCount] = checkNull(data[((Integer) indexes.elementAt(elementCount)).intValue()]);
+    }
+
+    public void setAt(Object o, int n) {
+        if (elementCount <= n)
+            elementCount = n + 1;
+        elementData[n] = checkNull(o);
+    }
+
+    protected Object checkNull(Object o) {
+        return o == null ? theNull : o;
+    }
+
+    /**
+     * Compares the elements of two MultipleKey objects
+     * 
+     * @param o
+     *            The object to be compared with
+     */
+    public boolean equals(Object o) {
+        for (int i = 0; i < elementCount; i++)
+            if (!((MultipleKey) o).elementAt(i).equals(elementData[i]))
+                return false;
+        return true;
+    }
+
+    /**
+     * Computes a hash code of the MultipleKey by adding the individual hashcodes of each element
+     * 
+     * @return The hashcode of the MultipleKey
+     */
+    public int hashCode() {
+        int ret = 0;
+        for (int i = 0; i < elementCount; i++)
+            ret += elementData[i].hashCode();
+        return ret;
+    }
 }
-
-
