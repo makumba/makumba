8f31f7e376958ffce601ae29639a4bebc5e18c5d
Manuel Bernhardt <manuel@bernhardt.io>
2007-07-25 11:53:04 +0000

    Formatting source, improving javadoc

--- makumba/classes/org/makumba/Attributes.java
+++ makumba/classes/org/makumba/Attributes.java
@@ -23,39 +23,58 @@
 
 package org.makumba;
 
-/** The attributes provided by a makumba environment (eg a http session).
- * Attributes can be referred and assigned to in business logic code. 
+/**
+ * The attributes provided by a makumba environment (for example a http session). Attributes can be referred and assigned to in
+ * business logic code.
+ * 
+ * @author Cristian Bogdan
+ * @version $Id$
  */
-public interface Attributes
-{
-  /** Get the attribute with the given name. 
-    @param name the name of the attribute
-    @return the attribute value
-   *@throws LogicException if a business logic problem occured while trying to determine the attribute
-   *@throws AttributeNotFoundException if there was no error but the attribute could not be found.
-   */
-  public Object getAttribute(String name) throws LogicException;
+public interface Attributes {
+    /**
+     * Gets the attribute with the given name.
+     * 
+     * @param name
+     *            the name of the attribute
+     * @return the attribute value
+     * @throws LogicException
+     *             if a business logic problem occured while trying to determine the attribute
+     * @throws AttributeNotFoundException
+     *             if there was no error but the attribute could not be found.
+     */
+    public Object getAttribute(String name) throws LogicException;
 
-  /** Set the value of an attribute 
-    @param name the name of the attribute
-    @param value the value of the attribute
-    @return the old value of the attribue, or null if there was none
-    *@throws LogicException if a business logic problem occured while trying to set the attribute (though at present the BL has no supported way to check that).
-    */
-  public Object setAttribute(String name, Object value) throws LogicException;
-  
-  /**
-   * Removes an attribute
-   * @param name the name of the attribute
-   * @throws LogicException if a business logic problem occured while trying to remove the attribute
-   */
-  public void removeAttribute(String name) throws LogicException;
-  
-  /**
-   * Checks whether an attribute exists
-   * @param name the name of the attribute
-   * @return true if the attribute exists, false otherwise.
-   */
-  public boolean hasAttribute(String name);
-  
+    /**
+     * Sets the value of an attribute
+     * 
+     * @param name
+     *            the name of the attribute
+     * @param value
+     *            the value of the attribute
+     * @return the old value of the attribue, or null if there was none
+     * @throws LogicException
+     *             if a business logic problem occured while trying to set the attribute (though at present the BL has
+     *             no supported way to check that).
+     */
+    public Object setAttribute(String name, Object value) throws LogicException;
+
+    /**
+     * Removes an attribute
+     * 
+     * @param name
+     *            the name of the attribute
+     * @throws LogicException
+     *             if a business logic problem occured while trying to remove the attribute
+     */
+    public void removeAttribute(String name) throws LogicException;
+
+    /**
+     * Checks whether an attribute exists
+     * 
+     * @param name
+     *            the name of the attribute
+     * @return true if the attribute exists, false otherwise.
+     */
+    public boolean hasAttribute(String name);
+
 }
