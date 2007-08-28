353892b4115b5b7bffd0adda066f3e2c2df63443
Manuel Bernhardt <manuel@bernhardt.io>
2007-08-28 10:40:36 +0000

    Formatting source

--- makumba/classes/org/makumba/util/WrappedException.java
+++ makumba/classes/org/makumba/util/WrappedException.java
@@ -22,70 +22,88 @@
 /////////////////////////////////////
 
 package org.makumba.util;
+
 import java.io.PrintStream;
 import java.io.PrintWriter;
 
-/** wrap an exception to throw it further as a desired exception type. stacktraces of this exception will actually print the stracktrace of the wrapped exception */
-public class WrappedException extends Exception
-{
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-Throwable t;
+/**
+ * Wraps an exception to throw it further as a desired exception type. stacktraces of this exception will actually print
+ * the stracktrace of the wrapped exception.
+ * 
+ * @author Cristian Bogdan
+ * @author Manuel Gay
+ * 
+ * @version $Id$
+ */
 
-  /** wrap the given exception */
-  public WrappedException(Throwable e)
-  {
-    super(e.toString());
-    this.t=e;
-  }
 
-  /** wrap the given exception */
-  public WrappedException(Throwable e, String s)
-  {
-    super(e.toString()+(s!=null?s:""));
-    this.t=e;
-  }
-  
-  /** wrap nothing */
-  public WrappedException(){};
-  
-  /** wrap nothing, store a message */
-  public WrappedException(String s){ super(s); }
+public class WrappedException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
-  /** return the wrapped exception */
-  public Throwable getReason() { return t; }
+    Throwable t;
 
-  /** print the stacktrace of the wrapped exception */
-  public void printStackTrace() 
-  {
-    try{
-      t.printStackTrace();  
-    }catch(NullPointerException npe){ super.printStackTrace(); }
-  }
+    /** wrap the given exception */
+    public WrappedException(Throwable e) {
+        super(e.toString());
+        this.t = e;
+    }
 
-  /** print the stacktrace of the wrapped exception */
-  public void printStackTrace(PrintStream ps)
-  { 
-    try{
-      t.printStackTrace(ps);
-    }catch(NullPointerException npe){ super.printStackTrace(ps); }
-  }
+    /** wrap the given exception */
+    public WrappedException(Throwable e, String s) {
+        super(e.toString() + (s != null ? s : ""));
+        this.t = e;
+    }
 
-  /** print the stacktrace of the wrapped exception */
-  public void printStackTrace(PrintWriter ps)
-  { 
-    try{
-      t.printStackTrace(ps);
-    }catch(NullPointerException npe){ super.printStackTrace(ps); }
-  }
+    /** wrap nothing */
+    public WrappedException() {
+    };
 
-  /** the class name of this exception, then the toString of the wrapped */
-  public String toString()
-  { 
-    try{
-      return getClass().getName()+": "+t.toString();
-    }catch(NullPointerException npe){ return super.toString(); }
-  }
+    /** wrap nothing, store a message */
+    public WrappedException(String s) {
+        super(s);
+    }
+
+    /** return the wrapped exception */
+    public Throwable getReason() {
+        return t;
+    }
+
+    /** print the stacktrace of the wrapped exception */
+    public void printStackTrace() {
+        try {
+            t.printStackTrace();
+        } catch (NullPointerException npe) {
+            super.printStackTrace();
+        }
+    }
+
+    /** print the stacktrace of the wrapped exception */
+    public void printStackTrace(PrintStream ps) {
+        try {
+            t.printStackTrace(ps);
+        } catch (NullPointerException npe) {
+            super.printStackTrace(ps);
+        }
+    }
+
+    /** print the stacktrace of the wrapped exception */
+    public void printStackTrace(PrintWriter ps) {
+        try {
+            t.printStackTrace(ps);
+        } catch (NullPointerException npe) {
+            super.printStackTrace(ps);
+        }
+    }
+
+    /** the class name of this exception, then the toString of the wrapped */
+    public String toString() {
+        try {
+            return getClass().getName() + ": " + t.toString();
+        } catch (NullPointerException npe) {
+            return super.toString();
+        }
+    }
 }
