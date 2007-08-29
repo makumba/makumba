fbeaf57f202904272645ab297efc3bcc2e80e200
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2007-08-29 11:21:34 +0000

    formatting code

--- makumba/classes/org/makumba/InvalidValueException.java
+++ makumba/classes/org/makumba/InvalidValueException.java
@@ -24,44 +24,34 @@
 package org.makumba;
 
 /** This exception occurs when an invalid value is passed to a field */
-public class InvalidValueException extends RuntimeException
-{
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class InvalidValueException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 
-public InvalidValueException(String message)
-  {
-    super(message);
-  }
+    public InvalidValueException(String message) {
+        super(message);
+    }
 
-  public InvalidValueException(String field, String message)
-  {
-    super("Invalid value for "+field+": "+message);
-  }
+    public InvalidValueException(String field, String message) {
+        super("Invalid value for " + field + ": " + message);
+    }
 
-  public InvalidValueException(FieldDefinition fi, String message)
-  {
-    this(fi.getDataDefinition().getName()+"#"+fi.getName(), message);
-  }
+    public InvalidValueException(FieldDefinition fi, String message) {
+        this(fi.getDataDefinition().getName() + "#" + fi.getName(), message);
+    }
 
-  /** form an exception message from the required type and the pointer that doesn't respect it */
-  public InvalidValueException(FieldDefinition fi, Class requiredClass, Object value)
-  {
-    this(fi, "Required Java type:"+requiredClass.getName()+" ; given value: "+value+" of type "+value.getClass().getName());
-  }
+    /** form an exception message from the required type and the pointer that doesn't respect it */
+    public InvalidValueException(FieldDefinition fi, Class requiredClass, Object value) {
+        this(fi, "Required Java type:" + requiredClass.getName() + " ; given value: " + value + " of type "
+                + value.getClass().getName());
+    }
 
+    /** form an exception message from the required type and the pointer that doesn't respect it */
+    public InvalidValueException(FieldDefinition fi, String requiredType, Pointer wrongPointer) {
+        this(fi, "Required poiter type:" + requiredType + " ; given value: " + wrongPointer);
+    }
 
-  /** form an exception message from the required type and the pointer that doesn't respect it */
-  public InvalidValueException(FieldDefinition fi, String requiredType, Pointer wrongPointer)
-  {
-    this(fi, "Required poiter type:"+requiredType+" ; given value: "+wrongPointer);
-  }
-
-  /** form an exception message from the compared pointer and the pointer that doesn't match its type */
-  public InvalidValueException(Pointer comparedPointer, Pointer wrongPointer)
-  {
-    super("Compared pointer: "+comparedPointer+" ; given value: "+wrongPointer);
-  }
+    /** form an exception message from the compared pointer and the pointer that doesn't match its type */
+    public InvalidValueException(Pointer comparedPointer, Pointer wrongPointer) {
+        super("Compared pointer: " + comparedPointer + " ; given value: " + wrongPointer);
+    }
 }
--- makumba/classes/org/makumba/controller/html/charEnumEditor.java
+++ makumba/classes/org/makumba/controller/html/charEnumEditor.java
@@ -28,51 +28,52 @@ import org.makumba.view.RecordFormatter;
 
 public class charEnumEditor extends choiceEditor {
 
-	private static final class SingletonHolder {
-		static final FieldEditor singleton = new charEnumEditor();
-	}
+    private static final class SingletonHolder {
+        static final FieldEditor singleton = new charEnumEditor();
+    }
 
-	/** Don't use this, use getInstance() */
-	protected charEnumEditor() {}
+    /** Don't use this, use getInstance() */
+    protected charEnumEditor() {
+    }
 
-	public static FieldFormatter getInstance() {
-		return SingletonHolder.singleton;
-	}
+    public static FieldFormatter getInstance() {
+        return SingletonHolder.singleton;
+    }
 
-	public Object getOptions(RecordFormatter rf, int fieldIndex, java.util.Dictionary fP) {
-		return null;
-	}
+    public Object getOptions(RecordFormatter rf, int fieldIndex, java.util.Dictionary fP) {
+        return null;
+    }
 
-	public int getOptionsLength(RecordFormatter rf, int fieldIndex, Object opts) {
-		return rf.dd.getFieldDefinition(fieldIndex).getEnumeratorSize();
-	}
+    public int getOptionsLength(RecordFormatter rf, int fieldIndex, Object opts) {
+        return rf.dd.getFieldDefinition(fieldIndex).getEnumeratorSize();
+    }
 
-	public Object getOptionValue(RecordFormatter rf, int fieldIndex, Object options, int i) {
-		return rf.dd.getFieldDefinition(fieldIndex).getStringAt(i);
-	}
+    public Object getOptionValue(RecordFormatter rf, int fieldIndex, Object options, int i) {
+        return rf.dd.getFieldDefinition(fieldIndex).getStringAt(i);
+    }
 
-	public String formatOptionValue(RecordFormatter rf, int fieldIndex, Object val) {
-		return val.toString();
-	}
+    public String formatOptionValue(RecordFormatter rf, int fieldIndex, Object val) {
+        return val.toString();
+    }
 
-	public String formatOptionValue(RecordFormatter rf, int fieldIndex, Object opts, int i, Object val) {
-		return val.toString();
-	}
+    public String formatOptionValue(RecordFormatter rf, int fieldIndex, Object opts, int i, Object val) {
+        return val.toString();
+    }
 
-	public String formatOptionTitle(RecordFormatter rf, int fieldIndex, Object options, int i) {
-		return rf.dd.getFieldDefinition(fieldIndex).getNameAt(i);
-	}
+    public String formatOptionTitle(RecordFormatter rf, int fieldIndex, Object options, int i) {
+        return rf.dd.getFieldDefinition(fieldIndex).getNameAt(i);
+    }
 
-	public String getMultiple(RecordFormatter rf, int fieldIndex) {
-		return "";
-	}
+    public String getMultiple(RecordFormatter rf, int fieldIndex) {
+        return "";
+    }
 
-	public boolean isMultiple(RecordFormatter rf, int fieldIndex) {
-		return false;
-	}
+    public boolean isMultiple(RecordFormatter rf, int fieldIndex) {
+        return false;
+    }
 
-	public int getDefaultSize(RecordFormatter rf, int fieldIndex) {
-		return 1;
-	}
+    public int getDefaultSize(RecordFormatter rf, int fieldIndex) {
+        return 1;
+    }
 
 }
