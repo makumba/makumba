756c44212ef505b8d4d26bf89672422293a9e889
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2007-09-02 23:05:26 +0000

    better error messages for nullpointer exceptions; fixing bug 958

--- makumba/classes/org/makumba/view/jsptaglib/TomcatJsp.java
+++ makumba/classes/org/makumba/view/jsptaglib/TomcatJsp.java
@@ -23,16 +23,24 @@
 
 package org.makumba.view.jsptaglib;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
-/*
- import javax.servlet.http.HttpServlet;
- import javax.servlet.http.HttpServletResponse;
- import javax.servlet.ServletConfig;
- import javax.servlet.ServletException;
- import java.io.IOException;
- */
+import org.makumba.devel.SourceViewer;
+import org.makumba.devel.errorViewer;
+
+//
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.ServletConfig;
+// import javax.servlet.ServletException;
+// import java.io.IOException;
 
 /**
  * This class is tomcat-specific because the available APIs do not expose this functionality. Given a servlet request,
@@ -43,9 +51,12 @@ import javax.servlet.http.HttpServletRequest;
  * @author Cristian Bogdan
  * @version $Id$
  */
-public class TomcatJsp
-// extends HttpServlet
-{
+public class TomcatJsp /* extends HttpServlet */{
+
+    public static String[] tomcatKeyWords = { "application", "config", "out", "page" };
+
+    public static ArrayList tomcatKeyWordsList = new ArrayList(Arrays.asList(tomcatKeyWords));
+
     /**
      * Computes the URI to the current JSP
      * 
@@ -71,6 +82,7 @@ public class TomcatJsp
 
     /**
      * Computes the path to the directory in which the compiled JSP is
+     * 
      * @param context the ServletContext of the running tomcat
      * @return A Strong containing the path to the directory where JSPs are being compiled
      */
@@ -82,31 +94,65 @@ public class TomcatJsp
         return "org.apache.jasper";
     }
 
-    /*
-     * when uncommenting the line below, add jasper-compiler.jar to the compilation classpath
-     * 
-     * example configuration:
-     * 
-     * <servlet> <servlet-name>jspMak</servlet-name> <servlet-class>org.makumba.view.jsptaglib.JspServlet</servlet-class>
-     * <init-param> <param-name>logVerbosityLevel</param-name> <param-value>WARNING</param-value> </init-param>
-     * 
-     * <init-param> <param-name>jspCompilerPlugin</param-name>
-     * <param-value>org.apache.jasper.compiler.JikesJavaCompiler</param-value> </init-param>
-     * 
-     * <load-on-startup>3</load-on-startup> </servlet>
-     * 
-     * 
-     * 
-     * <!-- The mapping for the JSP servlet --> <servlet-mapping> <servlet-name>jspMak</servlet-name>
-     * <url-pattern>*.jsp</url-pattern> </servlet-mapping>
-     * 
-     * 
-     * HttpServlet decorated = new org.apache.jasper.servlet.JspServlet();
-     * 
-     * public void init(ServletConfig config) throws ServletException { decorated.init(config); }
-     * 
-     * public void service (HttpServletRequest request, HttpServletResponse response) throws ServletException,
-     * IOException { System.out.println("jsp hook: "+getJspPath(request)); decorated.service(request, response); }
-     * public void destroy() { decorated.destroy(); }
-     */
+    public static boolean treatException(Throwable original, Throwable t, PrintWriter wr, HttpServletRequest req,
+            HttpServlet servlet) {
+        if (t.getMessage().indexOf("Duplicate local variable") != -1) {
+            String message = t.getMessage();
+            String[] split = message.split("\n");
+            String variableName = null;
+            String errorLine = null;
+            for (int i = 0; i < split.length; i++) {
+                if (split[i].startsWith("An error occurred at line:")) {
+                    errorLine = split[i];
+                } else if (split[i].startsWith("Duplicate local variable")) {
+                    variableName = split[i].substring("Duplicate local variable".length()).trim();
+                }
+            }
+            if (variableName != null && tomcatKeyWordsList.contains(variableName)) {
+                String body = errorLine + "\n\n";
+                body += "'" + variableName + "' is a reserverd keyword in Tomcat!\n";
+                body += "Do not use it as name for your Java variables, or as <mak:value expr=\"...\" var=\""
+                        + variableName + "\" /> resp. <mak:value expr=\"...\" printVar=\"" + variableName + "\" />";
+                String hiddenBody = t.getMessage();
+                String title = "Programmer Error - usage of reserved Tomcat keyword";
+                try {
+                    SourceViewer sw = new errorViewer(req, servlet, title, body, hiddenBody);
+                    sw.parseText(wr);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new org.makumba.util.RuntimeWrappedException(e);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    //
+    // when uncommenting the line below, add jasper-compiler.jar to the compilation classpath
+    // 
+    // example configuration:
+    // 
+    // <servlet> <servlet-name>jspMak</servlet-name>
+    // <servlet-class>org.makumba.view.jsptaglib.JspServlet</servlet-class>
+    // <init-param> <param-name>logVerbosityLevel</param-name> <param-value>WARNING</param-value> </init-param>
+    // 
+    // <init-param> <param-name>jspCompilerPlugin</param-name>
+    // <param-value>org.apache.jasper.compiler.JikesJavaCompiler</param-value> </init-param>
+    // 
+    // <load-on-startup>3</load-on-startup> </servlet>
+    // 
+    // 
+    // 
+    // <!-- The mapping for the JSP servlet --> <servlet-mapping> <servlet-name>jspMak</servlet-name>
+    // <url-pattern>*.jsp</url-pattern> </servlet-mapping>
+    // 
+    // 
+    // HttpServlet decorated = new org.apache.jasper.servlet.JspServlet();
+    // 
+    // public void init(ServletConfig config) throws ServletException { decorated.init(config); }
+    // 
+    // public void service (HttpServletRequest request, HttpServletResponse response) throws ServletException,
+    // IOException { System.out.println("jsp hook: "+getJspPath(request)); decorated.service(request, response); }
+    // public void destroy() { decorated.destroy(); }
+    //
 }
