9ef3d59b3ac66a710e92c40068433e09e53f1e33
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2007-09-04 21:41:20 +0000

    collecting all form validation errors into composite exception; added method to find unassigned exceptions

--- trunk/makumba/classes/org/makumba/controller/html/RecordEditor.java
+++ trunk/makumba/classes/org/makumba/controller/html/RecordEditor.java
@@ -23,13 +23,19 @@
 
 package org.makumba.controller.html;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.makumba.DataDefinition;
 import org.makumba.FieldDefinition;
+import org.makumba.InvalidValueException;
+import org.makumba.CompositeValidationException;
 import org.makumba.view.FieldFormatter;
 import org.makumba.view.RecordFormatter;
 
@@ -39,9 +45,10 @@ import org.makumba.view.RecordFormatter;
  * 
  * @author Cristian Bogdan
  * @version $Id$
- * 
  */
 public class RecordEditor extends RecordFormatter {
+    private static final long serialVersionUID = 1L;
+
     String database;
 
     String[] db;
@@ -58,25 +65,51 @@ public class RecordEditor extends RecordFormatter {
         query = new String[ri.getFieldNames().size()];
     }
 
-    public Dictionary readFrom(HttpServletRequest req, String suffix) {
-        Dictionary data = new Hashtable();
+    public ArrayList getUnassignedExceptions(CompositeValidationException e, ArrayList unassignedExceptions,
+            HttpServletRequest req, String suffix) {
         for (int i = 0; i < dd.getFieldNames().size(); i++) {
             FieldEditor fe = (FieldEditor) formatterArray[i];
-            if (fe.getInputName(this, i, suffix) == null)
-                continue;
-            Object o = fe.readFrom(this, i, org.makumba.controller.http.RequestAttributes.getParameters(req), suffix);
-            if (o != null)
-                o = dd.getFieldDefinition(i).checkValue(o);
-            else
-                o = dd.getFieldDefinition(i).getNull();
+            Collection exceptions = e.getExceptions(fe.getInputName(this, i, suffix));
+            for (Iterator iter = exceptions.iterator(); iter.hasNext();) {
+                unassignedExceptions.remove(iter.next());
+            }
+        }
+        return unassignedExceptions;
+    }
 
-            org.makumba.controller.http.RequestAttributes.setAttribute(req, fe.getInputName(this, i, suffix) + "_type",
-                    dd.getFieldDefinition(i));
+    public Dictionary readFrom(HttpServletRequest req, String suffix) {
+        Dictionary data = new Hashtable();
+        Vector exceptions = new Vector(); // will collect all exceptions from the field validity checks
+        for (int i = 0; i < dd.getFieldNames().size(); i++) {
+            FieldEditor fe = (FieldEditor) formatterArray[i];
+            try {
+                if (fe.getInputName(this, i, suffix) == null) {
+                    continue;
+                }
+                Object o = fe.readFrom(this, i, org.makumba.controller.http.RequestAttributes.getParameters(req),
+                    suffix);
+                if (o != null) {
+                    o = dd.getFieldDefinition(i).checkValue(o);
+                } else {
+                    o = dd.getFieldDefinition(i).getNull();
+                }
 
-            if (o != null)
-                // the data is written in the dictionary without the suffix
-                data.put(fe.getInputName(this, i, ""), o);
-            org.makumba.controller.http.RequestAttributes.setAttribute(req, fe.getInputName(this, i, suffix), o);
+                org.makumba.controller.http.RequestAttributes.setAttribute(req, fe.getInputName(this, i, suffix)
+                        + "_type", dd.getFieldDefinition(i));
+
+                if (o != null) {
+                    // the data is written in the dictionary without the suffix
+                    data.put(fe.getInputName(this, i, ""), o);
+                }
+                org.makumba.controller.http.RequestAttributes.setAttribute(req, fe.getInputName(this, i, suffix), o);
+            } catch (InvalidValueException e) {
+                // if there is an exception in this field
+                // we store it in the hash, together with the field definition where it occured
+                exceptions.add(e);
+            }
+        }
+        if (exceptions.size() > 0) {
+            throw new CompositeValidationException(exceptions);
         }
         return data;
     }
@@ -93,55 +126,55 @@ public class RecordEditor extends RecordFormatter {
         for (int i = 0; i < dd.getFieldNames().size(); i++) {
             FieldDefinition fd = dd.getFieldDefinition(i);
             switch (fd.getIntegerType()) {
-            case FieldDefinition._ptr:
-                formatterArray[i] = ptrEditor.getInstance();
-                break;
-            case FieldDefinition._ptrOne:
-            case FieldDefinition._setComplex:
-                formatterArray[i] = FieldEditor.getInstance();
-                break;
-            case FieldDefinition._int:
-                formatterArray[i] = intEditor.getInstance();
-                break;
-            case FieldDefinition._intEnum:
-                formatterArray[i] = intEnumEditor.getInstance();
-                break;
-            case FieldDefinition._char:
-                formatterArray[i] = charEditor.getInstance();
-                break;
-            case FieldDefinition._charEnum:
-                formatterArray[i] = charEnumEditor.getInstance();
-                break;
-            case FieldDefinition._text:
-                formatterArray[i] = textEditor.getInstance();
-                break;
-            case FieldDefinition._date:
-                formatterArray[i] = dateEditor.getInstance();
-                break;
-            case FieldDefinition._set:
-                formatterArray[i] = setEditor.getInstance();
-                break;
-            // case FieldDefinition._nil:
-            // formatterArray[i] = nilEditor.getInstance();
-            // break;
-            case FieldDefinition._real:
-                formatterArray[i] = realEditor.getInstance();
-                break;
-            case FieldDefinition._setCharEnum:
-                formatterArray[i] = setcharEnumEditor.getInstance();
-                break;
-            case FieldDefinition._setIntEnum:
-                formatterArray[i] = setintEnumEditor.getInstance();
-                break;
-            case FieldDefinition._dateCreate:
-            case FieldDefinition._dateModify:
-            case FieldDefinition._ptrIndex:
-            case FieldDefinition._ptrRel:
-                formatterArray[i] = errorEditor.getInstance();
-                break;
-            default:
-                throw new RuntimeException(
-                        "Internal Makumba error: Unknown FieldDefinition type lead to invalid formatter content. Please report to developers.");
+                case FieldDefinition._ptr:
+                    formatterArray[i] = ptrEditor.getInstance();
+                    break;
+                case FieldDefinition._ptrOne:
+                case FieldDefinition._setComplex:
+                    formatterArray[i] = FieldEditor.getInstance();
+                    break;
+                case FieldDefinition._int:
+                    formatterArray[i] = intEditor.getInstance();
+                    break;
+                case FieldDefinition._intEnum:
+                    formatterArray[i] = intEnumEditor.getInstance();
+                    break;
+                case FieldDefinition._char:
+                    formatterArray[i] = charEditor.getInstance();
+                    break;
+                case FieldDefinition._charEnum:
+                    formatterArray[i] = charEnumEditor.getInstance();
+                    break;
+                case FieldDefinition._text:
+                    formatterArray[i] = textEditor.getInstance();
+                    break;
+                case FieldDefinition._date:
+                    formatterArray[i] = dateEditor.getInstance();
+                    break;
+                case FieldDefinition._set:
+                    formatterArray[i] = setEditor.getInstance();
+                    break;
+                // case FieldDefinition._nil:
+                // formatterArray[i] = nilEditor.getInstance();
+                // break;
+                case FieldDefinition._real:
+                    formatterArray[i] = realEditor.getInstance();
+                    break;
+                case FieldDefinition._setCharEnum:
+                    formatterArray[i] = setcharEnumEditor.getInstance();
+                    break;
+                case FieldDefinition._setIntEnum:
+                    formatterArray[i] = setintEnumEditor.getInstance();
+                    break;
+                case FieldDefinition._dateCreate:
+                case FieldDefinition._dateModify:
+                case FieldDefinition._ptrIndex:
+                case FieldDefinition._ptrRel:
+                    formatterArray[i] = errorEditor.getInstance();
+                    break;
+                default:
+                    throw new RuntimeException(
+                            "Internal Makumba error: Unknown FieldDefinition type lead to invalid formatter content. Please report to developers.");
             }
         }
     }
