72891e647b954adc4c3fcc0f3e1f22bbd8b91a70
Manuel Bernhardt <manuel@bernhardt.io>
2007-09-14 17:43:53 +0000

    Reimporting changes lost while attempting to recover in CVS

--- makumba/classes/org/makumba/abstr/RecordInfo.java
+++ makumba/classes/org/makumba/abstr/RecordInfo.java
@@ -24,6 +24,7 @@
 //TODO extra comments about changes from refactoring
 
 package org.makumba.abstr;
+
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -40,306 +41,365 @@ import org.makumba.util.NamedResourceFactory;
 import org.makumba.util.NamedResources;
 import org.makumba.util.RuntimeWrappedException;
 
-/** This is the internal representation of the org.makumba. One can make
- * RecordHandlers based on an instance of this class and do useful things
- * with it (generate sql tables, html code, etc)
+/**
+ * This is the internal representation of the org.makumba. One can make
+ * RecordHandlers based on an instance of this class and do useful things with
+ * it (generate sql tables, html code, etc)
  */
-public class RecordInfo implements java.io.Serializable, DataDefinition
-{
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-java.net.URL origin;
-  String name;
-  Properties templateValues;
-  //  Vector templateArgumentNames;
+public class RecordInfo implements java.io.Serializable, DataDefinition {
+    /**
+         * 
+         */
+    private static final long serialVersionUID = 1L;
 
-  Vector fieldOrder= new Vector();
-  String title;
-  String indexName;
-  static final String createName= "TS_create";
-  static final String modifyName= "TS_modify";
+    java.net.URL origin;
 
-  // for set and setComplex subtables
-  String mainPtr;
+    String name;
 
-  // for set tables, also used for setintEnum and setcharEnum to store the name of the int or char field
-  String setField;
+    Properties templateValues;
 
-  // nr of relations, 0= none, 1= 1:n, 2= m:n
-  int relations= 0;
+    // Vector templateArgumentNames;
 
-  Hashtable fields= new Hashtable();
-  // Hashtable fieldIndexes=null;
+    Vector fieldOrder = new Vector();
 
-  // for subtables
-  String subfield;
-  String ptrSubfield="";
-  String subfieldPtr="";
-  RecordInfo papa;
+    String title;
 
-  void addStandardFields(String name)
-  {
-    FieldInfo fi;
+    String indexName;
 
-    indexName=name;
-    
-    fi= new FieldInfo(this, indexName);
-    fi.type= "ptrIndex";
-    fi.description= "Unique index";
-    fi.fixed= true;
-    fi.notNull=true;
-    fi.unique=true;
-    addField1(fi);
+    static final String createName = "TS_create";
 
-    fi= new FieldInfo(this, modifyName);
-    fi.type= "dateModify";
-    fi.notNull=true;
-    fi.description= "Last modification date";
-    addField1(fi);
+    static final String modifyName = "TS_modify";
 
-    fi=new FieldInfo(this, createName);
-    fi.type= "dateCreate";
-    fi.description= "Creation date";
-    fi.fixed= true;
-    fi.notNull=true;
-    addField1(fi);
-  }
+    // for set and setComplex subtables
+    String mainPtr;
 
-  public boolean isTemporary()
-  {
-    return origin==null;
-  }
+    // for set tables, also used for setintEnum and setcharEnum to store the
+        // name of the int or char field
+    String setField;
 
-  RecordInfo(){ name= "temp"+hashCode(); } 
-  
-  /** make a temporary recordInfo that is only used for query results*/
-  public RecordInfo(String name)
-  {
-     this.name= name;
-     origin=null;
-  }
-  
-  protected void addField1(FieldDefinition fi)
-  {
-    fieldOrder.addElement(fi.getName());
-    fields.put(fi.getName(), fi);
-    ((FieldInfo)fi).dd=this;
-  }
+    // nr of relations, 0= none, 1= 1:n, 2= m:n
+    int relations = 0;
 
-  /** only meant for building of temporary types */
-  public void addField(FieldDefinition fi)
-  {
-    if(!isTemporary())
-      throw new RuntimeException("can't add field to non-temporary type");
-    addField1(fi);
-    //  the field cannot be of set type...
-    //if(fieldIndexes==null)
-    //  fieldIndexes=new Hashtable();
-    //fieldIndexes.put(fi.getName(), new Integer(fieldIndexes.size()));
-  }
+    Hashtable fields = new Hashtable();
 
-  RecordInfo(java.net.URL origin, String path)
-  {
-    name= path;
-    this.origin= origin;
-    //    templateArgumentNames= new Vector(0);
-  }
+    // Hashtable fieldIndexes=null;
 
-  RecordInfo(RecordInfo ri, String subfield)
-  {
-    //    initStandardFields(subfield);
-    name= ri.name;
-    origin= ri.origin;
-    this.subfield= subfield;
-    this.papa= ri;
-    //    this.templateArgumentNames= ri.templateArgumentNames;
-    ptrSubfield= papa.ptrSubfield+"->"+subfield;
-    subfieldPtr= papa.subfieldPtr+subfield+"->";
-  }
+    // for subtables
+    String subfield;
 
-  static int infos= NamedResources.makeStaticCache
-  ("Data definitions parsed",
-   new NamedResourceFactory()
-   {
-     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	protected Object getHashObject(Object name)
-       {
-	 java.net.URL u= RecordParser.findDataDefinition((String)name, "mdd");
-	 if(u== null)
-	   { throw new DataDefinitionNotFoundError((String)name); }
-	 return u;
-       }
+    String ptrSubfield = "";
 
-     protected Object makeResource(Object name, Object hashName) 
-       {
-	 String nm=(String)name;
-	 if(nm.indexOf('/')!=-1)
-	   nm=nm.replace('/', '.').substring(1);
-	 return new RecordInfo((java.net.URL)hashName, nm);
-      }
-     protected void configureResource(Object name, Object hashName, Object resource)
-       {
-	 new RecordParser().parse((RecordInfo)resource);
-       }
-   }
-    );
+    String subfieldPtr = "";
 
-/** returns the record info with the given absolute name
-   *  @throws org.makumba.DataDefinitionNotFoundError if the name is not a valid record info name
-   *  @throws org.makumba.DataDefinitionParseError if the syntax is wrong or a referred resource can't be found
-   */
-  public static DataDefinition getRecordInfo(String name)	
-  {
-    int n= name.indexOf("->");
-    if (n==-1)
-      {
-	try{
-	  return getSimpleRecordInfo(name);
-	}catch(DataDefinitionNotFoundError e)
-	  {
-	    n= name.lastIndexOf(".");
-	    if(n==-1)
-	      throw e;
-	    try{
-	      return getRecordInfo(name.substring(0, n)+"->"+name.substring(n+1));
-	      }
-	    catch(DataDefinitionParseError f){ throw e; }
-	  }
-      }
-    
-    DataDefinition ri= getRecordInfo(name.substring(0, n));
-    while(true)
-      {
-	name= name.substring(n+2);
-	n= name.indexOf("->");
-	if(n==-1)
-	  break;
-	ri= ri.getFieldDefinition(name.substring(0, n)).getSubtable();
-      }
-    ri=ri.getFieldDefinition(name).getSubtable();
-    return ri;
-  }
+    RecordInfo papa;
 
-  
-  public static synchronized DataDefinition getSimpleRecordInfo(String path)
-  {
-    // this is to avoid a stupid error if path is "..."
-    boolean dot=false;
-    for(int i=0; i<path.length(); i++)
-     {
-      if(path.charAt(i)=='.')
-	{
-	  if(dot)
-	    throw new DataDefinitionParseError("two consecutive dots not allowed in type name");
-	  dot=true;
+    void addStandardFields(String name) {
+	FieldInfo fi;
+
+	indexName = name;
+
+	fi = new FieldInfo(this, indexName);
+	fi.type = "ptrIndex";
+	fi.description = "Unique index";
+	fi.fixed = true;
+	fi.notNull = true;
+	fi.unique = true;
+	addField1(fi);
+
+	fi = new FieldInfo(this, modifyName);
+	fi.type = "dateModify";
+	fi.notNull = true;
+	fi.description = "Last modification date";
+	addField1(fi);
+
+	fi = new FieldInfo(this, createName);
+	fi.type = "dateCreate";
+	fi.description = "Creation date";
+	fi.fixed = true;
+	fi.notNull = true;
+	addField1(fi);
+    }
+
+    public boolean isTemporary() {
+	return origin == null;
+    }
+
+    RecordInfo() {
+	name = "temp" + hashCode();
+    }
+
+    /** make a temporary recordInfo that is only used for query results */
+    public RecordInfo(String name) {
+	this.name = name;
+	origin = null;
+    }
+
+    protected void addField1(FieldDefinition fi) {
+	fieldOrder.addElement(fi.getName());
+	fields.put(fi.getName(), fi);
+	((FieldInfo) fi).dd = this;
+    }
+
+    /** only meant for building of temporary types */
+    public void addField(FieldDefinition fi) {
+	if (!isTemporary())
+	    throw new RuntimeException("can't add field to non-temporary type");
+	addField1(fi);
+	// the field cannot be of set type...
+	// if(fieldIndexes==null)
+	// fieldIndexes=new Hashtable();
+	// fieldIndexes.put(fi.getName(), new Integer(fieldIndexes.size()));
+    }
+
+    RecordInfo(java.net.URL origin, String path) {
+	name = path;
+	this.origin = origin;
+	// templateArgumentNames= new Vector(0);
+    }
+
+    RecordInfo(RecordInfo ri, String subfield) {
+	// initStandardFields(subfield);
+	name = ri.name;
+	origin = ri.origin;
+	this.subfield = subfield;
+	this.papa = ri;
+	// this.templateArgumentNames= ri.templateArgumentNames;
+	ptrSubfield = papa.ptrSubfield + "->" + subfield;
+	subfieldPtr = papa.subfieldPtr + subfield + "->";
+    }
+
+    static int infos = NamedResources.makeStaticCache(
+	    "Data definitions parsed", new NamedResourceFactory() {
+		/**
+                 * 
+                 */
+		private static final long serialVersionUID = 1L;
+
+		protected Object getHashObject(Object name) {
+		    java.net.URL u = RecordParser.findDataDefinition(
+			    (String) name, "mdd");
+		    if (u == null) {
+			throw new DataDefinitionNotFoundError((String) name);
+		    }
+		    return u;
+		}
+
+		protected Object makeResource(Object name, Object hashName) {
+		    String nm = (String) name;
+		    if (nm.indexOf('/') != -1)
+			nm = nm.replace('/', '.').substring(1);
+		    return new RecordInfo((java.net.URL) hashName, nm);
+		}
+
+		protected void configureResource(Object name, Object hashName,
+			Object resource) {
+		    new RecordParser().parse((RecordInfo) resource);
+		}
+	    });
+
+    /**
+         * returns the record info with the given absolute name
+         * 
+         * @throws org.makumba.DataDefinitionNotFoundError
+         *                 if the name is not a valid record info name
+         * @throws org.makumba.DataDefinitionParseError
+         *                 if the syntax is wrong or a referred resource can't
+         *                 be found
+         */
+    public static DataDefinition getRecordInfo(String name) {
+	int n = name.indexOf("->");
+	if (n == -1) {
+	    try {
+		return getSimpleRecordInfo(name);
+	    } catch (DataDefinitionNotFoundError e) {
+		n = name.lastIndexOf(".");
+		if (n == -1)
+		    throw e;
+		try {
+		    return getRecordInfo(name.substring(0, n) + "->"
+			    + name.substring(n + 1));
+		} catch (DataDefinitionParseError f) {
+		    throw e;
+		}
+	    }
 	}
-      else dot=false;
 
-      //check if type name looks valid (no weird characters or spaces)
-      if( path.charAt(i)!='/' && path.charAt(i)!='.' )
-	if(i==0 && !Character.isJavaIdentifierStart(path.charAt(i)) || i>0 && !Character.isJavaIdentifierPart(path.charAt(i)) )
-	  throw new DataDefinitionParseError("Invalid character \""+path.charAt(i)+"\" in type name \""+path+"\"");
-     }
-	
-    if(path.indexOf('/')!=-1){
-      path=path.replace('/', '.');
-      if(path.charAt(0)=='.')
-	path=path.substring(1);
+	DataDefinition ri = getRecordInfo(name.substring(0, n));
+	while (true) {
+	    name = name.substring(n + 2);
+	    n = name.indexOf("->");
+	    if (n == -1)
+		break;
+	    ri = ri.getFieldDefinition(name.substring(0, n)).getSubtable();
+	}
+	ri = ri.getFieldDefinition(name).getSubtable();
+	return ri;
     }
-    
-    DataDefinition ri=null;
-    try{
-      ri= (DataDefinition)NamedResources.getStaticCache(infos).getResource(path);
-    }catch(RuntimeWrappedException e)
-      {
-	if(e.getReason() instanceof DataDefinitionParseError)
-	  throw (DataDefinitionParseError)e.getReason();
-	if(e.getReason() instanceof MakumbaError)
-	  throw (MakumbaError)e.getReason();
-	throw e;
-      }
-    if(path.indexOf("./")==-1)
-      ((RecordInfo)ri).name= path;
-    else
-      MakumbaSystem.getMakumbaLogger("debug.abstr").severe("shit happens: "+path);
-    return ri;
-  }
-  
-  /** returns all the field names */
-  public Vector getFieldNames(){ return (Vector)fieldOrder.clone(); }
 
-  /** returns the field info associated with a name */
-  public FieldDefinition getFieldDefinition(String nm){ return (FieldDefinition)fields.get(nm); }
+    public static synchronized DataDefinition getSimpleRecordInfo(String path) {
+	// this is to avoid a stupid error if path is "..."
+	boolean dot = false;
+	for (int i = 0; i < path.length(); i++) {
+	    if (path.charAt(i) == '.') {
+		if (dot)
+		    throw new DataDefinitionParseError(
+			    "two consecutive dots not allowed in type name");
+		dot = true;
+	    } else
+		dot = false;
 
-  /** the field with the respective index, null if such a field doesn't exist */
-  public FieldDefinition getFieldDefinition(int n)
-  {
-    if(n<0 ||n>= fieldOrder.size())
-      return null;
-    return getFieldDefinition((String)fieldOrder.elementAt(n));
-  }
+	    // check if type name looks valid (no weird characters or
+                // spaces)
+	    if (path.charAt(i) != '/' && path.charAt(i) != '.')
+		if (i == 0 && !Character.isJavaIdentifierStart(path.charAt(i))
+			|| i > 0
+			&& !Character.isJavaIdentifierPart(path.charAt(i)))
+		    throw new DataDefinitionParseError("Invalid character \""
+			    + path.charAt(i) + "\" in type name \"" + path
+			    + "\"");
+	}
 
-  /** returns the path-like abstract-level name of this record info */
-  public String getName(){ return name+ptrSubfield; }
+	if (path.indexOf('/') != -1) {
+	    path = path.replace('/', '.');
+	    if (path.charAt(0) == '.')
+		path = path.substring(1);
+	}
 
-  // TODO: see if this method is still needed, can't  it be expressed in terms of the DataDefinition interface?
-  String getBaseName(){ return name; }
-  
-  /** if this is a subtable, the field prefix is maintable->
-   * TODO: see if this method is still needed, can't  it be expressed in terms of the DataDefinition interface?
-   */
-  public String fieldPrefix(){ return subfieldPtr; }
-
-  DataDefinition makeSubtable(String name){ return new RecordInfo(this, name); }
-
-  /** which is the name of the index field, if any? */
-  public String getIndexPointerFieldName(){ return indexName; }
-
-  /** which is the name of the pointer to the main table, for set and internal set subtables */
-  public String getSetOwnerFieldName(){ return mainPtr; }
-
-  //-----------
-  /** the title field indicated, or the default one */
-  public String getTitleFieldName(){ return title; }
-
-  /** which is the name of the creation timestamp field, if any? */
-  public String getCreationDateFieldName(){ return createName; }
-
-  /** which is the name of the modification timestamp field, if any? */
-  public String getLastModificationDateFieldName(){ return modifyName; }
-
-  /** If this type is the data pointed by a 1-1 pointer or subset, return the type of the main record, otherwise return null */
-  public FieldDefinition getParentField(){
-    if(papa==null)
-      return null;
-    return papa.getFieldDefinition(subfield); 
-  }
-
-  /** which is the name of the set member (Pointer, Character or Integer), for set subtables */
-  public String getSetMemberFieldName(){ return setField ; }
-  
-  
-
-//moved from RecordHandler
-  public void checkFieldNames(Dictionary d)
-  {
-    for(Enumeration e=d.keys(); e.hasMoreElements(); ) {
-        Object o = e.nextElement();
-        if(!(o instanceof String))
-          throw new org.makumba.NoSuchFieldException(this, "Dictionaries passed to makumba DB operations should have String keys. Key <"+o+"> is of type "+o.getClass()+getName());
-        if(this.getFieldDefinition((String)o)==null)
-          throw new org.makumba.NoSuchFieldException(this, (String)o);
-        String checkFieldName = (String)o;
+	DataDefinition ri = null;
+	try {
+	    ri = (DataDefinition) NamedResources.getStaticCache(infos)
+		    .getResource(path);
+	} catch (RuntimeWrappedException e) {
+	    if (e.getReason() instanceof DataDefinitionParseError)
+		throw (DataDefinitionParseError) e.getReason();
+	    if (e.getReason() instanceof MakumbaError)
+		throw (MakumbaError) e.getReason();
+	    throw e;
+	}
+	if (path.indexOf("./") == -1)
+	    ((RecordInfo) ri).name = path;
+	else
+	    MakumbaSystem.getMakumbaLogger("debug.abstr").severe(
+		    "shit happens: " + path);
+	return ri;
     }
-  }
-  public String toString(){ return getName(); }
 
-public java.net.URL getOrigin() {
-    return origin;
-}
+    /** returns all the field names */
+    public Vector getFieldNames() {
+	return (Vector) fieldOrder.clone();
+    }
+
+    /** returns the field info associated with a name */
+    public FieldDefinition getFieldDefinition(String nm) {
+	return (FieldDefinition) fields.get(nm);
+    }
+
+    /**
+         * the field with the respective index, null if such a field doesn't
+         * exist
+         */
+    public FieldDefinition getFieldDefinition(int n) {
+	if (n < 0 || n >= fieldOrder.size())
+	    return null;
+	return getFieldDefinition((String) fieldOrder.elementAt(n));
+    }
+
+    /** returns the path-like abstract-level name of this record info */
+    public String getName() {
+	return name + ptrSubfield;
+    }
+
+    // TODO: see if this method is still needed, can't it be expressed in
+        // terms of the DataDefinition interface?
+    String getBaseName() {
+	return name;
+    }
+
+    /**
+         * if this is a subtable, the field prefix is maintable-> TODO: see if
+         * this method is still needed, can't it be expressed in terms of the
+         * DataDefinition interface?
+         */
+    public String fieldPrefix() {
+	return subfieldPtr;
+    }
+
+    DataDefinition makeSubtable(String name) {
+	return new RecordInfo(this, name);
+    }
+
+    /** which is the name of the index field, if any? */
+    public String getIndexPointerFieldName() {
+	return indexName;
+    }
+
+    /**
+         * which is the name of the pointer to the main table, for set and
+         * internal set subtables
+         */
+    public String getSetOwnerFieldName() {
+	return mainPtr;
+    }
+
+    // -----------
+    /** the title field indicated, or the default one */
+    public String getTitleFieldName() {
+	return title;
+    }
+
+    /** which is the name of the creation timestamp field, if any? */
+    public String getCreationDateFieldName() {
+	return createName;
+    }
+
+    /** which is the name of the modification timestamp field, if any? */
+    public String getLastModificationDateFieldName() {
+	return modifyName;
+    }
+
+    /**
+         * If this type is the data pointed by a 1-1 pointer or subset, return
+         * the type of the main record, otherwise return null
+         */
+    public FieldDefinition getParentField() {
+	if (papa == null)
+	    return null;
+	return papa.getFieldDefinition(subfield);
+    }
+
+    /**
+         * which is the name of the set member (Pointer, Character or Integer),
+         * for set subtables
+         */
+    public String getSetMemberFieldName() {
+	return setField;
+    }
+
+    // moved from RecordHandler
+    public void checkFieldNames(Dictionary d) {
+	for (Enumeration e = d.keys(); e.hasMoreElements();) {
+	    Object o = e.nextElement();
+	    if (!(o instanceof String))
+		throw new org.makumba.NoSuchFieldException(this,
+			"Dictionaries passed to makumba DB operations should have String keys. Key <"
+				+ o + "> is of type " + o.getClass()
+				+ getName());
+	    if (this.getFieldDefinition((String) o) == null)
+		throw new org.makumba.NoSuchFieldException(this, (String) o);
+	    String checkFieldName = (String) o;
+	}
+    }
+
+    public String toString() {
+	return getName();
+    }
+
+    public java.net.URL getOrigin() {
+	return origin;
+    }
+
+    public long lastModified() {
+	return new java.io.File(this.getOrigin().getFile()).lastModified();
+    }
+
 }
