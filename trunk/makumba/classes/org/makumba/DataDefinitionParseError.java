76ef830eb5bb65b5cb53a26010c213a8050dde17
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2007-09-16 21:13:06 +0000

    adding validation definition parse error, changing class hierarchy to reuse from data defintion parse error

--- trunk/makumba/classes/org/makumba/DataDefinitionParseError.java
+++ trunk/makumba/classes/org/makumba/DataDefinitionParseError.java
@@ -23,25 +23,16 @@
 
 package org.makumba;
 
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Vector;
-
 /**
  * Error occured during data definition parsing. It can contain a number of errors occured at different lines during
  * parsing
+ * 
+ * @author Cristian Bogdan
+ * @version $Id$
  */
-public class DataDefinitionParseError extends MakumbaError {
-    /**
-     * 
-     */
+public class DataDefinitionParseError extends DefinitionParseError {
+
     private static final long serialVersionUID = 1L;
-
-    String typeName;
-
-    String line;
-
-    int column;
 
     public DataDefinitionParseError() {
     }
@@ -49,12 +40,6 @@ public class DataDefinitionParseError extends MakumbaError {
     /** Construct a message from the given explanation */
     public DataDefinitionParseError(String explanation) {
         super(explanation);
-    }
-
-    static String showTypeName(String typeName) {
-        if (typeName.startsWith("temp"))
-            return "";
-        return typeName + ":";
     }
 
     /** Construct a message for an error that is due to an IOException */
@@ -71,9 +56,7 @@ public class DataDefinitionParseError extends MakumbaError {
 
     /** Construct a message for a line */
     public DataDefinitionParseError(String typeName, String reason, String line) {
-        super(showTypeName(typeName) + reason + "\n" + line);
-        this.typeName = typeName;
-        this.line = line;
+        super(typeName, reason, line);
     }
 
     /** Construct a message for a line and column */
@@ -82,52 +65,5 @@ public class DataDefinitionParseError extends MakumbaError {
         this.typeName = typeName;
         this.line = line;
         this.column = column;
-    }
-
-    /** return the type for which the error occured */
-    public String getTypeName() {
-        return typeName;
-    }
-
-    /** put a marker for a given column */
-    static public StringBuffer pointError(int column) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < column; i++)
-            sb.append(' ');
-        return sb.append('^');
-    }
-
-    Vector components;
-
-    Hashtable lines;
-
-    /** tells whether this error is empty or contains sub-errors */
-    public boolean isSingle() {
-        return components == null || components.isEmpty();
-    }
-
-    /** add another error to the main error */
-    public void add(DataDefinitionParseError e) {
-        if (components == null)
-            components = new Vector();
-
-        components.addElement(e);
-        if (e.line != null) {
-            if (lines == null)
-                lines = new Hashtable();
-            lines.put(e.line, e);
-        }
-    }
-
-    /** If the error is single, call the default action, else compose all components' messages */
-    public String getMessage() {
-        if (isSingle())
-            return super.getMessage();
-
-        StringBuffer sb = new StringBuffer();
-
-        for (Enumeration e = components.elements(); e.hasMoreElements();)
-            sb.append('\n').append(((DataDefinitionParseError) e.nextElement()).getMessage()).append('\n');
-        return sb.toString();
     }
 }
