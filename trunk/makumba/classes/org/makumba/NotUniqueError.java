2394b82342ed22b4793844cee0d98695f7f589e6
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2007-09-19 21:12:28 +0000

    left-over of what is needed for notunique error, should be removed as well.

--- trunk/makumba/classes/org/makumba/NotUniqueError.java
+++ trunk/makumba/classes/org/makumba/NotUniqueError.java
@@ -23,64 +23,20 @@
 
 package org.makumba;
 
-import java.util.Dictionary;
-import java.util.Enumeration;
+import org.makumba.db.sql.SQLUpdate;
 
-import org.makumba.util.StringUtils;
-
-/** An insert in a certain type has violated a unique constraint */
+/**
+ * An insert in a certain type has violated a unique constraint.<br>
+ * Note: this class has mostly been replaced by {@link NotUniqueException} in combination with
+ * {@link CompositeValidationException}, which has more or less the same functionality as this class used to have, but
+ * is ready for form annotation. This class is used onyl in
+ * {@link SQLUpdate#execute(org.makumba.db.DBConnection, Object[])}, usage there should also be stopped.
+ */
 public class NotUniqueError extends DBError {
     private static final long serialVersionUID = 1L;
-
-    String type;
-
-    Dictionary duplicates;
 
     public NotUniqueError(java.sql.SQLException se) {
         super("Not unique exception. " + se.getMessage());
     }
 
-    /** Build a NotUniqueError for the given type, with the duplicated field names and values indicated as a Dictionary */
-    public NotUniqueError(String type, Dictionary duplicates) {
-        super(makeMessage(type, duplicates));
-        this.type = type;
-        this.duplicates = duplicates;
-    }
-
-    static String makeMessage(String type, Dictionary duplicates) {
-        StringBuffer sb = new StringBuffer();
-        String separator = "";
-        for (Enumeration e = duplicates.keys(); e.hasMoreElements();) {
-            Object field = e.nextElement();
-            sb.append(separator);
-            if (field instanceof String[]) { // multi-field uniquness error
-                sb.append("The field-combination ").append(StringUtils.toString((String[]) field)).append(
-                    " in the record ").append(type).append(
-                    " only accepts unique values. There is however already an entry that has its values set to ").append(
-                    StringUtils.toString((Object[]) duplicates.get(field))).append("!");
-            } else { // other errors (should be all single fields
-                sb.append("The field <").append(field).append("> in the record ").append(type).append(
-                    " only accepts unique values. There is however already an entry that has its value set to '").append(
-                    duplicates.get(field)).append("'!");
-            }
-            sb.append("\nPlease go back and correct the values in the form!");
-            separator = "\n";
-        }
-        return sb.toString();
-    }
-
-    /** return the type where the duplicate was attempted */
-    public String getType() {
-        return type;
-    }
-
-    /** return the list of fields that were attempted be duplicated */
-    public Enumeration getDuplicateFieldNames() {
-        return duplicates.keys();
-    }
-
-    /** return the value that was attempted be duplicated for the given field */
-    public Object getDuplicateForField(String field) {
-        return duplicates.get(field);
-    }
 }
