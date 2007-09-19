ecf7f6192b5813a37ea4babcd91d4c6c641a9802
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2007-09-19 13:08:46 +0000

    slight fixes

--- trunk/makumba/classes/org/makumba/controller/html/KruseCalendarEditor.java
+++ trunk/makumba/classes/org/makumba/controller/html/KruseCalendarEditor.java
@@ -14,38 +14,37 @@ public class KruseCalendarEditor implements CalendarEditorProvider {
 
     public StringBuffer formatEditorCode(String inputName, String calendarLinkFormatting) {
         StringBuffer sb = new StringBuffer();
-        if (calendarLinkFormatting != null) {
-            MakumbaSystem.getCalendarProvider();
-            sb.append("<script language=\"javascript\" type=\"text/javascript\">\n");
-            String calendarName = inputName + "Calendar";
-            String anchorname = inputName + "Anchor";
-            String divname = "makCalendarDiv";
-            sb.append("var " + calendarName + " = new CalendarPopup(\"" + divname + "\");\n");
-            sb.append(calendarName + ".showYearNavigation();\n");
-            sb.append(calendarName + ".setReturnFunction(\"setMultipleValues" + inputName + "\");\n");
-            sb.append("function setMultipleValues" + inputName + "(y,m,d) {\n");
+        MakumbaSystem.getCalendarProvider();
+        sb.append("\n<script language=\"javascript\" type=\"text/javascript\">\n");
+        String calendarName = inputName + "Calendar";
+        String anchorname = inputName + "Anchor";
+        String divname = "makCalendarDiv";
+        sb.append("var " + calendarName + " = new CalendarPopup(\"" + divname + "\");\n");
+        sb.append(calendarName + ".showYearNavigation();\n");
+        sb.append(calendarName + ".setReturnFunction(\"setMultipleValues" + inputName + "\");\n");
+        sb.append("function setMultipleValues" + inputName + "(y,m,d) {\n");
 
-            String dayInput = "document.getElementById('" + inputName + "_0')";
-            // TODO: elementById is not necessarily unique, if you have the same field in different forms ? (not sure!,
-            // maybe it is unique?)
-            // therefore, maybe better would be to know the form index, and then use forms[index].name ..
-            sb.append("  " + dayInput + ".selectedIndex=LZ(d)-1;\n");
-            String monthInput = "document.getElementById('" + inputName + "_1')";
-            sb.append("  " + monthInput + ".selectedIndex=LZ(m)-1;\n");
-            String yearInput = "document.getElementById('" + inputName + "_2')";
-            sb.append("  " + yearInput + ".value=y;\n");
-            sb.append("};\n");
-            sb.append("</script>");
-            String dateString = "getDateString(" + yearInput + ", " + monthInput + ", " + dayInput + ")";
-            // TODO: this will default to 1/1/1900, maybe we should default to today?
-            sb.append("<a class=\"" + calendarEditorClass + "\" href=\"#\" onClick=\"" + calendarName
-                    + ".showCalendar('" + anchorname + "', " + dateString
-                    + "); return false;\" title=\"Pick date in calendar\" name=\"" + anchorname + "\" ID=\""
-                    + anchorname + "\">" + calendarLinkFormatting + "</a>\n");
-            sb.append("<div id=\""
-                    + divname
-                    + "\" STYLE=\"position:absolute; visibility:hidden; background-color:white; layer-background-color:white;\"></div>");
-        }
+        String dayInput = "document.getElementById('" + inputName + "_0')";
+        // TODO: elementById is not necessarily unique, if you have the same field in different forms ? (not sure!,
+        // maybe it is unique?)
+        // therefore, maybe better would be to know the form index, and then use forms[index].name ..
+        sb.append("  " + dayInput + ".selectedIndex=LZ(d)-1;\n");
+        String monthInput = "document.getElementById('" + inputName + "_1')";
+        sb.append("  " + monthInput + ".selectedIndex=LZ(m)-1;\n");
+        String yearInput = "document.getElementById('" + inputName + "_2')";
+        sb.append("  " + yearInput + ".value=y;\n");
+        sb.append("};\n");
+        sb.append("</script>\n");
+        String dateString = "getDateString(" + yearInput + ", " + monthInput + ", " + dayInput + ")";
+        String linkText = calendarLinkFormatting != null ? calendarLinkFormatting : " ";
+        // TODO: this will default to 1/1/1900, maybe we should default to today?
+        sb.append("<a class=\"" + calendarEditorClass + "\" href=\"#\" onClick=\"" + calendarName + ".showCalendar('"
+                + anchorname + "', " + dateString + "); return false;\" title=\"Pick date in calendar\" name=\""
+                + anchorname + "\" ID=\"" + anchorname + "\">" + linkText + "</a>\n");
+        sb.append("<div id=\""
+                + divname
+                + "\" style=\"position:absolute; visibility:hidden; background-color:white; layer-background-color:white;\"></div>\n");
+
         return sb;
     }
 
