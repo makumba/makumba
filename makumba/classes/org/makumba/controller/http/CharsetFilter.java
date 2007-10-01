c750ea203c3c1ef4e3476742a3e55de41a352be5
Manuel Bernhardt <manuel@bernhardt.io>
2007-10-01 08:39:45 +0000

    Formatting source

--- makumba/classes/org/makumba/controller/http/CharsetFilter.java
+++ makumba/classes/org/makumba/controller/http/CharsetFilter.java
@@ -11,42 +11,45 @@ import javax.servlet.FilterChain;
 import org.makumba.commons.Configuration;
 import org.makumba.providers.TransactionProvider;
 
+/**
+ * This is a filter making sure the charset encoding is set (for now, only UTF-8 is supported).
+ * 
+ * @author Marius Andra
+ * @author Manuel Gay
+ * @version $Id$
+ */
 public class CharsetFilter implements Filter {
 
-/**
-    this is not used
-**/
-	private String encoding;
-    
-    private Configuration config = new Configuration();
-    
-    private TransactionProvider tp = new TransactionProvider(config);
-	 
-//    static FilterConfig conf;
-    public void init(FilterConfig config) throws ServletException
-    {
-    	encoding = config.getInitParameter("requestEncoding");
-     if( encoding==null ) encoding="UTF-8";
-    }
-    /*public void init(FilterConfig c) { conf=c; }*/
+    /**
+     * this is not used
+     */
+    private String encoding;
 
-    public void doFilter(ServletRequest request, ServletResponse
-	        response, FilterChain chain) throws IOException, ServletException {
-		
-		if(tp.supportsUTF8())
-        {
-		    request.setCharacterEncoding("UTF-8");
-		    response.setContentType("text/html;charset=UTF-8");
+    private Configuration config = new Configuration();
+
+    private TransactionProvider tp = new TransactionProvider(config);
+
+    public void init(FilterConfig config) throws ServletException {
+        encoding = config.getInitParameter("requestEncoding");
+        if (encoding == null)
+            encoding = "UTF-8";
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+
+        if (tp.supportsUTF8()) {
+            request.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html;charset=UTF-8");
         }
 
         chain.doFilter(request, response);
-		if(tp.supportsUTF8())
-        {
-	        response.setContentType("text/html; charset=UTF-8");
-	        request.setCharacterEncoding("UTF8");	
+        if (tp.supportsUTF8()) {
+            response.setContentType("text/html; charset=UTF-8");
+            request.setCharacterEncoding("UTF8");
         }
     }
-    
-    public void destroy(){}
-} 
 
+    public void destroy() {
+    }
+}
