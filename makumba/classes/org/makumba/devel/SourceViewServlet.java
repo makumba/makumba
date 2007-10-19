16b37f1d8cdfa334755ffefdfdd99ae0ff9079ea
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2007-10-19 00:26:00 +0000

    formatting source

--- makumba/classes/org/makumba/devel/SourceViewServlet.java
+++ makumba/classes/org/makumba/devel/SourceViewServlet.java
@@ -33,25 +33,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
- * invoke the necessary SourceViewer, depending on the type of the source the architecture should change, and be organized in filters. example:
+ * invoke the necessary SourceViewer, depending on the type of the source the architecture should change, and be
+ * organized in filters. example:
  * <ul>
  * <li>jspx and jsps: JSP syntax colouring | Java linking | MDD linking | line numbering | header</li>
  * <li>java: Java syntax colouring | Java linking | MDD linking | line numbering | header</li>
  * <li>mdd: syntax_colouring | MDD linking | line numbering | header</li>
  * <li>jspxp: JSP syntax colouring | line numbering | makumba reduction | java linking | mdd linking | header</li>
  * </ul>
- * It's not difficult to get the current architecture to work like that This will be slower but the big advantage is that the Java and JSP syntax
- * colouring (and maybe Java linking) can be outsourced.
+ * It's not difficult to get the current architecture to work like that This will be slower but the big advantage is
+ * that the Java and JSP syntax colouring (and maybe Java linking) can be outsourced.
  */
 public class SourceViewServlet extends HttpServlet {
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         PrintWriter w = res.getWriter();
 
         SourceViewer sw = null;
@@ -73,7 +73,7 @@ public class SourceViewServlet extends HttpServlet {
             res.sendError(404, e.toString());
             return;
         }
-        if (sw != null) //we have a known handler
+        if (sw != null) // we have a known handler
         {
             File dir = sw.getDirectory();
             if (dir == null) {
@@ -84,12 +84,13 @@ public class SourceViewServlet extends HttpServlet {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            } else //try to handle anyway
+            } else // try to handle anyway
             {
                 if (redirected(req, res, servletPath)) {
                     return;
                 }
-                if (sw instanceof GeneratedCodeViewer && req.getPathInfo().endsWith("/")) { // redirect browsing of directories to mdd Viewer
+                if (sw instanceof GeneratedCodeViewer && req.getPathInfo().endsWith("/")) { // redirect browsing of
+                                                                                            // directories to mdd Viewer
                     res.sendRedirect(req.getContextPath() + "/dataDefinitions" + req.getPathInfo());
                     return;
                 }
@@ -99,7 +100,8 @@ public class SourceViewServlet extends HttpServlet {
                 if (dir.getAbsolutePath().indexOf("classes/") != -1) { // class viewer
                     relativeDirectory = dir.getAbsolutePath().substring(dir.getAbsolutePath().indexOf("classes"));
                 } else if (dir.getAbsolutePath().indexOf("dataDefinitions/") != -1) { // MDD viewer
-                    relativeDirectory = dir.getAbsolutePath().substring(dir.getAbsolutePath().indexOf("dataDefinitions"));
+                    relativeDirectory = dir.getAbsolutePath().substring(
+                        dir.getAbsolutePath().indexOf("dataDefinitions"));
                 }
                 res.setContentType("text/html");
                 printDirlistingHeader(w, dir, relativeDirectory);
@@ -111,7 +113,7 @@ public class SourceViewServlet extends HttpServlet {
                 if (sw instanceof javaViewer) {
                     // process and display directories
                     processDirectory(w, dir, ".java");
-                    
+
                     // process and display files
                     String[] list = dir.list();
                     Arrays.sort(list);
@@ -139,23 +141,26 @@ public class SourceViewServlet extends HttpServlet {
                         }
                     }
                 } else {
-                    java.util.logging.Logger.getLogger("org.makumba." + "devel").warning("Don't know how to handle viewer: " + sw + "(" + sw.getClass() + ")");
+                    java.util.logging.Logger.getLogger("org.makumba." + "devel").warning(
+                        "Don't know how to handle viewer: " + sw + "(" + sw.getClass() + ")");
                 }
                 w.println("</pre>");
                 DevelUtils.printDeveloperSupportFooter(w);
                 w.println("</body></html>");
             }
-        } else{
+        } else {
             res.setContentType("text/html");
             w.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-            w.println("<html><head><title>Error in source viewer</title><body>");            
-            w.println("Error in source viewer servlet '"+ getClass().getName() + "' - unknown source type: " + servletPath);
+            w.println("<html><head><title>Error in source viewer</title><body>");
+            w.println("Error in source viewer servlet '" + getClass().getName() + "' - unknown source type: "
+                    + servletPath);
             DevelUtils.printDeveloperSupportFooter(w);
             w.println("</body></html>");
         }
     }
 
-    public static boolean redirected(HttpServletRequest req, HttpServletResponse res, String servletPath) throws IOException {
+    public static boolean redirected(HttpServletRequest req, HttpServletResponse res, String servletPath)
+            throws IOException {
         if (req.getPathInfo() == null) {
             if (servletPath.startsWith("/"))
                 servletPath = servletPath.substring(1);
@@ -174,9 +179,8 @@ public class SourceViewServlet extends HttpServlet {
         w.println("<html><head><title>" + relativeDirectory + "</title>");
         w.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" >");
 
-        w .println("</head><body bgcolor=white><table width=\"100%\" bgcolor=\"lightblue\"><tr><td rowspan=\"2\">");
-        w.print("<font size=\"+2\"><a href=\".\"><font color=\"darkblue\">"
-                + relativeDirectory + "</font></a></font>");
+        w.println("</head><body bgcolor=white><table width=\"100%\" bgcolor=\"lightblue\"><tr><td rowspan=\"2\">");
+        w.print("<font size=\"+2\"><a href=\".\"><font color=\"darkblue\">" + relativeDirectory + "</font></a></font>");
         w.print("<font size=\"-1\"><br>" + dir.getCanonicalPath() + "</font>");
         w.print("</td>");
 
