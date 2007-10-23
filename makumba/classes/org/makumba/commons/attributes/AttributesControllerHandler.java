2b789d2304f114db62f33d04411a7cb10d2bf127
Manuel Bernhardt <manuel@bernhardt.io>
2007-10-23 10:03:55 +0000

    Refactoring ControllerFilter in order to separate concerns

---
+++ makumba/classes/org/makumba/commons/attributes/AttributesControllerHandler.java
@@ -0,0 +1,45 @@
+package org.makumba.commons.attributes;
+
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.makumba.LogicException;
+import org.makumba.controller.ControllerHandler;
+
+public class AttributesControllerHandler extends ControllerHandler {
+
+    @Override
+    public void afterFilter(ServletRequest request, ServletResponse response, FilterConfig conf) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean beforeFilter(ServletRequest request, ServletResponse response, FilterConfig conf) throws Exception {
+        if (!checkAttributes(request, response))
+            return false;
+        
+        return true;
+    }
+
+    @Override
+    public boolean onError(ServletRequest request, ServletResponse response, Exception e) {
+        
+        // we simply pass it on
+        return true;
+        
+    }
+
+    private boolean checkAttributes(ServletRequest req, ServletResponse resp) throws LogicException {
+        RequestAttributes.getAttributes((HttpServletRequest) req);
+        return true;
+    }
+
+    @Override
+    public void finalize(ServletRequest request, ServletResponse response) {
+        
+    }
+
+}
