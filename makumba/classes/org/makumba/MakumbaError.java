5af8f90a99a92ad00062c7f58cbe395d675620e6
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2007-11-06 23:45:40 +0000

    a true chained exception

--- makumba/classes/org/makumba/MakumbaError.java
+++ makumba/classes/org/makumba/MakumbaError.java
@@ -36,20 +36,16 @@ import java.io.PrintWriter;
 public class MakumbaError extends Error {
     private static final long serialVersionUID = 1L;
 
-    Throwable t;
-
     String explanation;
 
     /** Build a makumba error and associate it with the given reason */
     public MakumbaError(Throwable reason) {
-        super(reason.toString());
-        this.t = reason;
+        super(reason);
     }
 
     /** Build a makumba error and associate it with the given reason and explanation text */
     public MakumbaError(Throwable reason, String expl) {
-        super(reason.toString() + (expl != null ? expl : ""));
-        this.t = reason;
+        super(expl, reason);
         this.explanation = expl;
     }
 
@@ -62,47 +58,32 @@ public class MakumbaError extends Error {
         super(explanation);
     }
 
-    /** Return the reason for this error */
-    public Throwable getReason() {
-        return t;
-    }
-
-    /** Print the stacktrace of the reason exception, if any, otherwise print a normal stack trace */
+   /** Print the stacktrace of the reason exception, if any, otherwise print a normal stack trace */
     public void printStackTrace() {
         if (explanation != null)
             System.out.println(explanation);
-        if (t == null)
-            super.printStackTrace();
-        else
-            t.printStackTrace();
+        super.printStackTrace();
     }
 
     /** Print the stacktrace of the reason exception, if any, otherwise print a normal stack trace */
     public void printStackTrace(PrintStream ps) {
-        try {
             if (explanation != null)
                 ps.println(explanation);
-            t.printStackTrace(ps);
-        } catch (NullPointerException npe) {
+
             super.printStackTrace(ps);
-        }
     }
 
     /** Print the stacktrace of the reason exception, if any, otherwise print a normal stack trace */
     public void printStackTrace(PrintWriter ps) {
-        try {
             if (explanation != null)
                 ps.println(explanation);
-            t.printStackTrace(ps);
-        } catch (NullPointerException npe) {
             super.printStackTrace(ps);
-        }
     }
 
     /** A string representation of this error, for debugging */
     public String toString() {
         try {
-            return getClass().getName() + ": " + t.toString();
+            return getClass().getName() + ": " + getCause().toString();
         } catch (NullPointerException npe) {
             return super.toString();
         }
