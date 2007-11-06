e2634e0624e8d2d7b8e7dd13061dd50111ed7c87
Manuel Bernhardt <manuel@bernhardt.io>
2007-11-06 17:32:46 +0000

    Removing useless import

--- makumba/classes/org/makumba/commons/MakumbaJspAnalyzer.java
+++ makumba/classes/org/makumba/commons/MakumbaJspAnalyzer.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
