a7ffe6b120d02d50933befa67eb5a371845fb11c
Manuel Bernhardt <manuel@bernhardt.io>
2007-11-06 17:39:12 +0000

    Adding TODO

--- makumba/classes/org/makumba/providers/TransactionProviderInterface.java
+++ makumba/classes/org/makumba/providers/TransactionProviderInterface.java
@@ -5,6 +5,8 @@ import org.makumba.Transaction;
 /**
  * This is an interface for a Transaction provider, useful to perform data operations against a datastore.
  * 
+ * TODO to be refactored and Transformed into a superclass for the different kind of TPs out there
+ * 
  * @author Manuel Gay
  * @version $Id: TransactionProviderInterface.java,v 1.1 28.09.2007 15:43:42 Manuel Exp $
  */
