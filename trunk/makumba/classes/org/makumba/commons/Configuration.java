fdafffffa59548db981a3e968e32fe873d551e32
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2007-11-06 17:33:54 +0000

    Moving generic code to more generic place (but it shouldn't stay there)

--- trunk/makumba/classes/org/makumba/commons/Configuration.java
+++ trunk/makumba/classes/org/makumba/commons/Configuration.java
@@ -1,6 +1,11 @@
 package org.makumba.commons;
 
 import java.io.Serializable;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.makumba.db.Database;
+import org.makumba.db.MakumbaTransactionProvider;
 
 /**
  * This class knows how to read Makumba configuration and is used internally by different classes that need specifc services.
@@ -13,19 +18,21 @@ public class Configuration implements Serializable {
     
     private static final long serialVersionUID = 1L;
 
-    private String defaultDataDefinitionProvider = "org.makumba.providers.datadefinition.makumba.MakumbaDataDefinitionFactory";
+    private static final String defaultDataDefinitionProvider = "org.makumba.providers.datadefinition.makumba.MakumbaDataDefinitionFactory";
     
-    private String defaultTransactionProvider = "org.makumba.db.MakumbaTransactionProvider";
+    private static final String defaultTransactionProvider = "org.makumba.db.MakumbaTransactionProvider"; //"org.makumba.db.hibernate.HibernateTransactionProvider";
     
-    public String getDefaultDataDefinitionProviderClass() {
-        return this.defaultDataDefinitionProvider;
+    private String getDefaultDataDefinitionProviderClass() {
+        return defaultDataDefinitionProvider;
     }
     
-    public String getDefaultTransactionProviderClass() {
-        return this.defaultTransactionProvider;
+    private String getDefaultTransactionProviderClass() {
+        return defaultTransactionProvider;
     }
     
+    private String dataDefinitionProvider = null;
     
+    private String transactionProvider = null;
     
     /**
      * Gives the data definition provider implementation to use
@@ -34,7 +41,7 @@ public class Configuration implements Serializable {
     public String getDataDefinitionProviderClass() {
         
         // FIXME this should lookup a configuration file and return whatever is specified there
-        return getDefaultDataDefinitionProviderClass();
+        return (dataDefinitionProvider == null) ? getDefaultDataDefinitionProviderClass() : dataDefinitionProvider;
     }
     
     /**
@@ -44,9 +51,96 @@ public class Configuration implements Serializable {
     public String getTransactionProviderClass() {
         
         // FIXME this should lookup a configuration file and return whatever is specified there
-        return getDefaultTransactionProviderClass();
+        return (transactionProvider == null) ? getDefaultTransactionProviderClass() : transactionProvider;
         
     }
     
+    public void setDataDefinitionProvider(String ddp) {
+        this.dataDefinitionProvider = ddp;
+    }
+    
+    public void setTransactionProvider(String tp) {
+        this.transactionProvider = tp;
+    }
+
+    //FIXME should be somewhere else
+    static String findInHostProperties(Properties p, String str){
+        for (Enumeration e = p.keys(); e.hasMoreElements();) {
+            String s = (String) e.nextElement();
+            int i = s.indexOf('#');
+            try {
+                if ((i==-1|| java.net.InetAddress.getByName(s.substring(0, i)).equals(java.net.InetAddress.getLocalHost()))
+                        && str.endsWith(s.substring(i + 1)))
+                    return p.getProperty(s);
+            } catch (java.net.UnknownHostException uhe) {
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * finds the database name of the server according to the host name and current directory. If none is specified, a
+     * default is used, if available
+     * 
+     * FIXME should be somewhere else
+     * 
+     */
+    public static String findDatabaseName(Properties p) {
+        String userDir= System.getProperty("user.dir");
+        String n;
+        java.net.URL u= ClassResource.get("/"); 
+        String wbp= u!=null?u.toString():null;
+        
+        if(
+                (n= findInHostProperties(p, userDir))!=null 
+                ||
+                wbp!=null &&((n= findInHostProperties(p, wbp))!=null)
+                ||
+                (n= findInHostProperties(p, "default"))!=null
+                )
+          
+            return n;
+        
+        return p.getProperty("default");
+    }
+
+    //FIXME should be somewhere else
+    public static Database findDatabase(Properties p) {
+        return MakumbaTransactionProvider.getDatabase(findDatabaseName(p));
+    }
+
+    //FIXME should be somewhere else
+    public static Database findDatabase(String s) {
+        return MakumbaTransactionProvider.getDatabase(findDatabaseName(s));
+    }
+
+    //FIXME should be somewhere else
+    static int dbsel = NamedResources.makeStaticCache("Database selection files", new NamedResourceFactory() {
+        
+        private static final long serialVersionUID = 1L;
+
+        protected Object makeResource(Object nm) {
+            Properties p = new Properties();
+            try {
+                java.io.InputStream input = org.makumba.commons.ClassResource.get((String) nm).openStream();
+                p.load(input);
+                input.close();
+            } catch (Exception e) {
+                throw new org.makumba.ConfigFileError((String) nm);
+            }
+            return p;
+        }
+    });
+
+    //FIXME should be somewhere else
+    public static String findDatabaseName(String s) {
+        try {
+            return findDatabaseName((Properties) NamedResources.getStaticCache(dbsel).getResource(s));
+        } catch (RuntimeWrappedException e) {
+            if (e.getCause() instanceof org.makumba.MakumbaError)
+                throw (org.makumba.MakumbaError) e.getCause();
+            throw e;
+        }
+    }
 
 }
