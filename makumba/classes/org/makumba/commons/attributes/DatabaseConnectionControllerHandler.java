d4b52ff21528c73c65d50ad7c959c6d4399f9f63
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2008-02-24 21:23:26 +0000

    more general, focusing on finalization

--- makumba/classes/org/makumba/commons/attributes/DatabaseConnectionControllerHandler.java
+++ makumba/classes/org/makumba/commons/attributes/DatabaseConnectionControllerHandler.java
@@ -9,26 +9,23 @@ import org.makumba.commons.ControllerHandler;
 import org.makumba.commons.DbConnectionProvider;
 
 public class DatabaseConnectionControllerHandler extends ControllerHandler {
-    
-    private DbConnectionProvider dbcp;
 
     @Override
     public boolean beforeFilter(ServletRequest request, ServletResponse response, FilterConfig conf) throws Exception {
-        // initalises a database pool (one connection per database) needed for the response (BL execution) and the attributes (BL attribute lookup)
-        dbcp = RequestAttributes.getConnectionProvider((HttpServletRequest) request);
+        // we make sure it's initialized, this is not actually needed
+        RequestAttributes.getConnectionProvider((HttpServletRequest) request);
         return true;
     }
 
 
     @Override
     public void afterBeforeFilter(ServletRequest request, ServletResponse response, FilterConfig conf) {
-        dbcp.close();
+        finalize(request, response);
     }
 
     @Override
     public void finalize(ServletRequest request, ServletResponse response) {
-        dbcp.close();
-        
+        RequestAttributes.getConnectionProvider((HttpServletRequest) request).close();
     }
 
 }
