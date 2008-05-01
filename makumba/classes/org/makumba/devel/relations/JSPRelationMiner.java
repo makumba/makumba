a25e85cb8d1035f4dec94e2da52dd99c24cdecee
Manuel Bernhardt <manuel@bernhardt.io>
2008-05-01 20:27:11 +0000

    Adding method to get the errors of the mining of a set of files

--- makumba/classes/org/makumba/devel/relations/JSPRelationMiner.java
+++ makumba/classes/org/makumba/devel/relations/JSPRelationMiner.java
@@ -46,6 +46,7 @@ public class JSPRelationMiner extends RelationMiner {
             // page analysis failed
             logger.warning(
                 "Page analysis for page " + path + " failed due to error: " + t.getMessage());
+            rc.addJSPAnalysisError(t);
             return;
         }
 
