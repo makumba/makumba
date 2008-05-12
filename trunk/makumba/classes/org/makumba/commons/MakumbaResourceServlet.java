c984a289336ae10ed7dc4a7759fe1fa440769b19
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-05-12 22:14:19 +0000

    reading makumba resources from jar file

--- trunk/makumba/classes/org/makumba/commons/MakumbaResourceServlet.java
+++ trunk/makumba/classes/org/makumba/commons/MakumbaResourceServlet.java
@@ -6,12 +6,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.JarURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -50,8 +54,18 @@ public class MakumbaResourceServlet extends HttpServlet {
         String resource = requestURI.substring(requestURI.indexOf(servletPath) + servletPath.length());
         URL url = ClassResource.get(resourceDirectory + resource);
         try {
-            File file = new File(url.toURI());
-            if (file.isDirectory()) {
+            File file = null;
+            JarEntry jarEntry = null;
+            JarFile jarFile = null;
+            if (!url.toExternalForm().startsWith("jar:")) {
+                file = new File(url.toURI());
+            } else {
+                jarFile = ((JarURLConnection) url.openConnection()).getJarFile();
+                String[] jarURL = url.toExternalForm().split("!");
+                jarEntry = jarFile.getJarEntry(jarURL[1].substring(1));
+            }
+            // check if we have a jar file, or a directory directory
+            if (file != null && file.isDirectory()) {
                 // do a directory viewing
                 PrintWriter writer = resp.getWriter();
                 resp.setContentType("text/html");
@@ -65,7 +79,7 @@ public class MakumbaResourceServlet extends HttpServlet {
                     relativeDirectory = file.getAbsolutePath().substring(
                         file.getAbsolutePath().indexOf(resourceDirectory));
                 }
-                SourceViewServlet.printDirlistingHeader(writer, file, relativeDirectory);
+                SourceViewServlet.printDirlistingHeader(writer, file.getCanonicalPath(), relativeDirectory);
 
                 if (!(relativeDirectory.equals(resourceDirectory))) {
                     writer.println("<b><a href=\"../\">../</a></b> (up one level)");
@@ -88,8 +102,72 @@ public class MakumbaResourceServlet extends HttpServlet {
                 DevelUtils.writePageEnd(writer);
                 resp.setHeader("Last-Modified", dfLastModified.format(new Date()));
                 return;
-            } else {
-                resp.setHeader("Last-Modified", dfLastModified.format(new Date(file.lastModified())));
+            } else if (jarEntry != null && jarEntry.isDirectory()) {
+                Enumeration<JarEntry> entries = jarFile.entries();
+                ArrayList<String> files = new ArrayList<String>();
+                ArrayList<String> directories = new ArrayList<String>();
+                PrintWriter writer = resp.getWriter();
+                resp.setContentType("text/html");
+                DevelUtils.writePageBegin(writer);
+                DevelUtils.writeTitleAndHeaderEnd(writer, "Makumba resources");
+                if (SourceViewServlet.redirected(req, resp, servletPath)) {
+                    return;
+                }
+                String relativeDirectory = jarEntry.getName();
+                SourceViewServlet.printDirlistingHeader(writer, url.toExternalForm(), relativeDirectory);
+
+                if (!relativeDirectory.equals(resourceDirectory) && !relativeDirectory.equals(resourceDirectory + "/")) {
+                    writer.println("<b><a href=\"../\">../</a></b> (up one level)");
+                }
+
+                while (entries.hasMoreElements()) {
+                    JarEntry entry = (JarEntry) entries.nextElement();
+                    if (entry.getName().startsWith(relativeDirectory)) {
+                        System.out.println("entry name: " + entry.getName());
+                        String s = entry.getName().substring(relativeDirectory.length());
+                        while (s.length() > 0 && s.startsWith("/")) {
+                            s = s.substring(1);
+                        }
+                        System.out.println(s);
+                        if (s.indexOf("/") == -1) { // we have an entry
+                            if (s.length() > 0) {
+                                files.add(s);
+                            }
+                        } else if (s.indexOf("/") == s.lastIndexOf("/") && s.endsWith("/")) { // we have a 1-level dir
+                            if (s.endsWith("/")) { // remove trailing /
+                                s = s.substring(0, s.length() - 1);
+                            }
+                            if (s.length() > 0) {
+                                directories.add(s);
+                            }
+                        }
+                    }
+                }
+
+                // display directories
+                for (String string : directories) {
+                    writer.println("<b><a href=\"" + string + "/\">" + string + "/</a></b>");
+                }
+
+                // display files
+                for (String string : files) {
+                    writer.println("<b><a href=\"" + string + "\">" + string + "</a></b>");
+                }
+
+                writer.println("</pre>");
+                DevelUtils.writePageEnd(writer);
+                resp.setHeader("Last-Modified", dfLastModified.format(new Date()));
+                return;
+            } else { // file or file in jar entry
+                final Date lastModified;
+                if (url.toExternalForm().startsWith("jar:")) { // for jar files, read from the jar
+                    JarFile jf = ((JarURLConnection) url.openConnection()).getJarFile();
+                    String[] jarURL = url.toExternalForm().split("!");
+                    lastModified = new Date(jf.getJarEntry(jarURL[1].substring(1)).getTime());
+                } else {
+                    lastModified = new Date(new File(url.toURI()).lastModified());
+                }
+                resp.setHeader("Last-Modified", dfLastModified.format(lastModified));
                 resp.setContentType(getContentType(url));
                 Object cachedResource = NamedResources.getStaticCache(makumbaResources).getResource(resource);
                 ServletOutputStream outputStream = resp.getOutputStream();
@@ -143,8 +221,17 @@ public class MakumbaResourceServlet extends HttpServlet {
                 }
                 StringBuffer sb = new StringBuffer();
                 URL url = (URL) hashName;
-                // FIXME: we need to handle image types, if we decide we want this
-                InputStream stream = url.openStream();
+                InputStream stream;
+
+                if (url.toExternalForm().startsWith("jar:")) { // for jar files, open the entry
+                    JarFile jf = ((JarURLConnection) url.openConnection()).getJarFile();
+                    String[] jarURL = url.toExternalForm().split("!");
+                    JarEntry je = jf.getJarEntry(jarURL[1].substring(1));
+                    stream = jf.getInputStream(je);
+                    System.out.println("\n\n\nopened jar url: " + je.getName());
+                } else { // for files, simply open a stream
+                    stream = url.openStream();
+                }
                 if (isBinary(url)) {
                     ArrayList<Byte> bytesList = new ArrayList<Byte>();
                     InputStreamReader bis = new InputStreamReader(stream);
