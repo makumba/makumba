3b3b4c994630bb852bb735d7bf35fe75d30cce2a
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-05-13 21:28:32 +0000

    printing infor about currently used database

--- trunk/makumba/classes/org/makumba/devel/DevelUtils.java
+++ trunk/makumba/classes/org/makumba/devel/DevelUtils.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 
 import org.makumba.Pointer;
 import org.makumba.commons.MakumbaResourceServlet;
+import org.makumba.providers.TransactionProvider;
 
 /**
  * This class combines some methods to print pages used by various developer support tools in package org.makumba.devel.
@@ -18,7 +19,9 @@ public class DevelUtils {
     /** Write the page footer to the given writer. */
     public static void printDeveloperSupportFooter(PrintWriter printWriter) throws IOException {
         printWriter.println("<hr><font size=\"-1\"><a href=\"http://www.makumba.org\">Makumba</a> developer support, version: "
-                + org.makumba.MakumbaSystem.getVersion() + "</font>");
+                + org.makumba.MakumbaSystem.getVersion()
+                + "; using database "
+                + TransactionProvider.getInstance().getDefaultDataSourceName() + "</font>");
     }
 
     public static void writeStylesAndScripts(PrintWriter w, String contextPath) {
