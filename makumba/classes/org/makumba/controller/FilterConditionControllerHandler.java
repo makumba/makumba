bdab34316efde65388d84529392aa939fa935dfa
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2008-05-18 12:12:54 +0000

    organising imports, code clean-up

--- makumba/classes/org/makumba/controller/FilterConditionControllerHandler.java
+++ makumba/classes/org/makumba/controller/FilterConditionControllerHandler.java
@@ -17,8 +17,9 @@ public class FilterConditionControllerHandler extends ControllerHandler {
         String uri = req.getRequestURI();
 
         // accesses to the source viewer are not filtered
-        if (uri.startsWith("/dataDefinitions") || uri.startsWith("/logic") || uri.startsWith("/classes"))
+        if (uri.startsWith("/dataDefinitions") || uri.startsWith("/logic") || uri.startsWith("/classes")) {
             return false;
+        }
         String file = null;
         try {
             file = new URL(req.getRequestURL().toString()).getFile();
@@ -26,19 +27,22 @@ public class FilterConditionControllerHandler extends ControllerHandler {
         } // can't be
  
         // JSP and HTML are always filtered
-        if (file.endsWith(".jsp") || file.endsWith(".html"))
+        if (file.endsWith(".jsp") || file.endsWith(".html")) {
             return true;
+        }
 
         // JSPX is never filtered
-        if (file.endsWith(".jspx"))
+        if (file.endsWith(".jspx")) {
             return false;
+        }
 
         // we compute the file that corresponds to the indicated path
         java.io.File f = new java.io.File(conf.getServletContext().getRealPath(uri));
 
         // if it's a directory, there will most probably be a redirection, we filter anyway
-        if (f.isDirectory())
+        if (f.isDirectory()) {
             return true;
+        }
 
         // if the file does not exist on disk, it means that it's produced dynamically, so we filter
         // it it exists, it's probably an image or a CSS, we don't filter
