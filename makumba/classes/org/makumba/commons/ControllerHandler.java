4073fb06a8dafcb7b2cb91a887395993bf4c761f
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2008-05-18 12:04:36 +0000

    formatted code

--- makumba/classes/org/makumba/commons/ControllerHandler.java
+++ makumba/classes/org/makumba/commons/ControllerHandler.java
@@ -12,32 +12,39 @@ import javax.servlet.ServletResponse;
  * @version $Id: ControllerHandler.java,v 1.1 22.10.2007 13:38:15 Manuel Exp $
  */
 public abstract class ControllerHandler {
-    
+
     /**
      * Performs an operation before the doFilterChain() method is called
+     * 
      * @return <code>true</code> if the operation worked out successfully, <code>false</code> otherwise
      * @throws Exception
      */
-    public boolean beforeFilter(ServletRequest request, ServletResponse response, FilterConfig conf) throws Exception
-    { return true; }
-    
-    public void afterBeforeFilter(ServletRequest request, ServletResponse response, FilterConfig conf) throws Exception{};
+    public boolean beforeFilter(ServletRequest request, ServletResponse response, FilterConfig conf) throws Exception {
+        return true;
+    }
+
+    public void afterBeforeFilter(ServletRequest request, ServletResponse response, FilterConfig conf) throws Exception {
+    }
 
     /**
      * Performs an operation after the doFilterChain() method is called
      */
-    public void afterFilter(ServletRequest request, ServletResponse response, FilterConfig conf){};
-    
+    public void afterFilter(ServletRequest request, ServletResponse response, FilterConfig conf) {
+    }
+
     /**
      * Performs an operation when an error occurs
+     * 
      * @return <code>true</code> if this still is an error, <code>false</code> if it was handled
      */
-    public boolean onError(ServletRequest request, ServletResponse response, Throwable e, FilterConfig conf)
-    { return true; }
-    
+    public boolean onError(ServletRequest request, ServletResponse response, Throwable e, FilterConfig conf) {
+        return true;
+    }
+
     /**
      * Performs cleanup operations after the filter operation is executed
      */
-    public void finalize(ServletRequest request, ServletResponse response){}
+    public void finalize(ServletRequest request, ServletResponse response) {
+    }
 
 }
