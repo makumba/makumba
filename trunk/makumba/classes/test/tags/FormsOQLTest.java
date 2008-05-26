9d016d916decc3e63ede06adc7ab7782225a0c70
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-05-26 21:35:45 +0000

    added test for login

--- trunk/makumba/classes/test/tags/FormsOQLTest.java
+++ trunk/makumba/classes/test/tags/FormsOQLTest.java
@@ -35,7 +35,6 @@ import com.meterware.httpunit.WebForm;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
 
-
 /**
  * @author Johannes Peeters
  * @author Manuel Gay
@@ -44,259 +43,333 @@ import com.meterware.httpunit.WebResponse;
 public class FormsOQLTest extends MakumbaJspTestCase {
 
     private boolean record = false;
-    
-	static Pointer person;
-	static Pointer brother;
-	static Pointer address;
-	static Dictionary pc;
-	static Suite setup;
-	static Vector v;
-	static String readPerson = "SELECT p.indiv.name AS name, p.indiv.surname AS surname, p.gender AS gender, p.uniqChar AS uniqChar, p.uniqInt AS uniqInt, p.birthdate AS birthdate, p.weight AS weight, p.TS_modify AS TS_modify, p.TS_create AS TS_create, p.comment AS comment, a.description AS description, a.email AS email, a.usagestart AS usagestart FROM test.Person p, p.address a WHERE p= $1";
-	private String output;
-	private String line;
 
-	static ArrayList languages = new ArrayList();
-	static Object[][] languageData = { { "English", "en" }, { "French", "fr" },
-			{ "German", "de" }, { "Italian", "it" }, { "Spanish", "sp" } };
+    static Pointer person;
 
-	private static final class Suite extends TestSetup {
-		private Suite(Test arg0) {
-			super(arg0);
-		}
+    static Pointer brother;
 
-		protected void setUp() {
+    static Pointer address;
+
+    static Dictionary pc;
+
+    static Suite setup;
+
+    static Vector v;
+
+    static String readPerson = "SELECT p.indiv.name AS name, p.indiv.surname AS surname, p.gender AS gender, p.uniqChar AS uniqChar, p.uniqInt AS uniqInt, p.birthdate AS birthdate, p.weight AS weight, p.TS_modify AS TS_modify, p.TS_create AS TS_create, p.comment AS comment, a.description AS description, a.email AS email, a.usagestart AS usagestart FROM test.Person p, p.address a WHERE p= $1";
+
+    private String output;
+
+    private String line;
+
+    static ArrayList languages = new ArrayList();
+
+    static Object[][] languageData = { { "English", "en" }, { "French", "fr" }, { "German", "de" },
+            { "Italian", "it" }, { "Spanish", "sp" } };
+
+    private static final class Suite extends TestSetup {
+        private Suite(Test arg0) {
+            super(arg0);
+        }
+
+        protected void setUp() {
             TransactionProvider tp = TransactionProvider.getInstance();
-            Transaction db =  tp.getConnectionTo(tp.getDataSourceName("test/testDatabase.properties"));
-            
+            Transaction db = tp.getConnectionTo(tp.getDataSourceName("test/testDatabase.properties"));
+
             insertLanguages(db);
-			insertPerson(db);
-			
-			/* Just a dummy select, so the test_Person__extraData_ is mentioned in the client side part of the tests.
-			 * If this is not done, the server side and the client side will attempt to insert the same primary key
-			 * in the catalog table (because they use the same DBSV, because they use the same database connection file). */
-			db.executeQuery("SELECT p.extraData.something FROM test.Person p WHERE 1=0", null);
-			db.close();
-		}
+            insertPerson(db);
 
-		protected void insertPerson(Transaction db) {
-			Properties p = new Properties();
-			
-			p.put("indiv.name", "bart");
-			brother=db.insert("test.Person", p);
+            /*
+             * Just a dummy select, so the test_Person__extraData_ is mentioned in the client side part of the tests. If
+             * this is not done, the server side and the client side will attempt to insert the same primary key in the
+             * catalog table (because they use the same DBSV, because they use the same database connection file).
+             */
+            db.executeQuery("SELECT p.extraData.something FROM test.Person p WHERE 1=0", null);
+            db.close();
+        }
 
-			p.clear();
-			p.put("indiv.name", "john");
-			
-			Calendar c = Calendar.getInstance();
-			c.clear();
-			c.set(1977, 2, 5);
-			Date birthdate = c.getTime();
-			p.put("birthdate", birthdate);
-					
-			p.put("uniqDate", birthdate);
-			p.put("gender", new Integer(1));
-			p.put("uniqChar", new String("testing \" character field"));
-			
-			p.put("weight", new Double(85.7d));
-			
-			p.put("comment", new Text("This is a text field. It's a comment about this person."));
+        protected void insertPerson(Transaction db) {
+            Properties p = new Properties();
 
-			p.put("uniqInt", new Integer(255));				
-			
-			Vector intSet = new Vector();
-			intSet.addElement(new Integer(1));
-			intSet.addElement(new Integer(0));
-			p.put("intSet", intSet);
+            p.put("indiv.name", "bart");
+            brother = db.insert("test.Person", p);
 
-			p.put("brother", brother);
-			p.put("uniqPtr", languages.get(0));
-			person = db.insert("test.Person", p);
-			
-			p.clear();
-			p.put("description", "");
-			p.put("usagestart", birthdate);
-			p.put("email", "email1");
-			address=db.insert(person, "address", p);
+            p.clear();
+            p.put("indiv.name", "john");
 
-		}
+            Calendar c = Calendar.getInstance();
+            c.clear();
+            c.set(1977, 2, 5);
+            Date birthdate = c.getTime();
+            p.put("birthdate", birthdate);
 
-		protected void deletePerson(Transaction db) {
-			db.delete(address);
-			db.delete(person);
+            p.put("uniqDate", birthdate);
+            p.put("gender", new Integer(1));
+            p.put("uniqChar", new String("testing \" character field"));
+
+            p.put("weight", new Double(85.7d));
+
+            p.put("comment", new Text("This is a text field. It's a comment about this person."));
+
+            p.put("uniqInt", new Integer(255));
+
+            Vector intSet = new Vector();
+            intSet.addElement(new Integer(1));
+            intSet.addElement(new Integer(0));
+            p.put("intSet", intSet);
+
+            p.put("brother", brother);
+            p.put("uniqPtr", languages.get(0));
+            person = db.insert("test.Person", p);
+
+            p.clear();
+            p.put("description", "");
+            p.put("usagestart", birthdate);
+            p.put("email", "email1");
+            address = db.insert(person, "address", p);
+
+        }
+
+        protected void deletePerson(Transaction db) {
+            db.delete(address);
+            db.delete(person);
             // brother is referenced by person so we delete it after person
             db.delete(brother);
-		}
-
-		protected void insertLanguages(Transaction db) {
-			languages.clear();
-			Dictionary p = new Hashtable();
-			for (int i = 0; i < languageData.length; i++) {
-				p.put("name", languageData[i][0]);
-				p.put("isoCode", languageData[i][1]);
-				languages.add(db.insert("test.Language", p));
-			}				
-		}
-
-		protected void deleteLanguages(Transaction db) {
-			for (int i = 0; i<languages.size(); i++)
-				db.delete((Pointer)languages.get(i));
-		}
-
-		public void tearDown() {
-			// do your one-time tear down here!
-            TransactionProvider tp = TransactionProvider.getInstance();
-            Transaction db =  tp.getConnectionTo(tp.getDataSourceName("test/testDatabase.properties"));
-            
-			deletePerson(db);
-			deleteLanguages(db);
-			db.close();
-		}
-	}
-
-	
-	public static Test suite() {
-		setup = new Suite(new TestSuite(FormsOQLTest.class));
-		return setup;
-	}
-	
-    
-    
-    
-    
-    
-    
-    
-	
-	public void beginTomcat(Request request) {
-		WebConversation wc = new WebConversation();
-	    WebRequest     req = new GetMethodWebRequest(System.getProperty("cactus.contextURL"));
-	    try {
-			WebResponse   resp = wc.getResponse( req );
-		} catch (MalformedURLException e) {
-		} catch (IOException e) {
-			setup.tearDown();
-			System.err.println("\n\n\n\n\nYou should run tomcat first! Use mak-tomcat to do that.\n\n");			
-			System.exit(1);
-		} catch (SAXException e) {
-		}		
-	}
-	public void testTomcat(){}
-	
-	public void testMakNewForm() throws ServletException, IOException {
-		pageContext.include("forms-oql/testMakNewForm.jsp");
-	}
-	public void endMakNewForm(WebResponse response) throws Exception {
-		try {
-			output = response.getText(); fetchValidTestResult(output, record);
-		} catch (IOException e) {
-			fail("JSP output error: " + response.getResponseMessage());
-		}
-        assertTrue(compareTest(output));
-		
         }
-    
-	public void beginMakAddForm(Request request) throws MalformedURLException, IOException, SAXException {
-		WebConversation wc = new WebConversation();
-		WebResponse   resp = wc.getResponse( System.getProperty("cactus.contextURL") + "/forms-oql/beginMakAddForm.jsp" );
 
-		// we get the first form in the jsp
-		WebForm form = resp.getForms()[0];
-		// set the input field "email" to "bartolomeus@rogue.be"
-		form.setParameter("email","bartolomeus@rogue.be");
-		// submit the form
-		form.submit();
-	}
-	public void testMakAddForm() throws ServletException, IOException {
-		pageContext.include("forms-oql/testMakAddForm.jsp");
-	}
-	public void endMakAddForm(WebResponse response) throws Exception {
-		try {
-			output = response.getText(); fetchValidTestResult(output, record);
-		} catch (IOException e) {
-			fail("JSP output error: " + response.getResponseMessage());
-		}
-		
+        protected void insertLanguages(Transaction db) {
+            languages.clear();
+            Dictionary p = new Hashtable();
+            for (int i = 0; i < languageData.length; i++) {
+                p.put("name", languageData[i][0]);
+                p.put("isoCode", languageData[i][1]);
+                languages.add(db.insert("test.Language", p));
+            }
+        }
+
+        protected void deleteLanguages(Transaction db) {
+            for (int i = 0; i < languages.size(); i++)
+                db.delete((Pointer) languages.get(i));
+        }
+
+        public void tearDown() {
+            // do your one-time tear down here!
+            TransactionProvider tp = TransactionProvider.getInstance();
+            Transaction db = tp.getConnectionTo(tp.getDataSourceName("test/testDatabase.properties"));
+
+            deletePerson(db);
+            deleteLanguages(db);
+            db.close();
+        }
+    }
+
+    public static Test suite() {
+        setup = new Suite(new TestSuite(FormsOQLTest.class));
+        return setup;
+    }
+
+    public void beginTomcat(Request request) {
+        WebConversation wc = new WebConversation();
+        WebRequest req = new GetMethodWebRequest(System.getProperty("cactus.contextURL"));
+        try {
+            WebResponse resp = wc.getResponse(req);
+        } catch (MalformedURLException e) {
+        } catch (IOException e) {
+            setup.tearDown();
+            System.err.println("\n\n\n\n\nYou should run tomcat first! Use mak-tomcat to do that.\n\n");
+            System.exit(1);
+        } catch (SAXException e) {
+        }
+    }
+
+    public void testTomcat() {
+    }
+
+    public void testMakNewForm() throws ServletException, IOException {
+        pageContext.include("forms-oql/testMakNewForm.jsp");
+    }
+
+    public void endMakNewForm(WebResponse response) throws Exception {
+        try {
+            output = response.getText();
+            fetchValidTestResult(output, record);
+        } catch (IOException e) {
+            fail("JSP output error: " + response.getResponseMessage());
+        }
         assertTrue(compareTest(output));
-	}
-	
-	public void testMakEditForm() throws ServletException, IOException {
-		pageContext.include("forms-oql/testMakEditForm.jsp");
-	}
-	public void endMakEditForm(WebResponse response) throws Exception {
-		try {
-			output = response.getText(); fetchValidTestResult(output, record);
-		} catch (IOException e) {
-			fail("JSP output error: " + response.getResponseMessage());
-		}
+
+    }
+
+    public void beginMakAddForm(Request request) throws MalformedURLException, IOException, SAXException {
+        WebConversation wc = new WebConversation();
+        WebResponse resp = wc.getResponse(System.getProperty("cactus.contextURL") + "/forms-oql/beginMakAddForm.jsp");
+
+        // we get the first form in the jsp
+        WebForm form = resp.getForms()[0];
+        // set the input field "email" to "bartolomeus@rogue.be"
+        form.setParameter("email", "bartolomeus@rogue.be");
+        // submit the form
+        form.submit();
+    }
+
+    public void testMakAddForm() throws ServletException, IOException {
+        pageContext.include("forms-oql/testMakAddForm.jsp");
+    }
+
+    public void endMakAddForm(WebResponse response) throws Exception {
+        try {
+            output = response.getText();
+            fetchValidTestResult(output, record);
+        } catch (IOException e) {
+            fail("JSP output error: " + response.getResponseMessage());
+        }
+
         assertTrue(compareTest(output));
-				
-	}
-	
-	public void testMakForm() throws ServletException, IOException, SAXException {
-		pageContext.include("forms-oql/testMakForm.jsp");
-	}
-	public void endMakForm(WebResponse response) throws Exception {
-		try {
-			output = response.getText(); fetchValidTestResult(output, record);
-		} catch (IOException e) {
-			fail("JSP output error: " + response.getResponseMessage());
-		}
-		
+    }
+
+    public void testMakEditForm() throws ServletException, IOException {
+        pageContext.include("forms-oql/testMakEditForm.jsp");
+    }
+
+    public void endMakEditForm(WebResponse response) throws Exception {
+        try {
+            output = response.getText();
+            fetchValidTestResult(output, record);
+        } catch (IOException e) {
+            fail("JSP output error: " + response.getResponseMessage());
+        }
         assertTrue(compareTest(output));
-		
-	}
+
+    }
+
+    public void testMakForm() throws ServletException, IOException, SAXException {
+        pageContext.include("forms-oql/testMakForm.jsp");
+    }
+
+    public void endMakForm(WebResponse response) throws Exception {
+        try {
+            output = response.getText();
+            fetchValidTestResult(output, record);
+        } catch (IOException e) {
+            fail("JSP output error: " + response.getResponseMessage());
+        }
+
+        assertTrue(compareTest(output));
+
+    }
 
     public void testBug946() throws ServletException, IOException, SAXException {
         pageContext.include("forms-oql/testBug946.jsp");
     }
+
     public void endBug946(WebResponse response) throws Exception {
         try {
-            output = response.getText(); fetchValidTestResult(output, record);
+            output = response.getText();
+            fetchValidTestResult(output, record);
         } catch (IOException e) {
             fail("JSP output error: " + response.getResponseMessage());
         }
 
         assertTrue(compareTest(output));
     }
-    
-    
+
     public void testFormMultipleForms() throws ServletException, IOException, SAXException {
         pageContext.include("forms-oql/testMakMultipleForms.jsp");
     }
+
     public void endFormMultipleForms(WebResponse response) throws Exception {
         try {
-            output = response.getText(); fetchValidTestResult(output, record);
+            output = response.getText();
+            fetchValidTestResult(output, record);
         } catch (IOException e) {
             fail("JSP output error: " + response.getResponseMessage());
         }
 
         assertTrue(compareTest(output));
     }
-    
+
     public void testFormNestedForms() throws ServletException, IOException, SAXException {
         pageContext.include("forms-oql/testMakNestedForms.jsp");
     }
+
     public void endFormNestedForms(WebResponse response) throws Exception {
         try {
-            output = response.getText(); fetchValidTestResult(output, record);
+            output = response.getText();
+            fetchValidTestResult(output, record);
         } catch (IOException e) {
             fail("JSP output error: " + response.getResponseMessage());
         }
 
         assertTrue(compareTest(output));
     }
-    
+
     public void testFormMakNewFile() throws ServletException, IOException, SAXException {
         pageContext.include("forms-oql/testMakNewFormFile.jsp");
     }
+
     public void endFormMakNewFile(WebResponse response) throws Exception {
         try {
-            output = response.getText(); fetchValidTestResult(output, record);
+            output = response.getText();
+            fetchValidTestResult(output, record);
         } catch (IOException e) {
             fail("JSP output error: " + response.getResponseMessage());
         }
 
         assertTrue(compareTest(output));
     }
-    
+
+    // public void beginMakAddToNewForm(Request request) throws MalformedURLException, IOException, SAXException {
+    // WebConversation wc = new WebConversation();
+    // WebResponse resp = wc.getResponse( System.getProperty("cactus.contextURL") +
+    // "/forms-oql/beginMakAddToNewForm.jsp" );
+    //
+    // // we get the first form in the jsp
+    // WebForm form = resp.getForms()[0];
+    // // set the inputs in the add-to-new form
+    // form.setParameter("description_1","addToNewDescription");
+    // form.setParameter("email_1","addToNew@makumba.org");
+    // // submit the form
+    // form.submit();
+    // }
+    // public void testMakAddToNewForm() throws ServletException, IOException {
+    // pageContext.include("forms-oql/testMakAddToNewForm.jsp");
+    // }
+    // public void endMakAddToNewForm(WebResponse response) throws Exception {
+    // try {
+    // output = response.getText(); fetchValidTestResult(output, record);
+    // } catch (IOException e) {
+    // fail("JSP output error: " + response.getResponseMessage());
+    // }
+    //        
+    // assertTrue(compareTest(output));
+    // }
+
+    public void beginLogin(Request request) throws MalformedURLException, IOException, SAXException {
+        WebConversation wc = new WebConversation();
+        WebResponse resp = wc.getResponse(System.getProperty("cactus.contextURL") + "/login/loginTest.jsp");
+
+        // we get the first form in the jsp
+        WebForm form = resp.getForms()[0];
+        // we try to login
+        form.setParameter("username", "manu");
+        form.setParameter("password", "secret");
+        // submit the form
+        form.submit();
+    }
+
+    public void testLogin() throws ServletException, IOException {
+        pageContext.include("login/loginTest.jsp");
+    }
+
+    public void endLogin(WebResponse response) throws Exception {
+        try {
+            output = response.getText();
+            fetchValidTestResult(output, true);
+        } catch (IOException e) {
+            fail("JSP output error: " + response.getResponseMessage());
+        }
+
+        assertTrue(compareTest(output));
+    }
+
 }
