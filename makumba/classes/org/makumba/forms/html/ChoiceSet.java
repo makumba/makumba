68795004e1734737a42364cfce3a6673c8809f3e
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2008-06-14 00:56:34 +0000

    applying code clean up, resolving generics

--- makumba/classes/org/makumba/forms/html/ChoiceSet.java
+++ makumba/classes/org/makumba/forms/html/ChoiceSet.java
@@ -4,15 +4,18 @@ import java.io.Serializable;
 
 /**
  * An ordered set of chooser choices
+ * 
  * @author Cristian Bogdan
  */
-public class ChoiceSet extends java.util.ArrayList {
+public class ChoiceSet extends java.util.ArrayList<ChoiceSet.Choice> {
 
     private static final long serialVersionUID = 1L;
 
     public static final String PARAMNAME = "org.makumba.ChoiceSet";
 
-    public class Choice implements Serializable{
+    public class Choice implements Serializable {
+        private static final long serialVersionUID = 1L;
+
         Object value;
 
         String title;
@@ -28,7 +31,7 @@ public class ChoiceSet extends java.util.ArrayList {
         public String getTitle() {
             return title;
         }
-        
+
         @Override
         public String toString() {
             return "Choice, title: '" + getTitle() + "', value: '" + getValue() + "'";
@@ -40,15 +43,17 @@ public class ChoiceSet extends java.util.ArrayList {
     public void add(Object value, String title, boolean forceSelection, boolean forceDeselection) {
         // only Pointer.Null values can be repeated, rest are ignored
         // null values are not a choice, they are simply a text between choices
-        if (value != org.makumba.Pointer.Null && value != null && m.get(value) != null)
+        if (value != org.makumba.Pointer.Null && value != null && m.get(value) != null) {
             return;
+        }
         Choice c = new Choice();
         c.value = value;
         c.title = title;
         c.forceSelection = forceSelection;
         c.forceDeselection = forceDeselection;
-        if (value != null)
+        if (value != null) {
             m.put(value, c);
+        }
         add(c);
     }
 }
