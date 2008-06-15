a9020b75ef9414b9d82659889b905b72cc6f72e6
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2008-06-15 14:43:07 +0000

    added makumba header

--- makumba/classes/test/tags/FormsOQLTest.java
+++ makumba/classes/test/tags/FormsOQLTest.java
@@ -1,7 +1,26 @@
-/*
- * Created on Apr 29, 2005
- *
- */
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
 package test.tags;
 
 import java.io.File;
@@ -152,9 +171,9 @@ public class FormsOQLTest extends MakumbaJspTestCase {
             for (int i = 0; i < namesPersonIndivName.length; i++) {
                 Vector<Dictionary<String, Object>> v = db.executeQuery(
                     "SELECT p AS p, p.indiv as i FROM test.Person p WHERE p.indiv.name=$1", namesPersonIndivName[i]);
-                if (v.size() >0) {
-                db.delete((Pointer) v.firstElement().get("p"));
-                db.delete((Pointer) v.firstElement().get("i"));
+                if (v.size() > 0) {
+                    db.delete((Pointer) v.firstElement().get("p"));
+                    db.delete((Pointer) v.firstElement().get("i"));
                 }
             }
         }
