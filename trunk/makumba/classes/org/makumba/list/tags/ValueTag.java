8de55b16515b07b214ba4b71777b3ad37e02b9d1
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2008-06-28 14:01:33 +0000

    Creating new tag makumba-0_7_ALPHA5

---
+++ trunk/makumba/classes/org/makumba/list/tags/ValueTag.java
@@ -0,0 +1,152 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package org.makumba.list.tags;
+
+import javax.servlet.jsp.JspException;
+
+import org.makumba.LogicException;
+import org.makumba.MakumbaError;
+import org.makumba.MakumbaSystem;
+import org.makumba.analyser.PageCache;
+import org.makumba.commons.MultipleKey;
+import org.makumba.devel.relations.JSPRelationMiner;
+import org.makumba.list.engine.ComposedQuery;
+import org.makumba.list.engine.valuecomputer.ValueComputer;
+
+/**
+ * mak:value tag
+ * 
+ * @author Cristian Bogdan
+ *
+ */
+public class ValueTag extends GenericListTag {
+
+    private static final long serialVersionUID = 1L;
+
+    private String expr;
+
+    private String var;
+
+    private String printVar;
+
+    public void setExpr(String expr) {
+        this.expr = expr;
+    }
+
+    public void setVar(String var) {
+        this.var = var;
+    }
+
+    public void setPrintVar(String var) {
+        this.printVar = var;
+    }
+    
+    public String getExpr() {
+        return expr;
+    }
+
+    public String getPrintVar() {
+        return printVar;
+    }
+
+    public String getVar() {
+        return var;
+    }
+
+    /**
+     * Sets tagKey to uniquely identify this tag. Called at analysis time before doStartAnalyze() and at runtime before
+     * doMakumbaStartTag()
+     * 
+     * @param pageCache
+     *            the page cache of the current page
+     */
+    public void setTagKey(PageCache pageCache) {
+        addToParentListKey(expr.trim());
+    }
+
+    /** 
+     * Determines the ValueComputer and caches it with the tagKey
+     * @param pageCache the page cache of the current page
+     */
+    public void doStartAnalyze(PageCache pageCache) {
+        pageCache.cache(GenericListTag.VALUE_COMPUTERS, tagKey, ValueComputer.getValueComputerAtAnalysis(this, QueryTag.getParentListKey(this, pageCache), expr, pageCache));
+        
+        // if we add a projection to a query, we also cache this so that we know where the projection comes from (for the relation analysis)
+        ComposedQuery query = null;
+        try {
+            query = QueryTag.getQuery(pageCache, QueryTag.getParentListKey(this, pageCache));
+        } catch(MakumbaError me) {
+            // this happens when there is no query for this mak:value
+            // we ignore it, query will stay null anyway
+        }
+        
+        if(query != null) {
+            pageCache.cache(JSPRelationMiner.PROJECTION_ORIGIN_CACHE, new MultipleKey(QueryTag.getParentListKey(this, pageCache), expr), tagKey);
+        }
+
+    }
+
+    /** 
+     * Tells the ValueComputer to finish analysis, and sets the types for var and printVar.
+     * @param pageCache the page cache of the current page
+     */
+    public void doEndAnalyze(PageCache pageCache) {
+        ValueComputer vc = (ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, tagKey);
+        vc.doEndAnalyze(pageCache);
+
+        if (var != null)
+            setType(pageCache, var, vc.getType());
+
+        if (printVar != null)
+            setType(pageCache, printVar, MakumbaSystem.makeFieldOfType(printVar, "char"));
+    }
+
+    /** 
+     * Asks the ValueComputer to present the expression
+     * @param pageCache the page cache of the current page
+     * @throws JspException
+     * @throws LogicException
+     *  */
+    public int doAnalyzedStartTag(PageCache pageCache) throws JspException,
+            org.makumba.LogicException {
+        ((ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, tagKey)).print(this, pageCache);
+
+        return EVAL_BODY_INCLUDE;
+    }
+
+    /**
+     * Computes a string
+     * @return A String holding the value in a form useful for debugging
+     */
+    public String toString() {
+        return "VALUE expr=" + expr + " parameters: " + params;
+    }
+    
+    @Override
+    protected void doAnalyzedCleanup() {
+        super.doAnalyzedCleanup();
+        expr= printVar= var= null;
+    }
+
+}
---
+++ trunk/makumba/doc/main.js
@@ -0,0 +1,78 @@
+/* $Id$ */
+
+
+//"global" variables, for the page
+var queryStr="";
+
+function makeNavBar(pathToBase) 
+{
+	var bDir=pathToBase;
+
+	document.write('<div class="navbar">');
+	//document.write(bDir);
+	
+	document.write('<a href="'+bDir+'index.html"><img src="'+bDir+'makumba-logo-small.gif" border="0"></a><br>');
+
+    document.write('<br>');
+	document.write('<a href="'+bDir+'index.html"><b>Home</b></a><br>');	
+	document.write('<br>');
+	document.write('<a href="http://blog.makumba.org"><b>Blog</b></a><br>');	
+
+	document.write('<br>');
+	document.write('<a href="'+bDir+'documentation.html"><b>Documentation</b></a><br>');
+	document.write('- <a href="'+bDir+'makumba-install.html">Quick start</a><br/>');
+	document.write('- <a href="'+bDir+'makumba-spec.html">Specification</a><br>');
+	document.write('- <a href="'+bDir+'makumba-example.html">Example</a><br>');
+	document.write('- <a href="'+bDir+'SQL-drivers.html"> Config DB</a><br>');
+	document.write('- <a href="'+bDir+'api/org/makumba/package-summary.html">API docs</a><br>');
+	document.write('- <a href="'+bDir+'faq.html">FAQ</a><br>');
+	
+	document.write('<br>');
+	document.write('<a href="'+bDir+'download.html"><b>Download</b></a><br>');
+
+	document.write('<br>');
+	document.write('<a href="http://bugzilla.makumba.org/cgi-bin/bugzilla/query.cgi?product=Makumba">Bugzilla</a><br>');
+	document.write('<a href="'+bDir+'documentation.html#devel"><b>Developer</b></a><br>');
+	document.write('- <a href="'+bDir+'hacking/index.html">documentation</a><br>');
+	document.write('- <a href="'+bDir+'issues/index.html">issues</a><br>');
+	document.write('- <a href="http://makumba.svn.sourceforge.net/">SVN repository</a><br>');
+	document.write('<br>');
+	document.write('<FORM method=GET action=http://www.google.com/custom class="search" id="searchForm">');
+	 document.write('<INPUT TYPE=text name=q size=10 maxlength=255 value="Search" onFocus="if(this.value==\'Search\') this.value=queryStr;" onBlur="queryStr=this.value; this.value=\'Search\';" title="Google Search makumba.org" class="search" id="searchBox" accesskey="s" onDblClick="location.href=\''+bDir+'search.html\'">');
+	 document.write('<INPUT type=hidden name=sa VALUE="Google Search">');
+	 document.write('<INPUT type=hidden name=cof VALUE="S:http://www.makumba.org;VLC:#044a2c;AH:center;BGC:white;LH:100;LC:#044a2c;GFNT:#999999;L:http://www.makumba.org/makumba-logo.gif;ALC:red;LW:329;T:black;GIMP:red;AWFID:ae27560a86a9a04e;">');
+	 document.write('<input type=hidden name=domains value="makumba.org">');
+	 document.write('<input type=hidden name=sitesearch value="makumba.org">');
+	document.write('</FORM>');
+        
+	document.write('</div>');
+}
+
+
+/* Takes svn "$Id$" */
+/*   or "$Header$" */
+/*   tag and breaks it down to basic parts */
+function getPageInfo(svnID)
+{
+	//trim it
+	svnID=svnID.substring(svnID.indexOf(": ")+2,svnID.lastIndexOf(" $"));
+	//split it at spaces
+	var svnIds=svnID.split(" ");
+	//take needed parts
+	var filename=svnIds[0];
+	var revision=svnIds[1];
+	var date=svnIds[2].replace("/","-").replace("/","-");
+	var time=svnIds[3];
+	var author=svnIds[4];
+
+	return(''+filename+', revision '+revision+', last modified on '+date+' at '+time+' by '+author);
+
+}
+
+function makeFooter(svnID)
+{
+	document.write('<div class="pageFooter">');
+	document.write(getPageInfo(svnID));
+	document.write('</div>');
+
+}
