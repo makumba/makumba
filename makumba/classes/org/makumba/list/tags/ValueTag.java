c354faf7aa44b229673b90e1d72c24bb0cf3d6c8
Manuel Bernhardt <manuel@bernhardt.io>
2008-07-02 22:34:07 +0000

    - adapting to also support HQL pages (and passing this to the ValueEditor servlet) - refactoring: moving all the cache keys to MakumbaJSPAnalyser, so it is possible to have an overview

--- makumba/classes/org/makumba/list/tags/ValueTag.java
+++ makumba/classes/org/makumba/list/tags/ValueTag.java
@@ -37,16 +37,15 @@ import org.makumba.MakumbaSystem;
 import org.makumba.Pointer;
 import org.makumba.ProgrammerError;
 import org.makumba.analyser.PageCache;
+import org.makumba.commons.MakumbaJspAnalyzer;
 import org.makumba.commons.MakumbaResourceServlet;
 import org.makumba.commons.MultipleKey;
-import org.makumba.devel.relations.JSPRelationMiner;
 import org.makumba.list.engine.ComposedQuery;
 import org.makumba.list.engine.valuecomputer.ValueComputer;
 
 /**
  * mak:value tag
  * 
- * FIXME "editable" for now only works for OQL, it needs to take into account HQL's ".id"
  * FIXME use formatters in order to compute the editor fields
  * TODO  implement support for in-place edition of dates and others
  * 
@@ -131,24 +130,26 @@ public class ValueTag extends GenericListTag {
      */
     public void doStartAnalyze(PageCache pageCache) {
 
-        pageCache.cache(GenericListTag.VALUE_COMPUTERS, tagKey, ValueComputer.getValueComputerAtAnalysis(this,
+        pageCache.cache(MakumbaJspAnalyzer.VALUE_COMPUTERS, tagKey, ValueComputer.getValueComputerAtAnalysis(this,
             QueryTag.getParentListKey(this, pageCache), expr, pageCache));
         
         inheritInPlaceEditAttributes();
         
         if(editPage != null && logicClass != null) {
-            // FIXME maybe we should be able to set a different editPage/logicClass in a child mak:value
+            // TODO maybe we should be able to set a different editPage/logicClass in a child mak:value
             throw new ProgrammerError("You cannot set both a 'logicClass' and a 'editPage' attribute. Make sure you do not set both of them in the same mak:value or mak:list tag (those attributes are inherited from the parent list of a mak:value).");
         }
 
         if (StringUtils.equals(editable, "true")) {
 
-            System.out.println("I am editable");
-            
             // if we want this value to be editable, we also ask for a ValueComputer of the parent object
             String parentExpr = expr.indexOf(".") > 0 ? expr.substring(0, expr.lastIndexOf(".")) : null;
 
             if (parentExpr != null) {
+                
+                if(MakumbaJspAnalyzer.isHQLPage(pageCache)) {
+                    parentExpr += ".id";
+                }
 
                 // TODO move me to setTagKey
                 MultipleKey parentObjectKey = new MultipleKey(tagKey);
@@ -156,12 +157,12 @@ public class ValueTag extends GenericListTag {
                 parentObjectKey.add(parentExpr);
 
                 // first try to retrieve the projection, see if it already exists
-                Object check = pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, parentObjectKey);
+                Object check = pageCache.retrieve(MakumbaJspAnalyzer.VALUE_COMPUTERS, parentObjectKey);
                 if (check == null) {
 
                     ValueComputer vc = ValueComputer.getValueComputerAtAnalysis(this, QueryTag.getParentListKey(this,
                         pageCache), parentExpr, pageCache);
-                    pageCache.cache(GenericListTag.VALUE_COMPUTERS, parentObjectKey, vc);
+                    pageCache.cache(MakumbaJspAnalyzer.VALUE_COMPUTERS, parentObjectKey, vc);
 
                 }
             }
@@ -177,7 +178,7 @@ public class ValueTag extends GenericListTag {
         }
 
         if (query != null) {
-            pageCache.cache(JSPRelationMiner.PROJECTION_ORIGIN_CACHE, new MultipleKey(QueryTag.getParentListKey(this,
+            pageCache.cache(MakumbaJspAnalyzer.PROJECTION_ORIGIN_CACHE, new MultipleKey(QueryTag.getParentListKey(this,
                 pageCache), expr), tagKey);
         }
 
@@ -209,17 +210,22 @@ public class ValueTag extends GenericListTag {
      *            the page cache of the current page
      */
     public void doEndAnalyze(PageCache pageCache) {
-        ValueComputer vc = (ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, tagKey);
+        ValueComputer vc = (ValueComputer) pageCache.retrieve(MakumbaJspAnalyzer.VALUE_COMPUTERS, tagKey);
         vc.doEndAnalyze(pageCache);
 
         if (StringUtils.equals(editable, "true")) {
             String parentExpr = expr.indexOf(".") > 0 ? expr.substring(0, expr.lastIndexOf(".")) : null;
-
+            
             if (parentExpr != null) {
+                
+                if(MakumbaJspAnalyzer.isHQLPage(pageCache)) {
+                    parentExpr += ".id";
+                }
+                
                 MultipleKey parentObjectKey = new MultipleKey(tagKey);
                 parentObjectKey.remove(tagKey.size() - 1);
                 parentObjectKey.add(parentExpr);
-                ((ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, parentObjectKey)).doEndAnalyze(pageCache);
+                ((ValueComputer) pageCache.retrieve(MakumbaJspAnalyzer.VALUE_COMPUTERS, parentObjectKey)).doEndAnalyze(pageCache);
             }
 
         }
@@ -257,7 +263,7 @@ public class ValueTag extends GenericListTag {
             // we first check if we have a editable type (this will need to be changed, later we will also support dates and stuff)
             // this FieldDefinition is not a MDD one, it is computed through the ComputedQuery. so we can only use it
             // for type analysis
-            FieldDefinition fd = ((ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, tagKey)).getType();
+            FieldDefinition fd = ((ValueComputer) pageCache.retrieve(MakumbaJspAnalyzer.VALUE_COMPUTERS, tagKey)).getType();
 
             if(fd.getIntegerType() == FieldDefinition._char || fd.getIntegerType() == FieldDefinition._int || fd.getIntegerType() == FieldDefinition._text) {
             
@@ -280,21 +286,26 @@ public class ValueTag extends GenericListTag {
     
                 // prepare the data needed in order to call the edit-in-place servlet
                 
-                String formattedValue = ((ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, tagKey)).getFormattedValue(
+                String formattedValue = ((ValueComputer) pageCache.retrieve(MakumbaJspAnalyzer.VALUE_COMPUTERS, tagKey)).getFormattedValue(
                     this, pageCache);
                 
                 String externalPointer = "";
                 Pointer ptr = null;
                 String fieldName = expr.substring(expr.lastIndexOf(".") + 1);
-    
+                
                 String parentExpr = expr.indexOf(".") > 0 ? expr.substring(0, expr.lastIndexOf(".")) : null;
     
                 if (parentExpr != null) {
                     // TODO refactor this, it is repeated several times
+                    
+                    if(MakumbaJspAnalyzer.isHQLPage(pageCache)) {
+                        parentExpr += ".id";
+                    }
+                    
                     MultipleKey parentObjectKey = new MultipleKey(tagKey);
                     parentObjectKey.remove(tagKey.size() - 1);
                     parentObjectKey.add(parentExpr);
-                    ptr = (Pointer) ((ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, parentObjectKey)).getValue(pageContext);
+                    ptr = (Pointer) ((ValueComputer) pageCache.retrieve(MakumbaJspAnalyzer.VALUE_COMPUTERS, parentObjectKey)).getValue(pageContext);
                     externalPointer = ptr.toExternalForm();
                 }
     
@@ -304,7 +315,7 @@ public class ValueTag extends GenericListTag {
                 sb.append("</span>");
                 sb.append("<span id='mak_onedit_" + counter + "' class='mak_onedit'>\n");
                 sb.append("       <form method='post' class='mak_edit' action='" + request.getContextPath()
-                        + "/valueEditor?logicClass=" + logicClass + "&editPage=" + editPage + "&table=" + ptr.getType() + "&field=" + fieldName + "&pointer=" + externalPointer
+                        + "/valueEditor?queryLanguage=" + MakumbaJspAnalyzer.getQueryLanguage(pageCache) + "&logicClass=" + logicClass + "&editPage=" + editPage + "&table=" + ptr.getType() + "&field=" + fieldName + "&pointer=" + externalPointer
                         + "'");
                 sb.append("onsubmit=\"return eip.AIM.submit(this, {'onComplete' :");
                 sb.append("eip.completeCallback}, '" + counter + "')\">");
@@ -376,7 +387,7 @@ public class ValueTag extends GenericListTag {
             }
 
         } else {
-            ((ValueComputer) pageCache.retrieve(GenericListTag.VALUE_COMPUTERS, tagKey)).print(this, pageCache);
+            ((ValueComputer) pageCache.retrieve(MakumbaJspAnalyzer.VALUE_COMPUTERS, tagKey)).print(this, pageCache);
         }
 
         return EVAL_BODY_INCLUDE;
