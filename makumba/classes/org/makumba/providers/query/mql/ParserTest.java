abb350c8b1892257f5a2a9b1cb48c70c90870d1e
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2008-07-29 01:15:42 +0000

    more advanced replacement of IN SET

--- makumba/classes/org/makumba/providers/query/mql/ParserTest.java
+++ makumba/classes/org/makumba/providers/query/mql/ParserTest.java
@@ -5,16 +5,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.hibernate.hql.antlr.HqlTokenTypes;
 import org.hibernate.hql.ast.HqlParser;
 import org.hibernate.hql.ast.tree.Node;
 import org.hibernate.hql.ast.util.ASTPrinter;
 import org.makumba.commons.ClassResource;
+import org.makumba.commons.RegExpUtils;
 import org.makumba.providers.QueryAnalysisProvider;
 import org.makumba.providers.query.hql.HQLQueryAnalysisProvider;
 
 import antlr.collections.AST;
+
 //import antlr.debug.misc.ASTFrame;
 
 public class ParserTest {
@@ -23,7 +27,7 @@ public class ParserTest {
 
     public static void main(String[] argv) {
         qap = new HQLQueryAnalysisProvider();
-        ASTPrinter printer = new ASTPrinter( HqlTokenTypes.class );
+        ASTPrinter printer = new ASTPrinter(HqlTokenTypes.class);
         PrintWriter pw = new PrintWriter(System.out);
         int line = 1;
         try {
@@ -34,13 +38,13 @@ public class ParserTest {
                 query = preProcess(query);
                 AST a = analyseQuery(line, query);
                 transformOQL(a);
-                
+
                 if (line == 295) {
 
-                    //ASTFrame frame = new ASTFrame("normal",a);
-                    //frame.setVisible(true);
+                    // ASTFrame frame = new ASTFrame("normal",a);
+                    // frame.setVisible(true);
 
-                    printer.showAst(a,pw);                    
+                    printer.showAst(a, pw);
                 }
                 line++;
             }
@@ -52,26 +56,26 @@ public class ParserTest {
 
     }
 
-    private static void transformOQL(AST a){
-        if(a==null)
+    private static void transformOQL(AST a) {
+        if (a == null)
             return;
-        if(a.getType()==HqlTokenTypes.IDENT && a.getText().startsWith("$")){
+        if (a.getType() == HqlTokenTypes.IDENT && a.getText().startsWith("$")) {
             a.setType(HqlTokenTypes.COLON);
-            AST para= new Node();
+            AST para = new Node();
             para.setType(HqlTokenTypes.IDENT);
-            para.setText("makumbaParam"+a.getText().substring(1));
+            para.setText("makumbaParam" + a.getText().substring(1));
             a.setFirstChild(para);
             a.setText(":");
-        }else{
-            if(a.getType()==HqlTokenTypes.EQ || a.getType()==HqlTokenTypes.NE){
-                if(isNil(a.getFirstChild())){
+        } else {
+            if (a.getType() == HqlTokenTypes.EQ || a.getType() == HqlTokenTypes.NE) {
+                if (isNil(a.getFirstChild())) {
                     setNullTest(a);
                     a.setFirstChild(a.getFirstChild().getNextSibling());
-                }else if(isNil(a.getFirstChild().getNextSibling())){
+                } else if (isNil(a.getFirstChild().getNextSibling())) {
                     setNullTest(a);
                     a.getFirstChild().setNextSibling(null);
                 }
-                
+
             }
         }
         transformOQL(a.getFirstChild());
@@ -79,17 +83,17 @@ public class ParserTest {
     }
 
     private static void setNullTest(AST a) {
-        if(a.getType()==HqlTokenTypes.EQ){
+        if (a.getType() == HqlTokenTypes.EQ) {
             a.setType(HqlTokenTypes.IS_NULL);
             a.setText("is null");
-        }else{
+        } else {
             a.setType(HqlTokenTypes.IS_NOT_NULL);
             a.setText("is not null");
-        }        
+        }
     }
 
     private static boolean isNil(AST a) {
-        return a.getType()== HqlTokenTypes.IDENT && a.getText().equals("NIL");
+        return a.getType() == HqlTokenTypes.IDENT && a.getText().equals("NIL");
     }
 
     private static AST analyseQuery(int line, String query) {
@@ -105,12 +109,28 @@ public class ParserTest {
         }
     }
 
+    public static final String regExpInSET = "in" + RegExpUtils.minOneWhitespace + "set" + RegExpUtils.whitespace
+            + "\\(";
+
+    public static final Pattern patternInSet = Pattern.compile(regExpInSET);
+
     public static String preProcess(String query) {
         // replace -> (subset separators) with __
         query = query.replaceAll("->", "__");
+
         // replace IN SET with IN.
-        // FIXME: too simplistic approach
+        Matcher m = patternInSet.matcher(query.toLowerCase()); // find all occurrences of lower-case "in set"
+        while (m.find()) {
+            int start = m.start();
+            int beginSet = m.group().indexOf("set"); // find location of "set" keyword
+            // System.out.println(query);
+            // composing query by concatenating the part before "set", 3 space and the part after "set"
+            query = query.substring(0, start + beginSet) + "   " + query.substring(start + beginSet + 3);
+            // System.out.println(query);
+            // System.out.println();
+        }
         query = query.replaceAll("IN SET", "IN    ");
         return query;
     }
+
 }
