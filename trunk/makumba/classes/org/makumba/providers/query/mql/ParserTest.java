d605987db4ec0ab111c32ec256c1cc92e4283da3
cristian_bogdan <cristian_bogdan@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-07-29 00:01:42 +0000

    transforming the oql tree into a proper HQL one

--- trunk/makumba/classes/org/makumba/providers/query/mql/ParserTest.java
+++ trunk/makumba/classes/org/makumba/providers/query/mql/ParserTest.java
@@ -4,14 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
+import org.hibernate.hql.antlr.HqlTokenTypes;
 import org.hibernate.hql.ast.HqlParser;
+import org.hibernate.hql.ast.tree.Node;
+import org.hibernate.hql.ast.util.ASTPrinter;
 import org.makumba.commons.ClassResource;
 import org.makumba.providers.QueryAnalysisProvider;
 import org.makumba.providers.query.hql.HQLQueryAnalysisProvider;
 
 import antlr.collections.AST;
-import antlr.debug.misc.ASTFrame;
+//import antlr.debug.misc.ASTFrame;
 
 public class ParserTest {
 
@@ -19,6 +23,8 @@ public class ParserTest {
 
     public static void main(String[] argv) {
         qap = new HQLQueryAnalysisProvider();
+        ASTPrinter printer = new ASTPrinter( HqlTokenTypes.class );
+        PrintWriter pw = new PrintWriter(System.out);
         int line = 1;
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader((InputStream) ClassResource.get(
@@ -27,9 +33,14 @@ public class ParserTest {
             while ((query = rd.readLine()) != null) {
                 query = preProcess(query);
                 AST a = analyseQuery(line, query);
-                if (line == 1075) {
-                    ASTFrame frame = new ASTFrame("normal", a);
-                    frame.setVisible(true);
+                transformOQL(a);
+                
+                if (line == 295) {
+
+                    //ASTFrame frame = new ASTFrame("normal",a);
+                    //frame.setVisible(true);
+
+                    printer.showAst(a,pw);                    
                 }
                 line++;
             }
@@ -39,6 +50,46 @@ public class ParserTest {
         }
         System.out.println("analyzed " + line + " queries");
 
+    }
+
+    private static void transformOQL(AST a){
+        if(a==null)
+            return;
+        if(a.getType()==HqlTokenTypes.IDENT && a.getText().startsWith("$")){
+            a.setType(HqlTokenTypes.COLON);
+            AST para= new Node();
+            para.setType(HqlTokenTypes.IDENT);
+            para.setText("makumbaParam"+a.getText().substring(1));
+            a.setFirstChild(para);
+            a.setText(":");
+        }else{
+            if(a.getType()==HqlTokenTypes.EQ || a.getType()==HqlTokenTypes.NE){
+                if(isNil(a.getFirstChild())){
+                    setNullTest(a);
+                    a.setFirstChild(a.getFirstChild().getNextSibling());
+                }else if(isNil(a.getFirstChild().getNextSibling())){
+                    setNullTest(a);
+                    a.getFirstChild().setNextSibling(null);
+                }
+                
+            }
+        }
+        transformOQL(a.getFirstChild());
+        transformOQL(a.getNextSibling());
+    }
+
+    private static void setNullTest(AST a) {
+        if(a.getType()==HqlTokenTypes.EQ){
+            a.setType(HqlTokenTypes.IS_NULL);
+            a.setText("is null");
+        }else{
+            a.setType(HqlTokenTypes.IS_NOT_NULL);
+            a.setText("is not null");
+        }        
+    }
+
+    private static boolean isNil(AST a) {
+        return a.getType()== HqlTokenTypes.IDENT && a.getText().equals("NIL");
     }
 
     private static AST analyseQuery(int line, String query) {
