f4341895366b487e023466d6cd3eb86e87ae4c56
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2008-08-06 08:12:43 +0000

    writing in a NameResolver.TextList rather than a StringBuffer

--- makumba/classes/org/makumba/providers/query/mql/MqlIdentNode.java
+++ makumba/classes/org/makumba/providers/query/mql/MqlIdentNode.java
@@ -1,9 +1,16 @@
 package org.makumba.providers.query.mql;
 
 import org.makumba.DataDefinition;
+import org.makumba.commons.NameResolver.TextList;
 
 import antlr.SemanticException;
 
+/**
+ * This is a label used in the SELECT or WHERE of a query TODO issue a warning, and advice to select label.id
+ * 
+ * @author Cristian Bogdan
+ * @version $Id: MqlIdentNode.java,v 1.1 Aug 5, 2008 5:35:48 PM cristi Exp $
+ */
 public class MqlIdentNode extends MqlNode {
     public String label;
 
@@ -11,31 +18,30 @@ public class MqlIdentNode extends MqlNode {
     }
 
     public void resolve() throws SemanticException {
-        // this is a label used in the query
-        // TODO issue a warning, and advice to select label.id
-        if(walker.error!=null)
+        if (walker.error != null)
             return;
-        if(label!=null)
+        if (label != null)
             // we've analyzed already
             return;
         label = getText();
         DataDefinition dd = walker.currentContext.findLabelType(label);
-        if(dd==null){
-            throw new SemanticException("Unknown label: "+label);
+        if (dd == null) {
+            throw new SemanticException("Unknown label: " + label);
         }
-        
-        String field=null;
+
+        String field = null;
         if (dd.getParentField() != null) {
             String stp = dd.getParentField().getType();
-            if (stp.equals("setintEnum") || stp.equals("setcharEnum")){
+            if (stp.equals("setintEnum") || stp.equals("setcharEnum")) {
                 field = "enum";
                 setMakType(dd.getFieldDefinition(dd.getSetMemberFieldName()));
             }
         }
-        if (field == null){
-            field =dd.getIndexPointerFieldName();
-            setMakType(walker.currentContext.ddp.makeFieldDefinition("x", "ptr "+dd.getName()));
+        if (field == null) {
+            field = dd.getIndexPointerFieldName();
+            setMakType(walker.currentContext.ddp.makeFieldDefinition("x", "ptr " + dd.getName()));
         }
-        setText(label + "." + walker.currentContext.getFieldName(label, field, walker.nr));
+        text = new TextList();
+        text.append(label).append(".").append(dd, field);
     }
 }
