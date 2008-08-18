8f56dc7d72cb6060e7728da14fd34b5c8bfb58dd
cristian_bogdan <cristian_bogdan@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-08-18 21:40:07 +0000

    simpler and better parse error handling

--- trunk/makumba/classes/org/makumba/providers/query/mql/MqlQueryAnalysisProvider.java
+++ trunk/makumba/classes/org/makumba/providers/query/mql/MqlQueryAnalysisProvider.java
@@ -1,6 +1,5 @@
 package org.makumba.providers.query.mql;
 
-
 import org.makumba.DataDefinition;
 import org.makumba.FieldDefinition;
 import org.makumba.OQLParseError;
@@ -14,34 +13,22 @@ public class MqlQueryAnalysisProvider extends QueryAnalysisProvider {
     public static int parsedQueries = NamedResources.makeStaticCache("MQL parsed queries", new NamedResourceFactory() {
 
         private static final long serialVersionUID = 1L;
-    
+
         protected Object makeResource(Object nm, Object hashName) throws Exception {
-            return new MqlQueryAnalysis((String)nm, true);
+            return new MqlQueryAnalysis((String) nm, true);
         }
     }, true);
 
     @Override
     public QueryAnalysis getRawQueryAnalysis(String query) {
-        
-        try {
-            return (QueryAnalysis) NamedResources.getStaticCache(parsedQueries).getResource(query);
-        } catch (RuntimeWrappedException e) {
-            if (e.getCause() instanceof antlr.ANTLRException) {
-                Exception f = (antlr.ANTLRException) e.getCause();
-                String s = f.getMessage();
-                if (s.startsWith("line"))
-                    s = s.substring(s.indexOf(':') + 1);
-                throw new OQLParseError("\r\nin query:\r\n" + query, f);
-            }
-            throw e;
-        }
+        return (QueryAnalysis) NamedResources.getStaticCache(parsedQueries).getResource(query);
     }
-
 
     @Override
     public boolean selectGroupOrOrderAsLabels() {
         return true;
     }
+
     @Override
     public FieldDefinition getAlternativeField(DataDefinition dd, String fn) {
         if (fn.equals("id"))
@@ -53,7 +40,7 @@ public class MqlQueryAnalysisProvider extends QueryAnalysisProvider {
     public String getPrimaryKeyNotation(String label) {
         return label;
     }
-    
+
     @Override
     public String getParameterSyntax() {
         return "$";
