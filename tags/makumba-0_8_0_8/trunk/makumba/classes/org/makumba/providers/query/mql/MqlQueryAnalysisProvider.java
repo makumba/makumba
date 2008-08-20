ad1c19e9eb1d45c07ddb4c85a1104d3d7321b155
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-08-20 21:56:17 +0000

    Creating new tag makumba-0_8_0_8

---
+++ tags/makumba-0_8_0_8/trunk/makumba/classes/org/makumba/providers/query/mql/MqlQueryAnalysisProvider.java
@@ -0,0 +1,49 @@
+package org.makumba.providers.query.mql;
+
+import org.makumba.DataDefinition;
+import org.makumba.FieldDefinition;
+import org.makumba.OQLParseError;
+import org.makumba.commons.NamedResourceFactory;
+import org.makumba.commons.NamedResources;
+import org.makumba.commons.RuntimeWrappedException;
+import org.makumba.providers.QueryAnalysis;
+import org.makumba.providers.QueryAnalysisProvider;
+
+public class MqlQueryAnalysisProvider extends QueryAnalysisProvider {
+    public static int parsedQueries = NamedResources.makeStaticCache("MQL parsed queries", new NamedResourceFactory() {
+
+        private static final long serialVersionUID = 1L;
+
+        protected Object makeResource(Object nm, Object hashName) throws Exception {
+            return new MqlQueryAnalysis((String) nm, true);
+        }
+    }, true);
+
+    @Override
+    public QueryAnalysis getRawQueryAnalysis(String query) {
+        return (QueryAnalysis) NamedResources.getStaticCache(parsedQueries).getResource(query);
+    }
+
+    @Override
+    public boolean selectGroupOrOrderAsLabels() {
+        return true;
+    }
+
+    @Override
+    public FieldDefinition getAlternativeField(DataDefinition dd, String fn) {
+        if (fn.equals("id"))
+            return dd.getFieldDefinition(dd.getIndexPointerFieldName());
+        return null;
+    }
+
+    @Override
+    public String getPrimaryKeyNotation(String label) {
+        return label;
+    }
+
+    @Override
+    public String getParameterSyntax() {
+        return "$";
+    }
+
+}
