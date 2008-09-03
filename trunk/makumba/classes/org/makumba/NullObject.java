2c33d59f7c2de11b12c8c8fc3e302d5d6e575922
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-09-03 18:43:35 +0000

    making serialisable to store in form responders

--- trunk/makumba/classes/org/makumba/NullObject.java
+++ trunk/makumba/classes/org/makumba/NullObject.java
@@ -1,6 +1,10 @@
 package org.makumba;
 
-public class NullObject {
+import java.io.Serializable;
+
+public class NullObject implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     String s;
 
     NullObject(String s) {
