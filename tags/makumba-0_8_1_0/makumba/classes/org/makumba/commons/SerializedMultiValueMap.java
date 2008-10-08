41d6ba50351d3c1c0d643416ae8d80f581dbc029
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-10-08 23:11:02 +0000

    Creating new tag makumba-0_8_1_0

---
+++ tags/makumba-0_8_1_0/makumba/classes/org/makumba/commons/SerializedMultiValueMap.java
@@ -0,0 +1,17 @@
+package org.makumba.commons;
+
+import java.io.Serializable;
+
+import org.apache.commons.collections.map.MultiValueMap;
+
+/**
+ * This class provides a serialized version of {@link MultiValueMap}.
+ * 
+ * @author Rudolf Mayer
+ * @version $Id: SerializedMultiValueMap.java,v 1.1 Sep 7, 2007 11:22:10 PM rudi Exp $
+ */
+public class SerializedMultiValueMap extends MultiValueMap implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+}
