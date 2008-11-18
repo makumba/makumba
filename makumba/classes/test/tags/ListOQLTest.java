13feb349a7fbe07518649c702e66fd910629e366
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2008-11-18 02:36:43 +0000

    adding one more test

--- makumba/classes/test/tags/ListOQLTest.java
+++ makumba/classes/test/tags/ListOQLTest.java
@@ -292,4 +292,17 @@ public class ListOQLTest extends MakumbaJspTestCase {
         assertTrue(compareTest(output));
     }
 
+    public void testMDDFunctions2() throws ServletException, IOException {
+        pageContext.include("list-oql/testMDDFunctions2.jsp");
+    }
+
+    public void endMDDFunctions2(WebResponse response) throws Exception {
+        try {
+            output = response.getText(); fetchValidTestResult(output, record);
+        } catch (IOException e) {
+            fail("JSP output error: " + response.getResponseMessage());
+        }
+        assertTrue(compareTest(output));
+    }
+
 }
