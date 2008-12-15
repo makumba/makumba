396967f3cd0d30fa885ccb3af8e5df65a60160cd
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-12-15 23:09:10 +0000

    fixing warning

--- trunk/makumba/classes/org/makumba/providers/query/mql/MqlBinaryOperator.java
+++ trunk/makumba/classes/org/makumba/providers/query/mql/MqlBinaryOperator.java
@@ -2,23 +2,27 @@ package org.makumba.providers.query.mql;
 
 import antlr.SemanticException;
 
-/** Various binary operations. We need to treat them separately to compute parameter types and to rewrite some constants  
+/**
+ * Various binary operations. We need to treat them separately to compute parameter types and to rewrite some constants
+ * 
  * @author Cristian Bogdan
  * @version $Id: MqlBinaryOperator.java,v 1.1 Aug 5, 2008 5:36:29 PM cristi Exp $
  */
-public class MqlBinaryOperator extends MqlNode{    
+public class MqlBinaryOperator extends MqlNode {
+    private static final long serialVersionUID = 1L;
+
     @Override
     protected void oneMoreChild(MqlNode node) {
-        if(this.getNumberOfChildren()!=2)
+        if (this.getNumberOfChildren() != 2)
             // this is the first child, we're not yet ready for analysis
             return;
-        analyze((MqlNode)getFirstChild(), node);
+        analyze((MqlNode) getFirstChild(), node);
     }
+
     private void analyze(MqlNode left, MqlNode right) {
         if (walker.error != null)
             return;
-        try 
-        {
+        try {
             checkForOperandType(left);
             checkForOperandType(right);
             analyzeOperands(left, right);
