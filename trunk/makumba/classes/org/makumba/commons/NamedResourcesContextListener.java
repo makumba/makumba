ca071eedb55185fab6e9f194c8b4928ef902a979
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2008-12-15 23:50:09 +0000

    avoiding useless string concatenation

--- trunk/makumba/classes/org/makumba/commons/NamedResourcesContextListener.java
+++ trunk/makumba/classes/org/makumba/commons/NamedResourcesContextListener.java
@@ -34,14 +34,14 @@ import javax.servlet.ServletContextEvent;
 public class NamedResourcesContextListener implements javax.servlet.ServletContextListener {
     public NamedResourcesContextListener() {
         // this also ensures that the loadTime of MakumbaSystem is set
-        java.util.logging.Logger.getLogger("org.makumba." + "system").info("loading makumba context listener");
+        java.util.logging.Logger.getLogger("org.makumba.system").info("loading makumba context listener");
     }
 
     public void contextInitialized(ServletContextEvent sce) {
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
-        java.util.logging.Logger.getLogger("org.makumba." + "system").info("destroying makumba caches");
+        java.util.logging.Logger.getLogger("org.makumba.system").info("destroying makumba caches");
         NamedResources.cleanup();
         JDBCUnload();
     }
