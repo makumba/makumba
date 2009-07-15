1e9fca0941b1c70753d66cbd268eab25bf30585b
Manuel Bernhardt <manuel@bernhardt.io>
2009-07-15 13:24:40 +0000

    replacing unit test MDDs with new MDDs, creating unit tests to work with the old mdd parser

--- makumba/classes/org/makumba/providers/datadefinition/mdd/MDDParserTest.java
+++ makumba/classes/org/makumba/providers/datadefinition/mdd/MDDParserTest.java
@@ -1,11 +1,18 @@
 package org.makumba.providers.datadefinition.mdd;
 
+import org.makumba.Transaction;
+import org.makumba.providers.TransactionProvider;
+
 public class MDDParserTest {
 
     public static void main(String[] args) {
 
         DataDefinitionImpl dd = (DataDefinitionImpl) MDDProvider.getMDD("ParserTest2");
-
+        
+        Transaction t = TransactionProvider.getInstance().getConnectionTo(TransactionProvider.getInstance().getDefaultDataSourceName());
+        t.executeQuery("select s.name from ParserTest2 t, t.test1 s", null);
+        t.executeQuery("select s.name from ParserTest2 t, t.test2 s", null);
+        
     }
    
     
