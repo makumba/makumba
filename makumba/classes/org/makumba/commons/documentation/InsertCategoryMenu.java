526d77254ebec20f18389996d9ccaa5c3f15858e
Manuel Bernhardt <manuel@bernhardt.io>
2009-11-28 13:48:10 +0000

    new plugin for dynamic top & left menu switching

---
+++ makumba/classes/org/makumba/commons/documentation/InsertCategoryMenu.java
@@ -0,0 +1,81 @@
+package org.makumba.commons.documentation;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import com.ecyrd.jspwiki.ReferenceManager;
+import com.ecyrd.jspwiki.WikiContext;
+import com.ecyrd.jspwiki.WikiPage;
+import com.ecyrd.jspwiki.plugin.AbstractReferralPlugin;
+import com.ecyrd.jspwiki.plugin.PluginException;
+import com.ecyrd.jspwiki.plugin.WikiPlugin;
+
+/**
+ * Plugin that will insert the appropriate menu for the page according to its category<br>
+ * TODO caching!!
+ * 
+ * @author Manuel Gay
+ * @version $Id: InsertCategoryMenu.java,v 1.1 Nov 27, 2009 9:07:54 AM manu Exp $
+ */
+public class InsertCategoryMenu extends AbstractReferralPlugin implements WikiPlugin {
+
+    private final static String PARAM_PAGE = "page";
+
+    private final static String[] DEFAULT_CATEGORIES = new String[] { "CategoryMain", "CategoryQuickStart", "CategoryConfiguration", "CategoryUsage",
+            "CategoryDocumentation", "CategoryShowcase", "CategoryDownload" };
+
+    public String execute(WikiContext context, Map params) throws PluginException {
+
+        // fetch categories from jspwiki config
+        String[] categories = null;
+        String categoriesConf = (String) (String) context.getEngine().getWikiProperties().get("navigation.categories");
+        if (categoriesConf == null) {
+            categories = DEFAULT_CATEGORIES;
+        } else {
+            categories = categoriesConf.split(",");
+        }
+
+        ReferenceManager refmgr = context.getEngine().getReferenceManager();
+        String pageName = (String) params.get(PARAM_PAGE);
+
+        if (pageName == null) {
+            pageName = context.getPage().getName();
+        }
+
+        WikiPage page = context.getEngine().getPage(pageName);
+        if(page == null) {
+            return "Page " + pageName + " does not exist yet so no menu for it can be computed.";
+        }
+        
+        String toBeInserted = null;
+        
+        // if we are a category ourselves, just display the menu
+        if(Arrays.asList(categories).contains(pageName)) {
+            toBeInserted = page.getName();
+        } else {
+            
+            // we lookup in which category this page is
+            for (String cat : categories) {
+
+                // all the pages that refer to the category
+                Collection<String> links = refmgr.findReferrers(cat);
+                if(links == null) {
+                    continue;
+                } else {
+                    if (links.contains(pageName)) {
+                        toBeInserted = cat;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        if(toBeInserted == null) {
+            return "Page " + page.getName() + " is in none of the categories " + Arrays.toString(categories) + " hence its menu cannot be found.";
+        }
+        
+        String wikiMarkup = "[{MenuTreePlugin menuPage='" + toBeInserted + "Menu" + "'}]";
+        return context.getEngine().textToHTML(context, wikiMarkup);
+    }
+}
