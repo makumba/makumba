fdae997f7048009084ada2257896793b11960e2a
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2009-12-09 21:41:43 +0000

    formatting and source code cleanup

--- makumba/classes/org/makumba/providers/datadefinition/mdd/ValidationType.java
+++ makumba/classes/org/makumba/providers/datadefinition/mdd/ValidationType.java
@@ -2,55 +2,57 @@ package org.makumba.providers.datadefinition.mdd;
 
 /**
  * Validation rule types
+ * 
  * @author Manuel Gay
  * @version $Id: ValidationType.java,v 1.1 May 21, 2009 2:27:41 PM manu Exp $
  */
 public enum ValidationType {
 
-    COMPARISON("comparison rule"),
-    UNIQUENESS("uniqueness rule", FieldType.CHAR, FieldType.BOOLEAN, FieldType.DATE, FieldType.INT, FieldType.PTR, FieldType.TEXT),
-    RANGE("range rule", FieldType.INT, FieldType.REAL),
-    LENGTH("length rule", FieldType.CHAR, FieldType.TEXT),
-    REGEX("regular expression rule", FieldType.CHAR, FieldType.TEXT);
-    
-    
-    
+    COMPARISON("comparison rule"), //
+    UNIQUENESS("uniqueness rule", FieldType.CHAR, FieldType.BOOLEAN, FieldType.DATE, FieldType.INT, FieldType.PTR,
+            FieldType.TEXT), //
+    RANGE("range rule", FieldType.INT, FieldType.REAL), //
+    LENGTH("length rule", FieldType.CHAR, FieldType.TEXT), //
+    REGEX("regular expression rule", FieldType.CHAR, FieldType.TEXT); //
+
     ValidationType(String description, FieldType... fieldType) {
         this.description = description;
         this.fieldTypes = fieldType;
     }
-    
+
     private FieldType[] fieldTypes;
-    
+
     private String description;
-    
+
     public boolean checkApplicability(FieldType fieldType) {
-        for(FieldType t : fieldTypes) {
-            if(t.equals(fieldType)) {
+        for (FieldType t : fieldTypes) {
+            if (t.equals(fieldType)) {
                 return true;
             }
         }
         return false;
     }
-    
+
     public String getDescription() {
         return this.description;
     }
-    
+
     public String getApplicableTypes() {
         String s = "";
-        
-        for(int i=0; i < fieldTypes.length; i++) {
-            
-            if(i + 1 == fieldTypes.length && fieldTypes.length != 1)
-                s +=" and ";
-            
+
+        for (int i = 0; i < fieldTypes.length; i++) {
+
+            if (i + 1 == fieldTypes.length && fieldTypes.length != 1) {
+                s += " and ";
+            }
+
             s += fieldTypes[i].getTypeName();
 
-            if(i + 1 < fieldTypes.length && i + 1 != fieldTypes.length - 1)
-                s+=", ";
+            if (i + 1 < fieldTypes.length && i + 1 != fieldTypes.length - 1) {
+                s += ", ";
+            }
         }
         return s;
     }
-    
+
 }
