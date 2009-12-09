df60636cf1caded2950eaac6417e4d0d85301cfd
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2009-12-09 21:38:40 +0000

    formatting and source code cleanup

--- trunk/makumba/classes/org/makumba/providers/datadefinition/mdd/validation/RangeValidationRule.java
+++ trunk/makumba/classes/org/makumba/providers/datadefinition/mdd/validation/RangeValidationRule.java
@@ -11,23 +11,23 @@ import org.makumba.providers.datadefinition.mdd.ValidationType;
 import antlr.collections.AST;
 
 public class RangeValidationRule extends ValidationRuleNode {
-    
+
     private static final long serialVersionUID = 3906776093230956372L;
 
     private Number lowerLimit;
-    
+
     private Number upperLimit;
-    
+
     public RangeValidationRule(MDDNode mdd, AST originAST, FieldNode field, ValidationType type) {
         super(mdd, originAST, field);
         this.type = type;
     }
-    
+
     @Override
     public boolean validate(Object value, Transaction t) throws InvalidValueException {
-        
-        if(lowerLimit == null || upperLimit == null) {
-            
+
+        if (lowerLimit == null || upperLimit == null) {
+
             if (lowerBound.equals("?")) {
                 lowerLimit = new Double(Double.MIN_VALUE); // FIXME: use the min value makumba can handle
             } else {
@@ -38,33 +38,33 @@ public class RangeValidationRule extends ValidationRuleNode {
             } else {
                 upperLimit = Double.valueOf(upperBound);
             }
-            
+
         }
-        
-        switch(type) {
+
+        switch (type) {
             case LENGTH:
                 if (!(value instanceof Number)) {
                     return false;// TODO: think of throwing some "cannot validate exception"
                 }
-                if ((lowerLimit.doubleValue() <= ((Number) value).doubleValue() && ((Number) value).doubleValue() <= upperLimit.doubleValue())) {
+                if (lowerLimit.doubleValue() <= ((Number) value).doubleValue() && ((Number) value).doubleValue() <= upperLimit.doubleValue()) {
                     return true;
                 } else {
                     throwException();
                     return false;
                 }
- 
-                
+
             case RANGE:
                 if (!(value instanceof String || value instanceof Text)) {
                     return false;// TODO: think of throwing some "cannot validate exception"
                 }
-                
+
                 String s;
-                if(value instanceof Text)
-                // FIXME: we actually only need the length of the Text, not the getString()
-                    s=((Text)value).getString();
-                else
-                    s= (String)value;
+                if (value instanceof Text) {
+                    // FIXME: we actually only need the length of the Text, not the getString()
+                    s = ((Text) value).getString();
+                } else {
+                    s = (String) value;
+                }
                 if (lowerLimit.intValue() <= s.length() && s.length() <= upperLimit.intValue()) {
                     return true;
                 } else {
@@ -74,12 +74,13 @@ public class RangeValidationRule extends ValidationRuleNode {
             default:
                 throw new RuntimeException("should not be here");
         }
-        
+
     }
-    
+
     @Override
     public String getRuleName() {
-        return type.name().toLowerCase() + "(" + field.getName() + ") {" + lowerBound + ".." +  upperBound+ "} : " + message  + " (line " + getLine() + ")";
+        return type.name().toLowerCase() + "(" + field.getName() + ") {" + lowerBound + ".." + upperBound + "} : "
+                + message + " (line " + getLine() + ")";
     }
 
 }
