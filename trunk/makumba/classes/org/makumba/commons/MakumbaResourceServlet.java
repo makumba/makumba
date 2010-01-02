142d86e388320c72f8b211b8604d3a6bf0962134
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-01-02 22:38:43 +0000

    removing methods not needed anymore after bug 1147 is solved

--- trunk/makumba/classes/org/makumba/commons/MakumbaResourceServlet.java
+++ trunk/makumba/classes/org/makumba/commons/MakumbaResourceServlet.java
@@ -263,55 +263,6 @@ public class MakumbaResourceServlet extends HttpServlet {
         }
     }
 
-    private void printLineBreaks(String s, OutputStream o) throws IOException {
-        byte[] bytes = s.getBytes();
-        for (int i = 0; i < bytes.length; i++) {
-            if (bytes[i] != '\n') {
-                o.write(bytes[i]);
-            } else {
-                o.write('\n');
-                o.write(bytes[i]);
-            }
-        }
-    }
-
-    public static void writeResources(StringBuffer sb, String contextPath, Iterable<Object> resources) {
-        for (Object object : resources) {
-            String o = (String) object;
-
-            String alternateLocation = Configuration.getResourceLocation(o);
-            // TODO: check if the alternate location is part of the external resources
-
-            if (o.endsWith(".js")) {
-                writeScriptsInHeader(sb, contextPath, o, alternateLocation);
-            } else if (o.endsWith(".css")) {
-                writeStylesInHeader(sb, contextPath, o, alternateLocation);
-            }
-
-        }
-    }
-
-    public static void writeStylesInHeader(StringBuffer sb, String contextPath, String styleSheet,
-            String alternateLocation) {
-        String filerefName = "fileref" + styleSheet.replace(".", "").replace("-", "_");
-        sb.append("<script type=\"text/javascript\">\n");
-        sb.append("var " + filerefName + " = document.createElement('link');\n");
-        sb.append(filerefName + ".setAttribute(\"media\",\"all\");\n");
-        sb.append(filerefName + ".setAttribute(\"type\",\"text/css\");\n");
-        String location = "";
-        if (alternateLocation != Configuration.PROPERTY_NOT_SET) {
-            location = contextPath + "/" + alternateLocation;
-        } else {
-            location = contextPath + Configuration.getMakumbaResourcesLocation() + "/" + RESOURCE_PATH_CSS + styleSheet;
-        }
-        sb.append(filerefName + ".setAttribute(\"href\", '" + location + "');\n");
-        sb.append(filerefName + ".setAttribute(\"rel\",\"StyleSheet\");\n");
-        sb.append("if (typeof " + filerefName + " != \"undefined\")\n");
-        sb.append("document.getElementsByTagName(\"head\")[0].appendChild(" + filerefName + ");\n");
-        sb.append("</script>\n");
-
-    }
-
     public static String getContentType(URL url) {
         for (int i = 0; i < imageContentTypes.length; i++) {
             if (url.getFile().endsWith("." + imageContentTypes[i])) {
@@ -339,28 +290,6 @@ public class MakumbaResourceServlet extends HttpServlet {
     private static boolean isBinary(URL url) {
         // TODO: this should be capable of detecting other types. A solution would be to check for "not text type"
         return isImageType(url);
-    }
-
-    public static void writeScriptsInHeader(StringBuffer sb, String contextPath, String script, String alternateLocation) {
-        // we write the scripts in the header using JS and DOM rewriting
-        String filerefName = "fileref" + script.replace(".", "").replace("-", "_");
-        sb.append("<script type=\"text/javascript\">\n");
-        sb.append("var " + filerefName + " = document.createElement('script');\n");
-        sb.append(filerefName + ".setAttribute(\"type\",\"text/javascript\");\n");
-        String location = "";
-        if (alternateLocation != Configuration.PROPERTY_NOT_SET) {
-            location = contextPath + "/" + alternateLocation;
-        } else {
-            location = contextPath + Configuration.getMakumbaResourcesLocation() + "/" + RESOURCE_PATH_JAVASCRIPT
-                    + script;
-        }
-        sb.append(filerefName + ".setAttribute(\"href\", '" + location + "');\n");
-
-        sb.append(filerefName + ".setAttribute(\"src\", '" + contextPath + Configuration.getMakumbaResourcesLocation()
-                + "/" + RESOURCE_PATH_JAVASCRIPT + script + "');\n");
-        sb.append("document.getElementsByTagName(\"head\")[0].appendChild(" + filerefName + ");\n");
-        sb.append("</script>\n");
-
     }
 
     public static int makumbaResources = NamedResources.makeStaticCache("Makumba resources",
