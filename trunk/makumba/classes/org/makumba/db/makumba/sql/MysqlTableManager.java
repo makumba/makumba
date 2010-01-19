e3ffb6f396cee774db45661a589f8cb6164a7805
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-01-19 16:38:45 +0000

    formatting

--- trunk/makumba/classes/org/makumba/db/makumba/sql/MysqlTableManager.java
+++ trunk/makumba/classes/org/makumba/db/makumba/sql/MysqlTableManager.java
@@ -22,6 +22,7 @@
 /////////////////////////////////////
 
 package org.makumba.db.makumba.sql;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -29,71 +30,71 @@ import java.sql.Statement;
 import org.makumba.FieldDefinition;
 import org.makumba.db.makumba.sql.SQLDBConnection;
 
-public class MysqlTableManager extends org.makumba.db.makumba.sql.TableManager
-{
-  @Override
-protected String createDbSpecific(String command){return command+" type=InnoDB"; }
+public class MysqlTableManager extends org.makumba.db.makumba.sql.TableManager {
+    @Override
+    protected String createDbSpecific(String command) {
+        return command + " type=InnoDB";
+    }
 
-  /** checks if an alteration is needed, and calls doAlter if so */
-  @Override
-protected void alter(org.makumba.db.makumba.sql.SQLDBConnection dbc, CheckingStrategy cs, boolean alter) throws SQLException
-  {
-    Statement st= dbc.createStatement();
-    ResultSet rs=st.executeQuery("SHOW CREATE TABLE "+getDBName());
-    rs.next();
-    String def=rs.getString(2).trim();
-    if(def.lastIndexOf(')') > def.lastIndexOf(" TYPE=InnoDB") &&
-			def.lastIndexOf(')') > def.lastIndexOf(" ENGINE=InnoDB")){
-      String s="ALTER TABLE "+getDBName()+" TYPE=InnoDB";
-      if(alter) {
-          java.util.logging.Logger.getLogger("org.makumba.db.init.tablechecking").info(getSQLDatabase().getName()+": "+s);
-          st.executeUpdate(s);
-      } else {
-          java.util.logging.Logger.getLogger("org.makumba.db.init.tablechecking").warning("should alter: " + s);
-      }
+    /** checks if an alteration is needed, and calls doAlter if so */
+    @Override
+    protected void alter(org.makumba.db.makumba.sql.SQLDBConnection dbc, CheckingStrategy cs, boolean alter)
+            throws SQLException {
+        Statement st = dbc.createStatement();
+        ResultSet rs = st.executeQuery("SHOW CREATE TABLE " + getDBName());
+        rs.next();
+        String def = rs.getString(2).trim();
+        if (def.lastIndexOf(')') > def.lastIndexOf(" TYPE=InnoDB")
+                && def.lastIndexOf(')') > def.lastIndexOf(" ENGINE=InnoDB")) {
+            String s = "ALTER TABLE " + getDBName() + " TYPE=InnoDB";
+            if (alter) {
+                java.util.logging.Logger.getLogger("org.makumba.db.init.tablechecking").info(
+                    getSQLDatabase().getName() + ": " + s);
+                st.executeUpdate(s);
+            } else {
+                java.util.logging.Logger.getLogger("org.makumba.db.init.tablechecking").warning("should alter: " + s);
+            }
 
-      }
-    rs.close();
-    st.close();
-    super.alter(dbc, cs, alter);
-  }
+        }
+        rs.close();
+        st.close();
+        super.alter(dbc, cs, alter);
+    }
 
-  /** mysql needs to have it adjustable, depending on version (see bug 512) */
-  @Override
-protected String getTableMissingStateName(SQLDBConnection dbc)
-  {
-   try{
-     //version:"3.0.5-gamma" has major:3, minor:0
-     String version=dbc.getMetaData().getDriverVersion();
-     int major=dbc.getMetaData().getDriverMajorVersion();
-     int minor=dbc.getMetaData().getDriverMinorVersion();
-     String mark=""+major+"."+minor+".";
-     String minorStr=version.substring(version.indexOf(mark)+mark.length());
-     if(minorStr.indexOf('-') == -1)
-     	minorStr=minorStr.substring(0,minorStr.indexOf(' '));
-     else
-       minorStr=minorStr.substring(0,minorStr.indexOf('-'));
-     int minor2=Integer.parseInt(minorStr);
+    /** mysql needs to have it adjustable, depending on version (see bug 512) */
+    @Override
+    protected String getTableMissingStateName(SQLDBConnection dbc) {
+        try {
+            // version:"3.0.5-gamma" has major:3, minor:0
+            String version = dbc.getMetaData().getDriverVersion();
+            int major = dbc.getMetaData().getDriverMajorVersion();
+            int minor = dbc.getMetaData().getDriverMinorVersion();
+            String mark = "" + major + "." + minor + ".";
+            String minorStr = version.substring(version.indexOf(mark) + mark.length());
+            if (minorStr.indexOf('-') == -1)
+                minorStr = minorStr.substring(0, minorStr.indexOf(' '));
+            else
+                minorStr = minorStr.substring(0, minorStr.indexOf('-'));
+            int minor2 = Integer.parseInt(minorStr);
 
-     if(major>3 || major==3 && minor>0 || major==3 && minor==0 && minor2>=8)
-       return "tableMissing";
-     else
-       return "tableMissing-before308";
-   }catch(Exception e) {
-       //e.printStackTrace();
-	return "tableMissing";
-   }
-  }
-  
-  @Override
-protected int getSQLType(String fieldName) {
-        switch (getFieldDefinition(fieldName).getIntegerType()) {
-        case FieldDefinition._text:
-            return -1;
-        default:
-            return super.getSQLType(fieldName);
+            if (major > 3 || major == 3 && minor > 0 || major == 3 && minor == 0 && minor2 >= 8)
+                return "tableMissing";
+            else
+                return "tableMissing-before308";
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return "tableMissing";
         }
     }
 
+    @Override
+    protected int getSQLType(String fieldName) {
+        switch (getFieldDefinition(fieldName).getIntegerType()) {
+            case FieldDefinition._text:
+                return -1;
+            default:
+                return super.getSQLType(fieldName);
+        }
+    }
 
 }
