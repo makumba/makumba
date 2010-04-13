9f5f31c7173fd29077613f5a3c6fe0daf5828557
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2010-04-13 23:48:48 +0000

    Programmer error with a reason

--- makumba/classes/org/makumba/ProgrammerError.java
+++ makumba/classes/org/makumba/ProgrammerError.java
@@ -24,12 +24,17 @@
 package org.makumba;
 
 /** this error is thrown when a handler method is missing but it is needed by some controller */
-public class ProgrammerError extends MakumbaError
-{
-  /**
+public class ProgrammerError extends MakumbaError {
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-public ProgrammerError(String expl){ super(expl); }
+    public ProgrammerError(String expl) {
+        super(expl);
+    }
+    public ProgrammerError(Throwable cause, String expl) {
+        super(cause, expl);
+    }
+    
 }
