82700c4324ccdd69913a7f956390c50b88bb4c47
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2010-04-27 11:25:35 +0000

    formatting code

--- makumba/classes/org/makumba/UnauthenticatedException.java
+++ makumba/classes/org/makumba/UnauthenticatedException.java
@@ -23,23 +23,36 @@
 
 package org.makumba;
 
-/** An unauthenticated exception, typically due to a missing attribute */
-public class UnauthenticatedException extends UnauthorizedException
-{
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-String attName;
-  
-  public UnauthenticatedException(){super("");}
-  public UnauthenticatedException(String message){super(message);}
-  
-  public String getAttributeName() { return attName; }
-  
-  /** set the missing attribute name */
-  public void setAttributeName(String s) { this.attName=s; }
-  
-  public String getMessage() { return super.getMessage(); }
+/**
+ * An unauthenticated exception, typically due to a missing attribute
+ * 
+ * @author Cristian Bogdan
+ * @version $Id$
+ */
+public class UnauthenticatedException extends UnauthorizedException {
+    private static final long serialVersionUID = 1L;
+
+    String attName;
+
+    public UnauthenticatedException() {
+        super("");
+    }
+
+    public UnauthenticatedException(String message) {
+        super(message);
+    }
+
+    public String getAttributeName() {
+        return attName;
+    }
+
+    /** set the missing attribute name */
+    public void setAttributeName(String s) {
+        this.attName = s;
+    }
+
+    public String getMessage() {
+        return super.getMessage();
+    }
 
 }
