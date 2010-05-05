ee74bb7164a1dd6c920f0b01142fcd5fce367c9e
cristian_bogdan <cristian_bogdan@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-05-05 08:06:54 +0000

    added a custom JSP factory which works in JSP API < 2

--- trunk/makumba/classes/org/makumba/controller/http/ControllerFilter.java
+++ trunk/makumba/classes/org/makumba/controller/http/ControllerFilter.java
@@ -26,11 +26,15 @@ package org.makumba.controller.http;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspEngineInfo;
+import javax.servlet.jsp.JspFactory;
+import javax.servlet.jsp.PageContext;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
@@ -83,8 +87,39 @@ public class ControllerFilter implements Filter {
         }
     }
 
+    static final JspFactory fact= JspFactory.getDefaultFactory();
+    static JspFactory makFact= new JspFactory(){
+        
+        @Override
+        public JspEngineInfo getEngineInfo() {
+            return fact.getEngineInfo();
+        }
+
+        @Override
+        public PageContext getPageContext(Servlet servlet, ServletRequest request, ServletResponse response,
+                String errorPageURL, boolean needsSession, int buffer, boolean autoflush) {
+            //System.out.println(servlet);
+            return fact.getPageContext(servlet, request, response, errorPageURL, needsSession, buffer, autoflush);
+            // here we can hang the pageContext in a threadLocal stack
+            // and also trigger page analysis
+            // this also tells us when a page starts or is included
+
+        }
+
+        @Override
+        public void releasePageContext(PageContext pc) {
+            fact.releasePageContext(pc);
+            // this tells us when a page finishes and if it was included, we will go back to the including page 
+            // here we can pop the pageContext from the thread local stack
+            // and activate the runtime state of the makumba tags from the including page
+            
+        }
+    };
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException,
             java.io.IOException {
+        
+        JspFactory.setDefaultFactory(makFact);
+
         // replace the response by the MakumbaResponseWrapper, which modifies the output to inject links to CSS/JS files
         // TODO: consider using a ControllerHandler; then, however, we can only work with ServletObjects in all methods
         resp = new MakumbaResponseWrapper((HttpServletResponse) resp, (HttpServletRequest) req);
