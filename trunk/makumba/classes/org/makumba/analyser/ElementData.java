24b08be390ffa4392f5ee378d5d82c7899294a2b
rosso_nero <rosso_nero@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-05-07 13:49:03 +0000

    formatted

--- trunk/makumba/classes/org/makumba/analyser/ElementData.java
+++ trunk/makumba/classes/org/makumba/analyser/ElementData.java
@@ -2,13 +2,21 @@ package org.makumba.analyser;
 
 import org.makumba.analyser.engine.SourceSyntaxPoints;
 
+/**
+ * @version $Id: ElementData.java,v 1.1 7 May 2010 15:48:42 rudi Exp $
+ */
 public class ElementData {
 
     private static final long serialVersionUID = 1L;
+
     protected int startLine;
+
     protected int startColumn;
+
     protected int endLine;
+
     protected int endColumn;
+
     protected SourceSyntaxPoints sourceSyntaxPoints;
 
     public int getStartLine() {
