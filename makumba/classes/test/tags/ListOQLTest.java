4029553ae85007db9fc0edbd62dcd513c3c68b3b
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2010-05-10 13:23:20 +0000

    one more test for mak:value() function, combining it in the same page with a mak:value tag (on the same expression)

--- makumba/classes/test/tags/ListOQLTest.java
+++ makumba/classes/test/tags/ListOQLTest.java
@@ -185,6 +185,14 @@ public class ListOQLTest extends MakumbaJspTestCase {
         compareToFileWithTestName(response);
     }
 
+    public void testMakValueFunctionAndTag() throws Exception {
+        includeJspWithTestName();
+    }
+
+    public void endMakValueFunctionAndTag(WebResponse response) throws Exception {
+        compareToFileWithTestName(response);
+    }
+
     /*
      * Commented out by manu on 22-05-2009, since those tests fail with the current comparison method public void
      * testMakValueTS_create() throws ServletException, IOException { // FIXME: this test will fail // a line-by-line
