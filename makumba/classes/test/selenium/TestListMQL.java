3bf3b06c4ca44b7a8d8b981dbaf563a97a97ca1d
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2010-05-11 18:25:06 +0000

    simplified

--- makumba/classes/test/selenium/TestListMQL.java
+++ makumba/classes/test/selenium/TestListMQL.java
@@ -1,58 +1,25 @@
 package test.selenium;
 
-import com.meterware.httpunit.GetMethodWebRequest;
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebRequest;
-import com.thoughtworks.selenium.*;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
 
 import javax.servlet.ServletException;
-
-import org.apache.cactus.Request;
-import org.xml.sax.SAXException;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import test.MakumbaTestSetup;
 
-public class TestListMQL extends SeleneseTestCase {
+import com.thoughtworks.selenium.SeleneseTestCase;
 
-    static Suite setup;
-    
-    private static final class Suite extends MakumbaTestSetup {
-        private Suite(Test arg0) {
-            super(arg0, "oql");
-        }
-    }
+public class TestListMQL extends SeleneseTestCase {
     
     public static Test suite() {
-        setup = new Suite(new TestSuite(TestListMQL.class));
-        return setup;
+        return new MakumbaTestSetup(new TestSuite(TestListMQL.class), "oql");
     }
     
     public void setUp() throws Exception {
         setUp("http://localhost:8080/", "*firefox");
     }
-
-    public void beginTomcat(Request request) {
-        WebConversation wc = new WebConversation();
-        WebRequest req = new GetMethodWebRequest(System.getProperty("cactus.contextURL"));
-        try {
-            wc.getResponse(req);
-        } catch (MalformedURLException e) {
-        } catch (IOException e) {
-            setup.tearDown();
-            System.err.println("\n\n\n\n\nYou should run tomcat first! Use mak-tomcat to do that.\n\n");
-            System.exit(1);
-        } catch (SAXException e) {
-        }
-    }
     
-    public void testTomcat() {
-    }
-
     public void testMakObjectTag() throws ServletException, IOException {
         selenium.open("/tests/list-oql/testMakObjectTag.jsp");
         verifyTrue(selenium.isTextPresent("name:john"));
