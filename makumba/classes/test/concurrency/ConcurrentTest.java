9e76d094f7525cc4c8f0fba966575d85a13cbcd7
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2010-05-14 00:11:09 +0000

    makumba web test setup formatted by save action

--- makumba/classes/test/concurrency/ConcurrentTest.java
+++ makumba/classes/test/concurrency/ConcurrentTest.java
@@ -14,27 +14,28 @@ import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 
 import test.MakumbaTestSetup;
+import test.MakumbaWebTestSetup;
 
 public class ConcurrentTest extends TestCase {
 
     private static MakumbaTestSetup setup;
-    
+
     private static final int THREADS = 200;
-    
+
     private static final String CONCURRENT_TEST_URL = "http://localhost:8080/tests/concurrent/concurrentListTest.jsp";
-    
+
     private static final String EXPECTED_TEST_RESULT_FRAGMENT = "speaks: English English French French German German Italian Italian Spanish Spanish";
-    
+
     public static Test suite() {
-        return setup = new MakumbaTestSetup(new TestSuite(ConcurrentTest.class), "oql");
+        return setup = new MakumbaWebTestSetup(new TestSuite(ConcurrentTest.class), "oql");
     }
-    
+
     public void testListConcurrent() throws Exception {
-        
+
         checkListPageAvailable();
-        
+
         final CountDownLatch startGate = new CountDownLatch(1);
-        final CountDownLatch endGate = new CountDownLatch(THREADS);         
+        final CountDownLatch endGate = new CountDownLatch(THREADS);
 
         for (int i = 0; i < THREADS; i++) {
             final int index = i;
@@ -43,16 +44,17 @@ public class ConcurrentTest extends TestCase {
                 public void run() {
                     try {
                         // each thread waits for the latch to be released before firing
-                        startGate.await();                      
+                        startGate.await();
                         try {
                             long start = System.currentTimeMillis();
                             HttpClient httpClient = new HttpClient();
                             GetMethod getTestPage = new GetMethod(CONCURRENT_TEST_URL);
                             httpClient.executeMethod(getTestPage);
                             String result = getTestPage.getResponseBodyAsString();
-                            //System.out.println(result);
+                            // System.out.println(result);
                             assertTrue(result.indexOf(EXPECTED_TEST_RESULT_FRAGMENT) > -1);
-                            System.out.println("LIST TIME for:"+index +"="+(System.currentTimeMillis()- start)+" ms");
+                            System.out.println("LIST TIME for:" + index + "=" + (System.currentTimeMillis() - start)
+                                    + " ms");
                         } finally {
                             // count down the end gate, when no threads are left the endGate will fire
                             endGate.countDown();
@@ -64,14 +66,14 @@ public class ConcurrentTest extends TestCase {
             };
             thread.start();
         }
-        
+
         long start = System.currentTimeMillis();
         // fire all threads
         startGate.countDown();
-        
+
         // the main thread waits for all threads to have ran before continuing
         endGate.await();
-        System.out.println("TOTAL TIME:"+(System.currentTimeMillis()- start)+" ms");
+        System.out.println("TOTAL TIME:" + (System.currentTimeMillis() - start) + " ms");
     }
 
     private void checkListPageAvailable() throws IOException, HttpException {
@@ -80,12 +82,12 @@ public class ConcurrentTest extends TestCase {
         int status = -1;
         try {
             status = httpClient.executeMethod(getTestPage);
-        } catch(ConnectException c) {
+        } catch (ConnectException c) {
             setup.tearDown();
             System.err.println("\n\n\n\n\nYou should run tomcat first! Use tomcat-mak to do that.\n\n");
             System.exit(1);
         }
         assertEquals("Test page did not run correctly", HttpStatus.SC_OK, status);
     }
-    
+
 }
