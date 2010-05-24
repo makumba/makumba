695957e0be72ace9e8070972af2d5c6b6065d578
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-05-24 16:32:29 +0000

    Restoring history

---
+++ trunk/makumba/test/org/makumba/test/AllTests.java
@@ -0,0 +1,68 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
+package test;
+
+import junit.framework.Test;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import test.component.ConcurrentTest;
+import test.component.ConfigurationTest;
+import test.component.MddTest;
+import test.component.TableHibernateTest;
+import test.component.TableTest;
+import test.tags.FormsHQLTest;
+import test.tags.FormsOQLTest;
+import test.tags.ListHQLTest;
+import test.tags.ListOQLTest;
+
+/**
+ * TestSuite that runs all the Makumba tests
+ * 
+ * @author Stefan Baebler
+ * @author Manuel Gay
+ */
+public class AllTests {
+
+    public static void main(String[] args) {
+        System.out.println("Makumba test suite: Running all tests...");
+        TestResult tr = junit.textui.TestRunner.run(suite());
+        if (!tr.wasSuccessful()) {
+            System.exit(1); // report an error
+        }
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("All JUnit Tests for Makumba");
+        suite.addTest(ConfigurationTest.suite());
+        suite.addTest(MddTest.suite());
+        suite.addTest(TableTest.suite());
+        suite.addTest(TableHibernateTest.suite());
+        suite.addTest(ListOQLTest.suite());
+        suite.addTest(FormsOQLTest.suite());
+        suite.addTest(ListHQLTest.suite());
+        suite.addTest(FormsHQLTest.suite());
+        suite.addTest(ConcurrentTest.suite());
+        return suite;
+    }
+}
