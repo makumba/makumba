32bf35de18ba118e9aa8480b3d231c8af0c57dde
Manuel Bernhardt <manuel@bernhardt.io>
2010-06-29 18:42:17 +0000

    multi-unique constraint

--- technology-playground/src/org/makumba/j2ee/model/Person.java
+++ technology-playground/src/org/makumba/j2ee/model/Person.java
@@ -5,8 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Max;
 
 @Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames={"name", "surname"})})
 public class Person {
 	
 	@Id
