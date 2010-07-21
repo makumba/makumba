b3def86bb7c82f499916bf307d6a32f386252af5
cristian_bogdan <cristian_bogdan@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-07-21 23:05:00 +0000

    adding the component via the API rather than via faces-config

--- trunk/makumba-jsf/src/org/makumba/jsf/ListTagHandler.java
+++ trunk/makumba-jsf/src/org/makumba/jsf/ListTagHandler.java
@@ -1,22 +1,25 @@
 package org.makumba.jsf;
 
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
 import javax.faces.view.facelets.ComponentConfig;
 import javax.faces.view.facelets.ComponentHandler;
 import javax.faces.view.facelets.TagHandler;
 
 /**
- * {@link TagHandler} for mak:list, may be needed later on for doing more advanced stuff
+ * {@link TagHandler} for mak:list, may be needed later on for doing more
+ * advanced stuff
+ * 
  * @author manu
- *
+ * 
  */
 public class ListTagHandler extends ComponentHandler {
 
     public ListTagHandler(ComponentConfig config) {
         super(config);
+        FacesContext context = FacesContext.getCurrentInstance();
+        Application app = context.getApplication();
+        app.addComponent("makumbaList", "org.makumba.jsf.ListComponent");
     }
-    
-   
-    
 
 }
-
