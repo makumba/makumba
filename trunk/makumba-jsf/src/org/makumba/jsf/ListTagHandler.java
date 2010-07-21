1dd1200647a42bf04c1bf7db9c3410989c90430c
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-07-21 17:20:57 +0000

    initial revision. simple scaffold with taglib and list tag

---
+++ trunk/makumba-jsf/src/org/makumba/jsf/ListTagHandler.java
@@ -0,0 +1,22 @@
+package org.makumba.jsf;
+
+import javax.faces.view.facelets.ComponentConfig;
+import javax.faces.view.facelets.ComponentHandler;
+import javax.faces.view.facelets.TagHandler;
+
+/**
+ * {@link TagHandler} for mak:list, may be needed later on for doing more advanced stuff
+ * @author manu
+ *
+ */
+public class ListTagHandler extends ComponentHandler {
+
+    public ListTagHandler(ComponentConfig config) {
+        super(config);
+    }
+    
+   
+    
+
+}
+
