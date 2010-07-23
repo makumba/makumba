cb895ec83ffa3d2494546604036da16e9cef73ae
Manuel Bernhardt <manuel@bernhardt.io>
2010-07-23 17:51:43 +0000

    Formatting

--- makumba-jsf/src/org/makumba/jsf/ListPhaseListener.java
+++ makumba-jsf/src/org/makumba/jsf/ListPhaseListener.java
@@ -1,9 +1,3 @@
-/*
- * Created on Jul 23, 2010
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.makumba.jsf;
 
 import javax.faces.event.PhaseEvent;
@@ -11,13 +5,9 @@ import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 
 /**
- * A phase listener example. It needs a component listener (see
- * {@link AnalysisListener}) to actually know when analysis can start, but it
- * may be useful for other purposes.
- * 
- * 
- * TODO: Currently this is not in use as it needs a faces-config declaration
- * which we can do without by installing the AnalysisListener some other way.
+ * A phase listener example. It needs a component listener (see {@link AnalysisListener}) to actually know when analysis
+ * can start, but it may be useful for other purposes. TODO: Currently this is not in use as it needs a faces-config
+ * declaration which we can do without by installing the AnalysisListener some other way.
  * 
  * @author cristi
  */
@@ -36,10 +26,11 @@ public class ListPhaseListener implements PhaseListener {
     public void afterPhase(PhaseEvent event) {
         // System.out.println("after " + event.getPhaseId());
 
-        if (event.getPhaseId().equals(PhaseId.RESTORE_VIEW))
+        if (event.getPhaseId().equals(PhaseId.RESTORE_VIEW)) {
             // at this point (and also at the start of RENDER_RESPONSE) the view
             // exists but it has no children
             // therefore we need a listener that tells us when the view is ready
             AnalysisListener.install(event.getFacesContext());
+        }
     }
 }
