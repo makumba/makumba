6faf1d7f265e3f8fe866823b2a50906042269551
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-07-24 09:30:16 +0000

    name

--- trunk/makumba-jsf-test/src/org/makumba/test/jsf/HelloWorld.java
+++ trunk/makumba-jsf-test/src/org/makumba/test/jsf/HelloWorld.java
@@ -1,11 +1,17 @@
 package org.makumba.test.jsf;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
 
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class HelloWorld {
+	
+	public String getName() {
+		return "John";
+	}
 
     public Vector<String> getList() {
         Vector<String> v = new Vector<String>();
@@ -14,5 +20,16 @@ public class HelloWorld {
         v.add("kiwi");
         return v;
     }
-
+    
+    public Map<String, Object> getMaps() {
+    	Map<String, Object> m = new HashMap<String, Object>();
+    	m.put("key1", "level1val1");
+    	m.put("key2", "level1val2");
+    	Map<String, Object> m1 = new HashMap<String, Object>();
+    	m1.put("key11", "level2val1");
+    	m1.put("key22", "level2val2");
+    	m.put("key3", m1);
+    	return m;
+    }
+    
 }
