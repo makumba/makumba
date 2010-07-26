496b5806c8f6d26436733e5940c38fd5719d922d
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2010-07-26 23:19:03 +0000

    mak:expr() support, uniform treatment of unknown expressins in cached environments

--- makumba-jsf/src/org/makumba/el/MakumbaFunctions.java
+++ makumba-jsf/src/org/makumba/el/MakumbaFunctions.java
@@ -1,28 +1,16 @@
 package org.makumba.el;
 
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
+import org.makumba.jsf.UIRepeatListComponent;
 
 /**
  * makumba EL functions
  * 
  * @author manu
+ * @author cristi
  */
 public class MakumbaFunctions {
 
-    public static String expr(String expr) {
-        FacesContext ctx = FacesContext.getCurrentInstance();
-        // System.out.println("MakumbaFunctions.expr() " + expr);
-        // System.out.println("MakumbaFunctions.expr() FacesContext renderResponse " + ctx.getRenderResponse());
-        // System.out.println("MakumbaFunctions.expr() FacesContext responseComplete " + ctx.getResponseComplete());
-        // System.out.println("MakumbaFunctions.expr() FacesContext currentPhaseId " + ctx.getCurrentPhaseId());
-
-        // from ze book, page 98
-        UIComponent component = ctx.getApplication().evaluateExpressionGet(ctx, "#{component}", UIComponent.class);
-
-        // ELContext elCtx = ctx.getELContext();
-        // Object component = elCtx.getELResolver().getValue(elCtx, "component", "class");
-
-        return "mak:expr(" + expr + ") nested in component " + component;
+    public static Object expr(String expr) {
+        return UIRepeatListComponent.getCurrentlyRunning().getExpressionValue(expr);
     }
 }
