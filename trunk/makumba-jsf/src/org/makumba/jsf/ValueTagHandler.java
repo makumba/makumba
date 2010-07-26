1f7c7a2c62b4ff4bf16725fdd9d3059c2ac375fa
cristian_bogdan <cristian_bogdan@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-07-26 14:04:52 +0000

    more registrations moved to the app listener

--- trunk/makumba-jsf/src/org/makumba/jsf/ValueTagHandler.java
+++ trunk/makumba-jsf/src/org/makumba/jsf/ValueTagHandler.java
@@ -1,7 +1,5 @@
 package org.makumba.jsf;
 
-import javax.faces.application.Application;
-import javax.faces.context.FacesContext;
 import javax.faces.view.facelets.ComponentConfig;
 import javax.faces.view.facelets.ComponentHandler;
 import javax.faces.view.facelets.TagHandler;
@@ -15,10 +13,6 @@ public class ValueTagHandler extends ComponentHandler {
 
     public ValueTagHandler(ComponentConfig config) {
         super(config);
-
-        FacesContext context = FacesContext.getCurrentInstance();
-        Application app = context.getApplication();
-        app.addComponent("makumbaValue", "org.makumba.jsf.ValueComponent");
     }
 
 }
