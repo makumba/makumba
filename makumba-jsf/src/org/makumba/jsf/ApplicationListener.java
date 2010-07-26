f457d0bf5afa41e453eafa468cb213d13813e834
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2010-07-26 09:39:55 +0000

    all initialization is in the ApplicationListener now

--- makumba-jsf/src/org/makumba/jsf/ApplicationListener.java
+++ makumba-jsf/src/org/makumba/jsf/ApplicationListener.java
@@ -1,10 +1,16 @@
 package org.makumba.jsf;
 
+import java.util.Iterator;
+
+import javax.faces.FactoryFinder;
 import javax.faces.application.Application;
 import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PhaseListener;
 import javax.faces.event.PostConstructApplicationEvent;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
+import javax.faces.lifecycle.Lifecycle;
+import javax.faces.lifecycle.LifecycleFactory;
 
 import org.makumba.el.MakumbaELResolver;
 
@@ -26,6 +32,17 @@ public class ApplicationListener implements SystemEventListener {
         if (event.getClass().equals(PostConstructApplicationEvent.class)) {
             Application app = (Application) event.getSource();
             app.addELResolver(new MakumbaELResolver());
+
+            app.addComponent("makumbaList", "org.makumba.jsf.UIRepeatListComponent");
+
+            LifecycleFactory f = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+            for (Iterator<String> i = f.getLifecycleIds(); i.hasNext();) {
+                Lifecycle l = f.getLifecycle(i.next());
+
+                PhaseListener listener = new ListPhaseListener();
+                l.removePhaseListener(listener);
+                l.addPhaseListener(listener);
+            }
         }
 
     }
