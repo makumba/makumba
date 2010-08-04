d70c4fc93e3dec512611fcb5b9bdad47a45ff1a0
cristian_bogdan <cristian_bogdan@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-08-04 21:14:43 +0000

    proof that ui:repeat cannot be rendered by f:ajax render=

--- trunk/makumba-jsf-test/src/org/makumba/test/jsf/ListBean.java
+++ trunk/makumba-jsf-test/src/org/makumba/test/jsf/ListBean.java
@@ -7,29 +7,31 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class ListBean {
-	
-	public List<Object> getList() {
-		List<Object> v = new ArrayList<Object>();
-		
-		List<Object> apples = new ArrayList<Object>();
-		apples.add("a-1-red apple");
-		apples.add("a-2-orange apple");
-		apples.add("a-3-yellow apple");
-		v.add(apples);
-		
-		List<Object> bananas = new ArrayList<Object>();
-		//bananas.add("b-1-red banana");
-		//bananas.add("b-2-orange banana");
-		//bananas.add("b-3-yellow banana");
-		v.add(bananas);
-		
-		List<Object> oranges = new ArrayList<Object>();
-		oranges.add("o-1-red orange");
-		oranges.add("o-2-orange orange");
-		oranges.add("o-3-yellow orange");
-		v.add(oranges);
-		
-		return v;
-	}
+    static int n = 0;
 
+    public List<Object> getList() {
+
+        List<Object> v = new ArrayList<Object>();
+        v.add(n++);
+        v.add(n++);
+        v.add(n++);
+        return v;
+
+        /*
+         * List<Object> apples = new ArrayList<Object>();
+         * apples.add("a-1-red apple"); apples.add("a-2-orange apple");
+         * apples.add("a-3-yellow apple"); v.add(apples);
+         * 
+         * List<Object> bananas = new ArrayList<Object>();
+         * //bananas.add("b-1-red banana"); //bananas.add("b-2-orange banana");
+         * //bananas.add("b-3-yellow banana"); v.add(bananas);
+         * 
+         * List<Object> oranges = new ArrayList<Object>();
+         * oranges.add("o-1-red orange"); oranges.add("o-2-orange orange");
+         * oranges.add("o-3-yellow orange"); v.add(oranges);
+         * 
+         * return v;
+         */
+
+    }
 }
