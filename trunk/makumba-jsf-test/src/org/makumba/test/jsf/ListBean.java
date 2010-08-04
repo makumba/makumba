cd80d48d8c9b70e24b9f9421d508368406c82eef
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-08-04 18:08:46 +0000

    More test pages, for nested list and nested ui repeat

---
+++ trunk/makumba-jsf-test/src/org/makumba/test/jsf/ListBean.java
@@ -0,0 +1,35 @@
+package org.makumba.test.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ListBean {
+	
+	public List<Object> getList() {
+		List<Object> v = new ArrayList<Object>();
+		
+		List<Object> apples = new ArrayList<Object>();
+		apples.add("a-1-red apple");
+		apples.add("a-2-orange apple");
+		apples.add("a-3-yellow apple");
+		v.add(apples);
+		
+		List<Object> bananas = new ArrayList<Object>();
+		//bananas.add("b-1-red banana");
+		//bananas.add("b-2-orange banana");
+		//bananas.add("b-3-yellow banana");
+		v.add(bananas);
+		
+		List<Object> oranges = new ArrayList<Object>();
+		oranges.add("o-1-red orange");
+		oranges.add("o-2-orange orange");
+		oranges.add("o-3-yellow orange");
+		v.add(oranges);
+		
+		return v;
+	}
+
+}
