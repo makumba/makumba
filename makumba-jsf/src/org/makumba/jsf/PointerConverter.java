8161773353b4edae246daf097ece0a2afe4f5f28
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2010-08-06 14:30:58 +0000

    worked around mojarra bug with adding converters dynamically

--- makumba-jsf/src/org/makumba/jsf/PointerConverter.java
+++ makumba-jsf/src/org/makumba/jsf/PointerConverter.java
@@ -13,12 +13,9 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 
-import org.makumba.DataDefinition;
 import org.makumba.Pointer;
 import org.makumba.commons.RuntimeWrappedException;
-import org.makumba.providers.QueryAnalysis;
-import org.makumba.providers.QueryAnalysisProvider;
-import org.makumba.providers.QueryProvider;
+import org.makumba.el.MakumbaELResolver;
 
 public class PointerConverter implements Converter, Serializable {
     static final Logger log = java.util.logging.Logger.getLogger("org.makumba.jsf.ptrConvert");
@@ -26,29 +23,15 @@ public class PointerConverter implements Converter, Serializable {
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
         // log.fine("Resolving  " + value);
-        // FIXME: this looks pretty laborious. Probably the query should prepare
         try {
-
-            UIRepeatListComponent list = UIRepeatListComponent.findMakListParent(component, true);
-            QueryAnalysisProvider qap = QueryProvider.getQueryAnalzyer(list.getQueryLanguage());
-            QueryAnalysis qa = qap.getQueryAnalysis(list.getComposedQuery().getTypeAnalyzerQuery());
 
             // FIXME: note that there can be more pointers in the expression, though that is unlikely
             String expr = component.getValueExpression("value").getExpressionString();
             // take away #{ }
             expr = expr.substring(2, expr.length() - 1).trim();
-            DataDefinition dd = qa.getProjectionType();
-            DataDefinition pointed = null;
-            for (int i = 0; i < list.getProjections().size(); i++) {
-                if (list.getProjections().get(i).equals(expr)) {
-                    pointed = dd.getFieldDefinition(i).getPointedType();
-                    break;
-                }
-            }
 
-            // JSF seems to require a SQLPointer... Maybe because the old value is of that class
-            Pointer ptr = new org.makumba.commons.SQLPointer(pointed.getName(),
-                    new Pointer(pointed.getName(), value).getId());
+            UIRepeatListComponent list = UIRepeatListComponent.findMakListParent(component, true);
+            Pointer ptr = MakumbaELResolver.resolvePointer(value, expr, list);
             System.out.println(UIRepeatListComponent.getCurrentlyRunning().debugIdent() + " " + value + " is "
                     + ptr.toString());
             return ptr;
