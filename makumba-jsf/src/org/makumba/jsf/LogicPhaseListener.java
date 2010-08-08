f922d34d22f3593c80012fb3fb8e5b13c796faae
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2010-08-08 16:14:25 +0000

    FacesAttributes, the JSF-specific implementation of Attributes. $parameters work,  BL discovery and authentication/authorization should work

---
+++ makumba-jsf/src/org/makumba/jsf/LogicPhaseListener.java
@@ -0,0 +1,42 @@
+package org.makumba.jsf;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.servlet.http.HttpServletRequest;
+
+import org.makumba.LogicException;
+import org.makumba.commons.RuntimeWrappedException;
+import org.makumba.commons.attributes.RequestAttributes;
+import org.makumba.experminetal.AnalysisListener;
+
+/**
+ * Detect when to apply Logic and authentication/authorization.
+ * 
+ * @author cristi
+ */
+public class LogicPhaseListener implements PhaseListener {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        System.out.println("before " + event.getPhaseId());
+        if (event.getPhaseId() == PhaseId.RESTORE_VIEW) {
+            try {
+                RequestAttributes.getAttributes((HttpServletRequest) event.getFacesContext().getExternalContext().getRequest());
+            } catch (LogicException e) {
+                throw new RuntimeWrappedException(e);
+            }
+        }
+    }
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+    }
+}
