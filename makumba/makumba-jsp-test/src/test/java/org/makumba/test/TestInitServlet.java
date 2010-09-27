8dd58ce4471de1a979ac12b57b604d229a189480
Manuel Bernhardt <manuel@bernhardt.io>
2010-09-27 11:41:10 +0000

    Mavenization: makumba-jsp-test module: moving files to right location, updating files

--- makumba/makumba-jsp-test/src/test/java/org/makumba/test/TestInitServlet.java
+++ makumba/makumba-jsp-test/src/test/java/org/makumba/test/TestInitServlet.java
@@ -1,15 +1,4 @@
-package org.makumba.test.util;
-
-import java.io.IOException;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Vector;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+package org.makumba.test;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.makumba.DataDefinition;
@@ -18,8 +7,19 @@ import org.makumba.Pointer;
 import org.makumba.Transaction;
 import org.makumba.commons.NameResolver;
 import org.makumba.commons.NamedResources;
+import org.makumba.forms.responder.ResponderCacheManager;
 import org.makumba.providers.DataDefinitionProvider;
 import org.makumba.providers.TransactionProvider;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 /**
  * This servlet manages the state of the server when running client-side tests (JSP test suites). It mostly does two
@@ -48,84 +48,92 @@ public class TestInitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        // clean all the bloody caches that hold state, which is toxic for consecutive runs
-        // in our case, some Pointer-s are not re-computed, which is perfectly fine in the real world where the data in
-        // the db does not change. only here it does (the PK values change on auto-increment)
-        NamedResources.cleanupStaticCaches();
+        if(req.getParameter("getResponderBaseDir") != null) {
 
-        Transaction db = TransactionProvider.getInstance().getConnectionToDefault();
-        boolean hql = TransactionProvider.getInstance().getQueryLanguage().equals("hql");
+            resp.getWriter().println(ResponderCacheManager.getInstance().getResponderBaseDirectory());
+             
+        } else {
+             // clean all the bloody caches that hold state, which is toxic for consecutive runs
+            // in our case, some Pointer-s are not re-computed, which is perfectly fine in the real world where the data in
+            // the db does not change. only here it does (the PK values change on auto-increment)
+            NamedResources.cleanupStaticCaches();
 
-        // initialise maximum PK values to allow repeatable execution of diff-based tests
+            Transaction db = TransactionProvider.getInstance().getConnectionToDefault();
+            boolean hql = TransactionProvider.getInstance().getQueryLanguage().equals("hql");
 
-        Map<String, Long> references = new HashMap<String, Long>();
-        Map<String, String> types = new HashMap<String, String>();
-        for (String type : testTypes) {
-            DataDefinition t = DataDefinitionProvider.getInstance().getDataDefinition(type);
-            String primaryKey = t.getFieldDefinition(0).getName();
-            types.put(t.getName(), primaryKey);
-            findRelated(t, types);
-        }
+            // initialise maximum PK values to allow repeatable execution of diff-based tests
 
-        // for each type, query the max of its PK value
-        for (String key : types.keySet()) {
-            String maxQuery = null;
-            if (hql && key.contains("->")) {
-                int i = key.indexOf("->");
-                maxQuery = "SELECT max(st.id) as maximum FROM " + key.substring(0, i) + " t, t." + key.substring(i + 2)
-                        + " st";
-            } else if (hql) {
-                maxQuery = "SELECT max(t.id) as maximum FROM " + key + " t";
-            } else {
-                maxQuery = "SELECT max(t." + types.get(key) + ") as maximum FROM " + key + " t";
+            Map<String, Long> references = new HashMap<String, Long>();
+            Map<String, String> types = new HashMap<String, String>();
+            for (String type : testTypes) {
+                DataDefinition t = DataDefinitionProvider.getInstance().getDataDefinition(type);
+                String primaryKey = t.getFieldDefinition(0).getName();
+                types.put(t.getName(), primaryKey);
+                findRelated(t, types);
             }
-            Vector<Dictionary<String, Object>> res = db.executeQuery(maxQuery, new Object[] {});
-            Object max = res.get(0).get("maximum");
 
-            if (max == null) {
-                references.put(key, 0l);
-                references.put(NameResolver.arrowToDoubleUnderscore(key), 0l);
-            } else {
-                // hibernate-db sometimes returns an Integer, sometimes a Pointer...the DB layer clearly needs some
-                // improvement
-                if (max instanceof Integer) {
-                    references.put(key, ((Integer) max).longValue());
-                    references.put(NameResolver.arrowToDoubleUnderscore(key), ((Integer) max).longValue());
-                } else if (max instanceof Pointer) {
-                    references.put(key, ((Pointer) max).longValue());
-                    references.put(NameResolver.arrowToDoubleUnderscore(key), ((Pointer) max).longValue());
+            // for each type, query the max of its PK value
+            for (String key : types.keySet()) {
+                String maxQuery = null;
+                if (hql && key.contains("->")) {
+                    int i = key.indexOf("->");
+                    maxQuery = "SELECT max(st.id) as maximum FROM " + key.substring(0, i) + " t, t." + key.substring(i + 2)
+                            + " st";
+                } else if (hql) {
+                    maxQuery = "SELECT max(t.id) as maximum FROM " + key + " t";
+                } else {
+                    maxQuery = "SELECT max(t." + types.get(key) + ") as maximum FROM " + key + " t";
+                }
+                Vector<Dictionary<String, Object>> res = db.executeQuery(maxQuery, new Object[] {});
+                Object max = res.get(0).get("maximum");
+
+                if (max == null) {
+                    references.put(key, 0l);
+                    references.put(NameResolver.arrowToDoubleUnderscore(key), 0l);
+                } else {
+                    // hibernate-db sometimes returns an Integer, sometimes a Pointer...the DB layer clearly needs some
+                    // improvement
+                    if (max instanceof Integer) {
+                        references.put(key, ((Integer) max).longValue());
+                        references.put(NameResolver.arrowToDoubleUnderscore(key), ((Integer) max).longValue());
+                    } else if (max instanceof Pointer) {
+                        references.put(key, ((Pointer) max).longValue());
+                        references.put(NameResolver.arrowToDoubleUnderscore(key), ((Pointer) max).longValue());
+                    }
                 }
             }
+
+            // uncomment this if you need to retrieve the initial pointer reference values (on an empty db)
+            /*
+            for (String r : references.keySet()) {
+                System.out.println(r + " " + references.get(r));
+            }
+            */
+
+            // hibernate-db sometimes asks for the key with pointer, sometimes with double underscore, so we give it both...
+            Map<String, Long> initial = new HashMap<String, Long>();
+            initial.put("test.Individual", 2l);
+            initial.put("test.Language", 5l);
+            initial.put("test.Person", 2l);
+            initial.put("test.Person->toys", 2l);
+            initial.put("test.Person->address", 1l);
+            initial.put("test.Person->intSet", 2l);
+            initial.put("test.Person->charSet", 0l);
+            initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->toys"), 2l);
+            initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->address"), 1l);
+            initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->intSet"), 2l);
+            initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->charSet"), 0l);
+
+            ReferenceUIDStrategy.setInitialReferences(initial);
+
+            ReferenceUIDStrategy.setReferences(references);
+
+            db.close();
+
         }
-
-        // uncomment this if you need to retrieve the initial pointer reference values (on an empty db)
-        /*
-        for (String r : references.keySet()) {
-            System.out.println(r + " " + references.get(r));
-        }
-        */
-
-        // hibernate-db sometimes asks for the key with pointer, sometimes with double underscore, so we give it both...
-        Map<String, Long> initial = new HashMap<String, Long>();
-        initial.put("test.Individual", 2l);
-        initial.put("test.Language", 5l);
-        initial.put("test.Person", 2l);
-        initial.put("test.Person->toys", 2l);
-        initial.put("test.Person->address", 1l);
-        initial.put("test.Person->intSet", 2l);
-        initial.put("test.Person->charSet", 0l);
-        initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->toys"), 2l);
-        initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->address"), 1l);
-        initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->intSet"), 2l);
-        initial.put(NameResolver.arrowToDoubleUnderscore("test.Person->charSet"), 0l);
-
-        ReferenceUIDStrategy.setInitialReferences(initial);
-
-        ReferenceUIDStrategy.setReferences(references);
-
-        db.close();
 
         resp.setStatus(HttpStatus.SC_OK);
+
 
     }
 
