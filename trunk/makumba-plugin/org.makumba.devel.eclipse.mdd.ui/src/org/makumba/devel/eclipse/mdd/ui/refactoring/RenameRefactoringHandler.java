45b2a058784a960b78c0d3ef8ce770bc4ec0529f
filip_kis <filip_kis@1c21897b-c138-0410-a5df-bc8783e84eff>
2010-11-11 07:20:19 +0000

    Fixing bug caused by changing the package configuration

--- trunk/makumba-plugin/org.makumba.devel.eclipse.mdd.ui/src/org/makumba/devel/eclipse/mdd/ui/refactoring/RenameRefactoringHandler.java
+++ trunk/makumba-plugin/org.makumba.devel.eclipse.mdd.ui/src/org/makumba/devel/eclipse/mdd/ui/refactoring/RenameRefactoringHandler.java
@@ -1,9 +1,14 @@
 package org.makumba.devel.eclipse.mdd.ui.refactoring;
 
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
@@ -12,14 +17,49 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.parsetree.CompositeNode;
+import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
 import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
+import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 import org.makumba.devel.eclipse.mdd.MDD.FieldDeclaration;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 
 public class RenameRefactoringHandler extends AbstractHandler {
+
+	private class EObjectResolver implements IUnitOfWork<IEObjectDescription, XtextResource> {
+		private final ITextSelection selection;
+
+		private EObjectResolver(ITextSelection selection) {
+			this.selection = selection;
+		}
+
+		public IEObjectDescription exec(XtextResource state) throws Exception {
+			EObject element = EObjectAtOffsetHelper.resolveElementAt(state, selection.getOffset(), null);
+			if (element != null) {
+				final URI eObjectURI = EcoreUtil.getURI(element);
+				IResourceDescription resourceDescription = resourceDescriptions.getResourceDescription(eObjectURI
+						.trimFragment());
+				if (resourceDescription != null) {
+					Iterator<IEObjectDescription> eObjectDescriptions = Iterables.filter(
+							resourceDescription.getExportedObjects(), new Predicate<IEObjectDescription>() {
+								public boolean apply(IEObjectDescription input) {
+									return input.getEObjectURI().equals(eObjectURI);
+								}
+							}).iterator();
+					if (eObjectDescriptions.hasNext()) {
+						return eObjectDescriptions.next();
+					}
+				}
+			}
+			return null;
+		}
+	}
 
 	@Inject
 	private IResourceDescriptions resourceDescriptions;
@@ -38,8 +78,8 @@ public class RenameRefactoringHandler extends AbstractHandler {
 
 			final ITextSelection selection = (ITextSelection) editor.getSelectionProvider().getSelection();
 
-			final IEObjectDescription eObjectDescription = editor.getDocument().readOnly(
-					new EObjectResolver(selection, resourceDescriptions));
+			final IEObjectDescription eObjectDescription = editor.getDocument()
+					.readOnly(new EObjectResolver(selection));
 
 			//TODO: debug this
 			CompositeNode o = editor.getDocument().readOnly(
