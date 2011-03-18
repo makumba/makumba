c7e84b18022d7fec6d41889f711f56e6048352c5
Filip Kis <filip.kis@gmail.com>
2011-03-18 18:01:53 +0000

    Refactoring - moving identifier check to MQLContext

--- makumba-plugin/org.makumba.devel.eclipse.mdd/src/org/makumba/devel/eclipse/mdd/validation/MQLValidator.java
+++ makumba-plugin/org.makumba.devel.eclipse.mdd/src/org/makumba/devel/eclipse/mdd/validation/MQLValidator.java
@@ -6,9 +6,7 @@ import org.eclipse.xtext.resource.IEObjectDescription;
 import org.makumba.devel.eclipse.mdd.MDDUtils;
 import org.makumba.devel.eclipse.mdd.MQLContext;
 import org.makumba.devel.eclipse.mdd.MDD.DataDefinition;
-import org.makumba.devel.eclipse.mdd.MDD.Declaration;
 import org.makumba.devel.eclipse.mdd.MDD.FieldDeclaration;
-import org.makumba.devel.eclipse.mdd.MDD.FunctionDeclaration;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
@@ -47,7 +45,6 @@ public class MQLValidator {
 				EcoreUtil2.resolveAll(resourceSet.get());
 				DataDefinition dd = (DataDefinition) EcoreUtil2.resolve((ddDescription.getEObjectOrProxy()),
 						resourceSet.get());
-
 				Iterable<FieldDeclaration> fields = MDDUtils.getPointerOrSetFields(dd.getD());
 				String[] segments = path.split("\\.");
 				for (final String segment : segments) {
@@ -81,92 +78,10 @@ public class MQLValidator {
 	 *            true if the identifier should be a function, false otherwise
 	 * @return
 	 */
-	public String validateQueryIdentifier(String ident, MQLContext context, final boolean isFunction) {
-		if (!ident.contains(".")) {
-			if (isFunction) {
-				if (context.isFunctionName(ident))
-					return null;
-				if (context.containsLabel("this")) {
-					return validateQueryIdentifier(context.resolveLabel("this") + "." + ident, context, true);
-				}
-				return "Unknown function:" + ident;
-			} else {
-				if (context.containsParam(ident)) {
-					return null; //it's a function paramater
-				}
-				if (context.containsLabel(ident)) {
-					return null; //it's a label
-				}
-				if (context.containsLabel("this")) {
-					return validateQueryIdentifier(context.resolveLabel("this") + "." + ident, context, false);
-				}
-				return "Unkonwn value:" + ident;
-			}
-		} else {
-			//			String label = context.resolveLabel(ident.substring(0, ident.indexOf(".")));
-			//			if (label.equals(ident.substring(0, ident.indexOf(".")))) {
-			//				return "Unknown label:" + label;
-			//			}
-			//			ident = ident.substring(ident.indexOf("."));
+	public void validateQueryIdentifier(String ident, MQLContext context, final boolean isFunction)
+			throws ValidationException {
 
-			String path = context.resolvePath(ident);
-			IEObjectDescription ddDescription = context.getDataDefinition(path);
-			//IEObjectDescription current = ddDescription;
-			if (ddDescription != null) {
-				String passed = path.substring(0, ddDescription.getName().length());
-				path = path.substring(ddDescription.getName().length());
-				if (!path.isEmpty()) { //we still have some path after data type
-					if (path.charAt(0) != '.') {
-						return "Found '" + path + "' where '.' was expected";
-
-					}
-					path = path.substring(1);
-					EcoreUtil2.resolveAll(resourceSet.get());
-					DataDefinition dd = (DataDefinition) EcoreUtil2.resolve((ddDescription.getEObjectOrProxy()),
-							resourceSet.get());
-
-					Iterable<Declaration> declarations = dd.getD();
-					String[] segments = path.split("\\.");
-					for (int i = 0; i < segments.length; i++) {//we search all the segments
-						final String segment = segments[i];
-						if (i < segments.length - 1) { //we are not at the last segment so it has to be a point or set
-							try {
-								Iterable<FieldDeclaration> fields = Iterables.filter(declarations,
-										FieldDeclaration.class);
-								FieldDeclaration field = Iterables.find(fields, new Predicate<FieldDeclaration>() {
-									public boolean apply(FieldDeclaration input) {
-										return input.getName().equals(segment);
-									}
-								});
-								passed += "." + segment;
-								declarations = MDDUtils.getPointedDeclarations(field);
-							} catch (Exception e) { //in the middle of the path we have a non existing field
-								return "No such field '" + segment + "' in '" + passed;
-							}
-						} else { //we are at the last segment we check if it is an existing function or field
-							try {
-								Iterables.find(declarations, new Predicate<Declaration>() {
-									public boolean apply(Declaration input) {
-										if (!isFunction && (input instanceof FieldDeclaration))
-											return ((FieldDeclaration) input).getName().equals(segment);
-										if (isFunction && (input instanceof FunctionDeclaration))
-											return ((FunctionDeclaration) input).getName().equals(segment);
-										return false;
-									}
-								});
-								passed += "." + segment;
-							} catch (Exception e) { //at the end of the path we have a non existing field
-								return "No such " + (isFunction ? "function" : "field") + " '" + segment + "' in '"
-										+ passed;
-							}
-						}
-					}
-				}
-			} else {
-				return "Unknown Data Type at the begining of: " + path;
-			}
-		}
-		return null;
+		context.resolveQueryIdentifier(ident, resourceSet.get(), isFunction);
 
 	}
 
