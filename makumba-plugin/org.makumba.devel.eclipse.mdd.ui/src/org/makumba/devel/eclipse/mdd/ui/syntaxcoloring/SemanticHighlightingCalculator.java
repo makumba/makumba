106ff6b235cfa1edb718a238302c3dd9d5675c6e
Filip Kis <filip.kis@gmail.com>
2011-03-18 18:11:14 +0000

    Added query functions hyperlinking in mdd editor

--- makumba-plugin/org.makumba.devel.eclipse.mdd.ui/src/org/makumba/devel/eclipse/mdd/ui/syntaxcoloring/SemanticHighlightingCalculator.java
+++ makumba-plugin/org.makumba.devel.eclipse.mdd.ui/src/org/makumba/devel/eclipse/mdd/ui/syntaxcoloring/SemanticHighlightingCalculator.java
@@ -8,6 +8,8 @@ import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.makumba.devel.eclipse.mdd.MDD.FromClassOrOuterQueryPath;
+import org.makumba.devel.eclipse.mdd.MDD.IdentPrimary;
 
 public class SemanticHighlightingCalculator implements ISemanticHighlightingCalculator {
 
@@ -19,12 +21,44 @@ public class SemanticHighlightingCalculator implements ISemanticHighlightingCalc
 		for (AbstractNode abstractNode : allNodes) {
 			if (abstractNode.getGrammarElement() instanceof CrossReference) {
 				highlightNode(abstractNode, DefaultHighlightingConfiguration.NUMBER_ID, acceptor);
-			}
+			} else if (abstractNode.getElement() instanceof FromClassOrOuterQueryPath) {
+				FromClassOrOuterQueryPath from = (FromClassOrOuterQueryPath) abstractNode.getElement();
+				for (LeafNode leaf : abstractNode.getLeafNodes()) {
+					if (!leaf.isHidden()) {
+						acceptor.addPosition(leaf.getOffset(), from.getPath().length(),
+								DefaultHighlightingConfiguration.NUMBER_ID);
+						break;
+					}
+				}
+			} else if (abstractNode.getElement() instanceof IdentPrimary) {
 
-			/* TODO: to be added when hyperlinking and validation are working
-			else if (abstractNode.getElement() instanceof FromClassOrOuterQueryPath) {
-				highlightNode(abstractNode, DefaultHighlightingConfiguration.NUMBER_ID, acceptor);
-			}*/
+				String content = "";
+				boolean isFunction = false;
+				boolean stop = false;
+				int offset = -1;
+
+				//we traverse the leafs and try to put the path together
+				for (LeafNode leaf : abstractNode.getLeafNodes()) {
+					if (!stop && leaf.getText() != null && !(content.isEmpty() && leaf.getText().matches("\\s*"))) { //we skip the empty leafs at the beginning
+						if (leaf.getText().matches("([a-zA-Z]\\w*|\\.)")) { //if we have a part of identifier we concatenate it
+							if (offset == -1)
+								offset = leaf.getOffset();
+							content = content + leaf.getText();
+						} else { //otherwise we stop matching the identifier
+							stop = true;
+						}
+					}
+					if (stop && leaf.getText() != null) {
+						if (leaf.getText().equals("(")) { //check if it's open bracket
+							isFunction = true;
+						} else if (!leaf.getText().matches("\\s*")) { //if it's not space we are done
+							break;
+						}
+					}
+				}
+				if (content.contains(".") || !isFunction)
+					acceptor.addPosition(offset, content.length(), DefaultHighlightingConfiguration.NUMBER_ID);
+			}
 		}
 	}
 
