04925478cde380aad40531c392bb4f49b88d60e9
manuelbernhardt <manuelbernhardt@1c21897b-c138-0410-a5df-bc8783e84eff>
2011-03-27 14:56:24 +0000

    Adapting to maven packaging

--- tags/makumba-0.9.5/makumba-core/src/main/java/org/makumba/version.java
+++ tags/makumba-0.9.5/makumba-core/src/main/java/org/makumba/version.java
@@ -27,16 +27,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 
 /**
- * Computes the version from SVN BaseURL tag. If used from a HEAD check-out, this will return "devel-<currentDate>" If
- * used from a TAG check-out, this will return the version of the tag, e.g. "0.7.1" Note that in order to work, the file
+ * Computes the version from the maven version. If none is found, inspect the SVN BaseURL tag. If used from a HEAD check-out,
+ * this will return "devel-<currentDate>".
+ * If used from a TAG check-out, this will return the version of the tag, e.g. "0.7.1" Note that in order to work, the file
  * needs to have the HeadURL keyword enabled
  * 
  * @author Cristian Bogdan
@@ -49,39 +52,57 @@ public class version {
 
     /** @see MakumbaSystem#getVersion() */
     public static String getVersion() {
-        String vs = "$HeadURL$";
+        String version = null;
 
-        // HeadURL will return something like
-        // https://makumba.svn.sourceforge.net/svnroot/makumba/trunk/makumba/classes/org/makumba/version.java for HEAD
-        // https://makumba.svn.sourceforge.net/svnroot/makumba/tags/makumba-0_5_10_2/makumba/classes/org/makumba/version.java
-        // for a tagged version
-
-        String version = "1";
-        vs = vs.substring("$HeadURL: https://makumba.svn.sourceforge.net/svnroot/makumba/".length());
-
-        if (vs.startsWith("tags")) {
-            vs = vs.substring("tags/".length());
-            vs = vs.substring(0, vs.indexOf("/"));
-
-            version = vs.substring(7);
-
-            if (version.indexOf('-') >= 0) {
-                // we have something like "-0_5_10_2"
-                version = version.substring(version.indexOf('-') + 1);
+        // first try - are we a packaged maven JAR?
+        Properties p = new Properties();
+        try {
+            InputStream mvnProperties = version.class.getResourceAsStream("/META-INF/maven/org.makumba/makumba-core/pom.properties");
+            if(mvnProperties != null) {
+                p.load(mvnProperties);
+                version = (String) p.get("version");
             }
-
-            if (version.length() > 2) {
-                // we have something like "0_5_10_2"
-                version = version.replace('_', '.');
-            }
-
-        } else {
-            SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss"); // yyyy-MMM-dd HH:mm:ss
-            // we simply take the current timestamp as a reference point to when the current version has been built
-            version = "devel-" + df.format(new Date());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return version;
+        if(version != null && !version.endsWith("SNAPSHOT")) {
+            return version;
+        } else {
+
+            // second attempt - are we an anonymous checkout from SVN that has already been tagged?
+            String vs = "$HeadURL$";
+
+            // HeadURL will return something like
+            // https://makumba.svn.sourceforge.net/svnroot/makumba/trunk/makumba/classes/org/makumba/version.java for HEAD
+            // https://makumba.svn.sourceforge.net/svnroot/makumba/tags/makumba-0_5_10_2/makumba/classes/org/makumba/version.java
+            // for a tagged version
+
+            vs = vs.substring("$HeadURL: https://makumba.svn.sourceforge.net/svnroot/makumba/".length());
+
+            if (vs.startsWith("tags")) {
+                vs = vs.substring("tags/".length());
+                vs = vs.substring(0, vs.indexOf("/"));
+
+                version = vs.substring(7);
+
+                if (version.indexOf('-') >= 0) {
+                    // we have something like "-0_5_10_2"
+                    version = version.substring(version.indexOf('-') + 1);
+                }
+
+                if (version.length() > 2) {
+                    // we have something like "0_5_10_2"
+                    version = version.replace('_', '.');
+                }
+                return version;
+
+            } else {
+                SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss"); // yyyy-MMM-dd HH:mm:ss
+                // we simply take the current timestamp as a reference point to when the current version has been built
+                return "devel-" + df.format(new Date());
+            }
+        }
     }
 
     /** @return only the numeric version in dewey decimal format */
