03cee76a0228a6c03275a171f58ca33c39b4f715
Cristian Bogdan <6556008+cristianbogdan@users.noreply.github.com>
2011-03-27 21:41:43 +0000

    formatted, cleaned up according to the etc/ makumba profiles

--- makumba/makumba-jsp-test/src/test/java/org/makumba/test/component/ConcurrentTest.java
+++ makumba/makumba-jsp-test/src/test/java/org/makumba/test/component/ConcurrentTest.java
@@ -24,76 +24,86 @@ import org.makumba.test.util.MakumbaWebTestSetup;
  */
 public class ConcurrentTest extends TestCase {
 
-    private static MakumbaTestSetup setup;
+	private static MakumbaTestSetup setup;
 
-    private static final int THREADS = 200;
+	private static final int THREADS = 200;
 
-    private static final String CONCURRENT_TEST_URL = "/concurrent/concurrentListTest.jsp";
+	private static final String CONCURRENT_TEST_URL = "/concurrent/concurrentListTest.jsp";
 
-    private static final String EXPECTED_TEST_RESULT_FRAGMENT = "speaks: English English French French German German Italian Italian Spanish Spanish";
+	private static final String EXPECTED_TEST_RESULT_FRAGMENT = "speaks: English English French French German German Italian Italian Spanish Spanish";
 
-    public static Test suite() {
-        return setup = new MakumbaWebTestSetup(new TestSuite(ConcurrentTest.class), "oql");
-    }
+	public static Test suite() {
+		return setup = new MakumbaWebTestSetup(new TestSuite(
+				ConcurrentTest.class), "oql");
+	}
 
-    public void testListConcurrent() throws Exception {
+	public void testListConcurrent() throws Exception {
 
-        checkListPageAvailable();
+		checkListPageAvailable();
 
-        final CountDownLatch startGate = new CountDownLatch(1);
-        final CountDownLatch endGate = new CountDownLatch(THREADS);
+		final CountDownLatch startGate = new CountDownLatch(1);
+		final CountDownLatch endGate = new CountDownLatch(THREADS);
 
-        for (int i = 0; i < THREADS; i++) {
-            final int index = i;
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        // each thread waits for the latch to be released before firing
-                        startGate.await();
-                        try {
-                            long start = System.currentTimeMillis();
-                            HttpClient httpClient = new HttpClient();
-                            GetMethod getTestPage = new GetMethod(System.getProperty("cactus.contextURL")
-                                    + CONCURRENT_TEST_URL);
-                            httpClient.executeMethod(getTestPage);
-                            String result = getTestPage.getResponseBodyAsString();
-                            // System.out.println(result);
-                            assertTrue(result.indexOf(EXPECTED_TEST_RESULT_FRAGMENT) > -1);
-                            System.out.println("LIST TIME for:" + index + "=" + (System.currentTimeMillis() - start)
-                                    + " ms");
-                        } finally {
-                            // count down the end gate, when no threads are left the endGate will fire
-                            endGate.countDown();
-                        }
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            };
-            thread.start();
-        }
+		for (int i = 0; i < THREADS; i++) {
+			final int index = i;
+			Thread thread = new Thread() {
+				@Override
+				public void run() {
+					try {
+						// each thread waits for the latch to be released before
+						// firing
+						startGate.await();
+						try {
+							long start = System.currentTimeMillis();
+							HttpClient httpClient = new HttpClient();
+							GetMethod getTestPage = new GetMethod(
+									System.getProperty("cactus.contextURL")
+											+ CONCURRENT_TEST_URL);
+							httpClient.executeMethod(getTestPage);
+							String result = getTestPage
+									.getResponseBodyAsString();
+							// System.out.println(result);
+							assertTrue(result
+									.indexOf(EXPECTED_TEST_RESULT_FRAGMENT) > -1);
+							System.out.println("LIST TIME for:" + index + "="
+									+ (System.currentTimeMillis() - start)
+									+ " ms");
+						} finally {
+							// count down the end gate, when no threads are left
+							// the endGate will fire
+							endGate.countDown();
+						}
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				}
+			};
+			thread.start();
+		}
 
-        long start = System.currentTimeMillis();
-        // fire all threads
-        startGate.countDown();
+		long start = System.currentTimeMillis();
+		// fire all threads
+		startGate.countDown();
 
-        // the main thread waits for all threads to have ran before continuing
-        endGate.await();
-        System.out.println("TOTAL TIME:" + (System.currentTimeMillis() - start) + " ms");
-    }
+		// the main thread waits for all threads to have ran before continuing
+		endGate.await();
+		System.out.println("TOTAL TIME:" + (System.currentTimeMillis() - start)
+				+ " ms");
+	}
 
-    private void checkListPageAvailable() throws IOException, HttpException {
-        HttpClient httpClient = new HttpClient();
-        GetMethod getTestPage = new GetMethod(System.getProperty("cactus.contextURL") + CONCURRENT_TEST_URL);
-        int status = -1;
-        try {
-            status = httpClient.executeMethod(getTestPage);
-        } catch (ConnectException c) {
-            setup.tearDown();
-            throw new RuntimeWrappedException(c);
-        }
-        assertEquals("Test page did not run correctly", HttpStatus.SC_OK, status);
-    }
+	private void checkListPageAvailable() throws IOException, HttpException {
+		HttpClient httpClient = new HttpClient();
+		GetMethod getTestPage = new GetMethod(
+				System.getProperty("cactus.contextURL") + CONCURRENT_TEST_URL);
+		int status = -1;
+		try {
+			status = httpClient.executeMethod(getTestPage);
+		} catch (ConnectException c) {
+			setup.tearDown();
+			throw new RuntimeWrappedException(c);
+		}
+		assertEquals("Test page did not run correctly", HttpStatus.SC_OK,
+				status);
+	}
 
 }
