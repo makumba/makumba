231bf86b0ed3a6097ad4a61dbd3b349fc567d4bb
Rudolf Mayer <3913642+rudolfmayer@users.noreply.github.com>
2011-04-01 15:10:41 +0000

    tool to test regular expression server-side (first part of re #1283)

--- makumba/makumba-core/src/main/java/org/makumba/providers/DeveloperTool.java
+++ makumba/makumba-core/src/main/java/org/makumba/providers/DeveloperTool.java
@@ -1,5 +1,32 @@
+///////////////////////////////
+//  Makumba, Makumba tag library
+//  Copyright (C) 2000-2003  http://www.makumba.org
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//  -------------
+//  $Id$
+//  $Name$
+/////////////////////////////////////
+
 package org.makumba.providers;
 
+/**
+ * @author Rudolf Mayer
+ * @author Manuel Bernhardt
+ */
 public enum DeveloperTool {
 
     MDD_VIEWER("mddViewer", "DataDefinition viewer", "View data definitions", true),
@@ -14,6 +41,8 @@ public enum DeveloperTool {
     REFERENCE_CHECKER("referenceChecker", "Reference Checker",
             "Checks creation the status of foreign and unique keys and displays broken references", true),
     ERRORLOG_VIEWER("errorLogViewer", "Error log viewer", "List logged makumba errors", true),
+    REGEXP_TESTER("regexpTester", "Regular Expression Tester", "Test regular expressions used in MDD validation rules",
+            true),
     CACHE_CLEANER("makumbaCacheCleaner", "Makumba Cache Cleaner",
             "Cleans all internal Makumba caches, like queries, data-definitions.<br/>"
                     + "Useful during development, to avoid having to restart the servlet container", false);
