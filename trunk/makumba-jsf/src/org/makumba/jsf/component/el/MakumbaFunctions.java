c0eaf239e3b6dc200adfd8e4832a18b7ece9baa2
cristian_bogdan <cristian_bogdan@1c21897b-c138-0410-a5df-bc8783e84eff>
2013-03-08 14:02:46 +0000

    these are very old uncommitted changes on the JSF subproject, so I commit them. JSF subproject was not continued since JSF is not a very succesful technology, but we came up with nice ideas when we did the  JSF implementation because in JSF the UI tree is more explicit + preserved between (partial) request and response

--- trunk/makumba-jsf/src/org/makumba/jsf/component/el/MakumbaFunctions.java
+++ trunk/makumba-jsf/src/org/makumba/jsf/component/el/MakumbaFunctions.java
@@ -1,5 +1,15 @@
 package org.makumba.jsf.component.el;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.faces.context.FacesContext;
+import javax.faces.model.ListDataModel;
+
 import org.makumba.jsf.MakumbaDataContext;
 
 /**
@@ -11,6 +21,63 @@ import org.makumba.jsf.MakumbaDataContext;
 public class MakumbaFunctions {
 
     public static Object expr(String expr) {
+        FacesContext faces = FacesContext.getCurrentInstance();
+        ELContext el = faces.getELContext();
+        ExpressionFactory ef = faces.getApplication().getExpressionFactory();
         return MakumbaDataContext.getDataContext().getCurrentList().getExpressionValue(expr);
+    }
+
+    public static Object from(String expr) {
+        ArrayList<String> list = new ArrayList<String>();
+        Collections.addAll(list, "a" + new Date(), "b" + new Date(), "c" + new Date());
+
+        return new ListDataModel<String>(list) {
+            @Override
+            public void setRowIndex(int i) {
+                super.setRowIndex(i);
+                System.out.println(Integer.toString(System.identityHashCode(this), 16) + " " + i);
+            }
+        };
+    }
+
+    private static ArrayList<String> makeList() {
+        ArrayList<String> list = new ArrayList<String>() {
+            @Override
+            public Iterator<String> iterator() {
+                final Iterator<String> orig = super.iterator();
+                final int hc = System.identityHashCode(this);
+                return new Iterator<String>() {
+
+                    @Override
+                    public boolean hasNext() {
+                        boolean b = orig.hasNext();
+                        if (!b) {
+                            System.out.println("end of iteration " + hc);
+                        }
+                        return b;
+                    }
+
+                    @Override
+                    public String next() {
+                        System.out.println("iteration " + hc);
+                        return orig.next();
+                    }
+
+                    @Override
+                    public void remove() {
+                        orig.remove();
+                    }
+
+                };
+            }
+
+            @Override
+            public String get(int i) {
+                System.out.println("get " + i + " " + System.identityHashCode(this));
+                return super.get(i);
+            }
+        };
+
+        return list;
     }
 }
